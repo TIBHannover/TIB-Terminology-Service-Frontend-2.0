3ba5b5cca5d439f6c3577ff74fac008c2ab3984f
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-03-31 13:08:41 +0200

    adding basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
-      
+      <Route>
+          <Route path="/about" element={<About />}/>
+      </Route>
     </div>
   );
 }
