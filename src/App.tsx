cd20f688e12a03df9fc25d6b3e73b9ab39114103
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-04-04 11:19:08 +0200

    adding basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/common/Header/Header'
 function App () {
   return (
     <div className="App">
-      {<Header appName={undefined} headers={undefined}/>}
+      {<Header />}
       <Footer/>
     </div>
   )
