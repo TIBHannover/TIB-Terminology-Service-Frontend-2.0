2f7d4eeddaa0618fa7bc56e9eefd8c75be7c2370
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-04-11 10:56:32 +0200

    adding footer template

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,45 +1,196 @@
-import React from 'react'
-import { Container, Grid, Box, Link } from '@material-ui/core'
+import { Container, Row, Col, Badge } from 'reactstrap';
+import ROUTES from 'constants/routes';
+import ROUTES_CMS from 'constants/routesCms';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { ReactComponent as Logo } from 'assets/img/vertical_logo.svg';
+import TIB_LOGO from 'assets/img/TIB_Logo_en.png';
+import DFG_LOGO from 'assets/img/dfg-logo.png';
+import styled from 'styled-components';
+import { reverse } from 'named-urls';
 
-function Footer () {
-  return (
-    <Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }} bgcolor="#f2f2f2">
-      <Container maxWidth="lg">
-        <Grid container spacing = {5}>
-          <Grid item xs = {12} sm={4}>
-            <Box borderBottom={1}>About the Service</Box>
-            <Box>
-              <Link href="/" color="inherit">
-                  Imprint
-              </Link>
-            </Box>
-            <Box>
-              <Link href="/" color="inherit">
-                  Terms of Use
-              </Link>
-            </Box>
-            <Box>
-              <Link href="/" color="inherit">
-                  Privacy Policy
-              </Link>
-            </Box>
-          </Grid>
-          <Grid item xs = {12} sm={4}>
-            <Box>Maintainer</Box>
-            <Box>
-              <img src= {'https://www.tib.eu/typo3conf/ext/tib_tmpl_bootstrap/Resources/Public/images/TIB_Logo_de.png'} alt="TIB logo" width="250"/>
-            </Box>
-          </Grid>
-          <Grid item xs = {12} sm={4}>
-            <Box>Funding</Box>
-            <Box>
-              <img src= {'https://terminology.nfdi4chem.de/img/dfg_logo.png'} alt="dfg logo" width="250"/>
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  )
-}
+const FooterWrapper = styled.div`
+    background: #e0e2ea;
+    margin-top: 75px;
+    border-top: 1px #d1d3d9 solid;
+`;
 
-export default Footer
+const FooterCol = styled(Col)`
+    color: ${props => props.theme.secondaryDarker};
+    margin: 10px 0;
+    font-size: 0.95rem;
+
+    h5 {
+        font-weight: 500;
+        text-transform: uppercase;
+        color: ${props => props.theme.secondaryDarker};
+        font-size: 1.1rem;
+    }
+    .description {
+        font-size: 0.85rem;
+    }
+    a {
+        color: ${props => props.theme.secondaryDarker};
+    }
+`;
+
+const PartnerLogoCol = styled(Col)`
+    text-align: center;
+`;
+
+const TwitterLink = styled.a`
+    :hover {
+        color: #00acee;
+    }
+`;
+
+const Footer = () => (
+    <FooterWrapper>
+        <Container>
+            <footer className="pt-4 pb-4">
+                <h1 className="sr-only">More information about ORKG</h1>
+                <Row>
+                    <FooterCol md={3}>
+                        <h2 className="h5">ORKG</h2>
+                        <hr className="me-5" />
+                        <Row>
+                            <div className="float-start col-md-3">
+                                <Link to={ROUTES.HOME}>
+                                    <Logo style={{ height: '80px' }} />
+                                </Link>
+                            </div>
+                            <div className="col-md-8 description">
+                                The Open Research Knowledge Graph aims to describe research papers in a structured manner
+                            </div>
+                        </Row>
+                    </FooterCol>
+                    <FooterCol md={3}>
+                        <h2 className="h5">About</h2>
+                        <hr className="me-5" />
+                        <ul className="p-0" style={{ listStyle: 'none' }}>
+                            <li>
+                                <Link to={reverse(ROUTES.ABOUT, {})}>About us</Link>
+                            </li>
+                            <li>
+                                <Link to={ROUTES.HELP_CENTER}>Help center</Link>
+                            </li>
+                            {/*<li>
+                                <a href="https://projects.tib.eu/orkg/get-involved/" target="_blank" rel="noopener noreferrer">
+                                    Get involved
+                                </a>
+                            </li>*/}
+                            <li>
+                                <Link to={reverse(ROUTES.PAGE, { url: ROUTES_CMS.DATA_PROTECTION })}>Data protection</Link>
+                            </li>
+                            <li>
+                                <Link to={reverse(ROUTES.PAGE, { url: ROUTES_CMS.TERMS_OF_USE })}>Terms of use</Link>
+                            </li>
+                            <li>
+                                <Link to={reverse(ROUTES.PAGE, { url: ROUTES_CMS.IMPRINT })}>Imprint</Link>
+                            </li>
+                        </ul>
+                    </FooterCol>
+                    <FooterCol md={3}>
+                        <h2 className="h5">Technical</h2>
+                        <hr className="me-5" />
+                        <ul className="p-0" style={{ listStyle: 'none' }}>
+                            <li>
+                                <Link to={ROUTES.DATA}>Data Access</Link>
+                            </li>
+                            <li>
+                                <Link to={ROUTES.CHANGELOG}>Changelog</Link>
+                            </li>
+                            <li>
+                                <a href="https://gitlab.com/TIBHannover/orkg/orkg-frontend/" target="_blank" rel="noopener noreferrer">
+                                    GitLab
+                                </a>
+                            </li>
+                            <li>
+                                <Link to={reverse(ROUTES.PAGE, { url: ROUTES_CMS.ACCESSIBILITY_STATEMENT })}>Accessibility</Link>
+                            </li>
+                            <li>
+                                <Link to={reverse(ROUTES.PAGE, { url: ROUTES_CMS.LICENSE })}>License</Link>
+                            </li>
+                        </ul>
+                    </FooterCol>
+                    <FooterCol md={3}>
+                        <h2 className="h5">More</h2>
+                        <hr className="me-5" />
+                        <ul className="p-0" style={{ listStyle: 'none' }}>
+                            <li>
+                                <TwitterLink href="https://twitter.com/orkg_org" target="_blank" rel="noopener noreferrer">
+                                    Follow us
+                                    <Icon className="ms-2" icon={faTwitter} />
+                                </TwitterLink>
+                            </li>
+                            <li>
+                                <Link to={reverse(ROUTES.PAGE, { url: ROUTES_CMS.CONTACT })}>Contact us</Link>
+                            </li>
+                            <li>
+                                <a href="https://gitlab.com/TIBHannover/orkg/orkg-frontend/issues" target="_blank" rel="noopener noreferrer">
+                                    Report an issue
+                                </a>
+                            </li>
+                            <li>
+                                <i className="me-3">Version</i> <Badge color="info">GIT_VERSION</Badge>
+                            </li>
+                        </ul>
+                    </FooterCol>
+                </Row>
+                <hr style={{ width: '70%', margin: '1rem auto' }} />
+                <Row className="mt-4">
+                    <PartnerLogoCol md={4} style={{}}>
+                        <a href="https://www.tib.eu/en/" target="_blank" rel="noopener noreferrer">
+                            <img src={TIB_LOGO} alt="Logo Technische Informationsbibliothek (TIB)" style={{ borderWidth: 0, height: '50px' }} />
+                        </a>
+                    </PartnerLogoCol>
+                    <Col md={8}>
+                        <Row className="g-0">
+                            <Col md={4} style={{ textAlign: 'center' }}>
+                                <a href="https://www.uni-hannover.de/en/" target="_blank" rel="noopener noreferrer">
+                                    <img src={LUH_LOGO} alt="Logo Leibniz University Hannover" style={{ borderWidth: 0, height: '45px' }} />
+                                </a>
+                            </Col>
+                            <Col md={4} style={{ textAlign: 'center' }}>
+                                <a href="https://www.l3s.de/en/" target="_blank" rel="noopener noreferrer">
+                                    <img src={L3S_LOGO} alt="Logo L3S Research Center" style={{ borderWidth: 0, height: '50px' }} />
+                                </a>
+                            </Col>
+                            <Col md={4} style={{ textAlign: 'center' }}>
+                                <a href="https://infai.org/en/" target="_blank" rel="noopener noreferrer">
+                                    <img
+                                        src={INFAI_LOGO}
+                                        alt="Logo  Institute for Applied Informatics (InfAI)"
+                                        style={{ borderWidth: 0, height: '45px' }}
+                                    />
+                                </a>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <hr style={{ width: '50%', margin: '1rem auto' }} />
+                <Row>
+                    <PartnerLogoCol md={{ size: 3, order: 1, offset: 3 }} style={{ textAlign: 'center' }}>
+                        <img
+                            src={EU_LOGO}
+                            alt="Co-funded by the Horizon 2020 programme of the European Union"
+                            style={{ borderWidth: 0, height: '50px' }}
+                        />
+                    </PartnerLogoCol>
+                    <PartnerLogoCol md={{ size: 3, order: 2, offset: 0 }} style={{ textAlign: 'center' }}>
+                        <a
+                            href="https://marketplace.eosc-portal.eu/services/open-research-knowledge-graph-orkg"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={EOSC_LOGO} alt="European Open Science Cloud (EOSC)" style={{ borderWidth: 0, height: '45px' }} />
+                        </a>
+                    </PartnerLogoCol>
+                </Row>
+            </footer>
+        </Container>
+    </FooterWrapper>
+);
+
+export default Footer;
