0f5d9b889423aa38a650a1881bf9bf275f456f93
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-04-12 10:33:38 +0200

    binding state

--- src/components/Search/SearchForm.jsx
+++ src/components/Search/SearchForm.jsx
@@ -14,6 +14,26 @@ class SearchForm extends React.Component{
         return (await result.json()).results;
       }
 
+      async handleChange(term){
+        if (term > 0){
+         this.setState({
+             result: true
+         });
+        }
+      }
+
+      render(){
+          return(
+            <div className="container">
+            {this.state.result &&
+            <input type="text" className="col-md-12 input" style={{marginTop: 10}}
+            onChange={this.handleChange}
+            placeholder="Search NFDI4Chem TS"
+          />}
+          </div>
+          )
+      }
+
 }
 
 export default SearchForm;
