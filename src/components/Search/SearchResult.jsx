bad4ace3b902cda81235f027fccce933ec6a843f
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-04-20 11:08:09 +0200

    adding search result

---
+++ src/components/Search/SearchResult.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './SearchResult.css'
+
+class SearchResult extends React.Component{
+    
+}
+
+export default SearchResult;
