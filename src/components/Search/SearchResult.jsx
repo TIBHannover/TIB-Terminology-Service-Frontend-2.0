34aa927f893f46a5cfa2daf03dde2c422394f5ec
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-04-21 11:16:14 +0200

    adding async function for searching

--- src/components/Search/SearchResult.jsx
+++ src/components/Search/SearchResult.jsx
@@ -4,8 +4,31 @@ import './SearchResult.css'
 
 class SearchResult extends React.Component{
     constructor(props){
+        super(props)
+        this.state = ({
+          term: "",
+          result: false,
+          searchResult: []
+        })
+    }
+
+    async searching(term){
+      term = term.target.value;
+    if (term.length > 0){
+        let searchResult = await fetch(`https://service.tib.eu/ts4tib/api/search?q=${term}`)
+        searchResult =  (await searchResult.json())['response']['docs'];
+     this.setState({
+         searchResult: searchResult,
+         result: true
+     });
+    }
+    else if (term.length == 0){
+        this.setState({
+            result: false
+        });
         
     }
+  }
 }
 
 export default SearchResult;
