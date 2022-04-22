5963e6045b3aa5a6577d4a45419aaaed4b46b68f
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-04-22 09:39:12 +0200

    adding pagination in results list

--- src/components/Search/SearchResult.jsx
+++ src/components/Search/SearchResult.jsx
@@ -5,6 +5,7 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
+import PaginationCustom from '../Pagination/Pagination';
 
 class SearchResult extends React.Component{
     constructor(props){
@@ -33,6 +34,30 @@ class SearchResult extends React.Component{
         
     }
   }
+
+  /**
+     * Handle the click on the pagination
+     * @param {*} value
+     */
+   handlePagination (value) {
+    this.setState({
+      pageNumber: value,
+      paginationReset: false
+    }, () => {
+      this.paginationHandler()
+    })
+  }
+
+
+
+  /**
+     * Count the number of pages for the pagination
+     * @returns
+     */
+  pageCount () {
+    return (Math.ceil(this.state.ontologies.length / this.state.pageSize))
+  }
+
 
   /**
      * Create the search results list view
