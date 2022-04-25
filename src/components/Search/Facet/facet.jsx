052358821bf26b2a4bfb1d02db32c071024bd036
pooya.oa <pooya.oladazimi@tib.eu>
2022-04-25 11:29:59 +0200

    get all ontologies from api in facet

--- src/components/Search/Facet/facet.jsx
+++ src/components/Search/Facet/facet.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import './facet.css';
+import {getChemOntologies} from '../../../api/nfdi4chemapi';
 
 
 class Facet extends React.Component{
     constructor(props){
         super(props);
+        this.state = ({
+            allOntologies: [],
+            allOntologiesIds: [],
+            ontologiesLoaded: false
+        });
+        this.getAllOntologies = this.getAllOntologies.bind(this);
+    }
+
+
+    /**
+     * Get a list of all ontologies
+     */
+    async getAllOntologies(){
+        if(!this.state.ontologiesLoaded){
+            let allOntologies = await getChemOntologies();
+            let allIds = [];
+            for(let i=0; i < allOntologies.length; i++){
+                allIds.push(allOntologies[i]['ontologyId']);
+            }
+            this.setState({
+                ontologiesLoaded: true,
+                allOntologies: allOntologies,
+                allOntologiesIds: allIds
+            });
+        }
+    }
+
+
+    componentDidMount(){
+        this.getAllOntologies();
     }
 
 
