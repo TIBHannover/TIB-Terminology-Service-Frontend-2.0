ea458f2fc02e84bf75480b56038cc7ece45286d8
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-04-27 11:21:43 +0200

    back to original

--- src/components/Search/Pagination.jsx
+++ src/components/Search/Pagination.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Pagination from '@material-ui/lab/Pagination'
-import { useState, useEffect } from 'react'
 
 const useStyles = makeStyles((theme) => ({
   // root: {
@@ -18,29 +17,11 @@ const useStyles = makeStyles((theme) => ({
 
 function PaginationCustom (props) {
   const classes = useStyles()
-  const [currentPage, setCurrentPage] = useState(1);
-  const [count, setCount] = useState();
-  const [nextPage, setNextPage] = useState();
-  const [previousPage, setPreviousPage] = useState();
-  const [valid, setValid] = useState(false);
-
-useEffect(() => {
-  fetch("/search?q=" + "start= ${currentPage}")
-      .then(response => response.json())
-      .then(data => {
-        setCount(data.count);
-        setNextPage(data.next);
-        setPreviousPage(data.previous);
-        setValid(true);
-      })
-}, [currentPage]);
-
-const incrementPageNumber = () => setCurrentPage(prevPage => prevPage + 10);
-
-const decrementPageNumber = () => {
-  if(currentPage <= 1) return; 
-  setCurrentPage(prevPage => prevPage - 10);
-}
+  const [page, setPage] = React.useState(1)
+  const handleChange = (event, value) => {
+    props.clickHandler(value)
+    setPage(value)
+  }
 
   return (
     <div className={classes.root}>
@@ -50,6 +31,7 @@ const decrementPageNumber = () => {
         variant="outlined"
         count={props.count}
         page={props.page}
+        onChange={handleChange}
       />
     </div>
   )
