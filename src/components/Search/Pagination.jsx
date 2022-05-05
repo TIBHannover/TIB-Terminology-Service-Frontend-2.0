152d8e27e9a1db12d8b94ea5a54e09f8f8abbb7b
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-05-05 10:17:44 +0200

    adding pagination field

--- src/components/Search/Pagination.jsx
+++ src/components/Search/Pagination.jsx
@@ -6,12 +6,18 @@ class Pagination extends React.Component{
   constructor(props){
     super(props)
     this.setState({
-
+      searchResult: [],
+      searchTerm: ''
     })
+    this.paginating = this.paginating.bind(this)
+    this.handlePageClick = this.handlePageClick.bind(this)
   }
 
   async paginating(){
-      
+    let searchResult = await fetch(`https://service.tib.eu/ts4tib/api/search?q=${searchTerm}`)
+        let resultJson = (await searchResult.json());
+        searchResult =  resultJson['response']['docs'];
+        let paginationFields = resultJson['response'];     
   }
 
   handlePageClick(){
