4130d6f9787c7ed0cd965df608067e56ee11b2a1
pooya.oa <pooya.oladazimi@tib.eu>
2022-05-10 11:08:32 +0200

    rename the css file to be for ontology detail page only

--- src/components/Ontologies/DataTree/DataTree.jsx
+++ src/components/Ontologies/DataTree/DataTree.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../layout/applicationStyles.css';
+import '../../layout/ontologyDetailPage.css';
 import Grid from '@material-ui/core/Grid';
 import TreeView from '@material-ui/lab/TreeView';
 import StyledTreeItem from './widgets/StyledTreeItem';
