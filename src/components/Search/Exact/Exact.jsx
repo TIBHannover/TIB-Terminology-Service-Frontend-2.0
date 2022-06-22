bdb10215ce11f09aa70c8e6ab50ed965b0995cf5
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-06-22 13:25:37 +0200

    exact result

---
+++ src/components/Search/Exact/Exact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class ExactResult extends React.Component{
+    constructor(props){
+        super(props)
+        this.state= ({
+
+        })
+    }
+}
+
+export default ExactResult;
