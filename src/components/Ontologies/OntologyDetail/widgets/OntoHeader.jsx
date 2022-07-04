4e9f301893e551de40b548b58ecbb5c0491ec828
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-07-04 11:09:57 +0200

    turning to functional comp

--- src/components/Ontologies/OntologyDetail/widgets/OntoHeader.jsx
+++ src/components/Ontologies/OntologyDetail/widgets/OntoHeader.jsx
@@ -1,34 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
+import '../../../layout/ontologies.css';
 
-class OntoHeader extends React.Component{
-    constructor(props){
-        super(props)
-        this.setState({
-            title:"",
-            ontologyId: ""
-        })
-        this.getOntoConfig = this.getOntoConfig.bind(this);
-
-    }
-/**
-   * Get the ontology detail config
-   */
-  async getOntoConfig(ontologyId){
-    let getConfig = await fetch(`https://service.tib.eu/ts4tib/api/ontologies/` + ontologyId)
-    getConfig = (await getConfig.json())['config'];
-    this.setState({
-      getConfig: getConfig 
-    })
+function OntoHeader(props){
+  const [ontologyObject, setOntologyObject]  = useState(props.ontology);
+  const ontology = props.ontology;
+  if (!ontology || ontology === null) {
+    return false
   }
 
-  render(){
-    return(
-        <div container className="onto-title">
-            {this.state.getConfig.title}
-        </div>
-    )
-  }
-
+return(
+  <div container className="onto-title">
+    {ontology.config.title}
+  </div>
+  )
 }
+
 export default OntoHeader;
