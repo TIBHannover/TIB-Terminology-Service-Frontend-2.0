1b72ce2552d2f7f460757619a1cb6f27f112b7e7
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-07-06 10:40:25 +0200

    mounting the component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -19,12 +19,16 @@ class Home extends React.Component{
 
   async Stats(){
     let statsResult = await fetch(`https://service.tib.eu/ts4tib/api/ontologies/getstatistics`)
-    statsResult = await statsResult.json();
+    statsResult = (await statsResult.json());
     console.info(statsResult)
     this.setState({
       statsResult: statsResult
     })
 
+  }
+
+  componentDidMount(){
+    this.Stats();
   }
     render(){
         return(
@@ -148,7 +152,7 @@ class Home extends React.Component{
                             <path d="M5 4.604v9.185a4 4 0 0 0 1.781 3.328L12 20.597l5.219-3.48A4 4 0 0 0 19 13.79V4.604L12 3.05 5 4.604zM3.783 2.826L12 1l8.217 1.826a1 1 0 0 1 .783.976v9.987a6 6 0 0 1-2.672 4.992L12 23l-6.328-4.219A6 6 0 0 1 3 13.79V3.802a1 1 0 0 1 .783-.976zM12 13.5l-2.939 1.545.561-3.272-2.377-2.318 3.286-.478L12 6l1.47 2.977 3.285.478-2.377 2.318.56 3.272L12 13.5z"/>
                           </g>
                         </svg>
-                        <h4 className ="font-weight-bold h2 mb-1">{this.state.statsResult['numberOfIndividuals']}</h4>
+                        <h4 className ="font-weight-bold h2 mb-1">{this.state.statsResult.numberOfIndividuals}</h4>
                         <p className ="mb-0">individual</p>
                       </div>                                 
                     </div>   
