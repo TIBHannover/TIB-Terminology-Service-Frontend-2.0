b737b280df4a3d6db02939fb708811e819a2bcc7
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-07-19 11:46:17 +0200

    added documentation

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import OntologyList from "./components/Ontologies/OntologyList/OntologyList";
 import OntologyDetail from "./components/Ontologies/OntologyDetail/OntologyDetail";
 import Home from "./components/Home/Home";
 import SearchResult from "./components/Search/SearchResult";
+import Documentation from "./components/Documentation/Documentation";
 
 
 
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/" component={Home}/>
           <Route exact path="/ontologies" component={OntologyList}/>
           <Route exact path="/ontologies/:ontologyId/:tab?" component={OntologyDetail}/>
+          <Route exact path="/documentation" component={Documentation}/>
           <Route exact path="/search" component={SearchResult} />
         </Switch>
         <Footer />
