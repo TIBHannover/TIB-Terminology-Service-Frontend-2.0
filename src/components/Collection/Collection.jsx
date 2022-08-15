23302f1df7c2243163565699543b8c352fc62207
pooya.oa <pooya.oladazimi@tib.eu>
2022-08-15 10:54:39 +0200

    import the collections logos

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import NFDI4CHEM  from "../../assets/img/NFDI4Chem_Logo_mit_Claim/Web_Word_Powerpoint/png/NFDI4Chem-Logo-Claim_mehrfarbig_schwarz.png";
+import COYPU from "../../assets/img/logo_CoyPu.png";
+import FAIRDS from "../../assets/img/FAIR_DS_Logo_RGB.png";
+import FIDMOVE from "../../assets/img/fidmove_logo.svg";
+import BAUDIGITAL from "../../assets/img/bau-digital_logo210420_RZ_Web_RGB_11.svg";
 
 class Collections extends React.Component{
 
