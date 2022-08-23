6b185cea44baf0e51569faa25da277ec312f4e23
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-08-23 10:46:45 +0200

    search term detail

---
+++ src/components/Search/SearchTermDetail.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class SearchTermDetail extends React.Component{
+    constructor(props){
+        super(props)
+        this.state=({
+
+        })
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default SearchTermDetail
