0ffd7dd4ffa4eea9692f39580b791bced46f53d0
pooya.oa <pooya.oladazimi@tib.eu>
2022-08-29 13:19:59 +0200

    get the list of collection ontologies with the id from json file

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import NFDI4CHEMLogo  from "../../assets/img/NFDI4Chem_Logo_mit_Claim/Web_Word_Powerpoint/png/NFDI4Chem-Logo-Claim_mehrfarbig_schwarz.png";
-import COYPULogo from "../../assets/img/logo_CoyPu.png";
-import FAIRDSLogo from "../../assets/img/FAIR_DS_Logo_RGB.png";
-import FIDMOVELogo from "../../assets/img/fidmove_logo.svg";
-import BAUDIGITALLogo from "../../assets/img/bau-digital_logo210420_RZ_Web_RGB_11.svg";
 import '../layout/Collections.css';
 import {getCollectionOntologies} from '../../api/fetchData';
+import collectionsInfoJson from "../../assets/collectionsText.json";
 
 
 class Collections extends React.Component{
@@ -13,7 +9,6 @@ class Collections extends React.Component{
         super(props);
         this.state = ({
             collectionOntologies: [],
-            collectionIds: {"NFDI4CHEM": {}, "NFDI4ING":{}, "CoyPu":{}, "FID move":{}, "FAIR Data Spaces":{}, "FID BAUdigital":{}}
         });
 
         this.getOntologies = this.getOntologies.bind(this);
@@ -28,8 +23,8 @@ class Collections extends React.Component{
      */
     async getOntologies(){
         let collectionOntologies = {};
-        for (let col in this.state.collectionIds){
-            let ontologies = await getCollectionOntologies([col], false);
+        for (let col in collectionsInfoJson){            
+            let ontologies = await getCollectionOntologies([collectionsInfoJson[col]["id"]], false);            
             collectionOntologies[col] = [];
             for (let onto of ontologies){
                 collectionOntologies[col].push(
@@ -85,18 +80,10 @@ class Collections extends React.Component{
     }
 
 
-    createCollectionList(){
-        let nfdi4ingLogoUrl = "https://terminology.nfdi4ing.de/ts4ing/img/logo_nfdi4ing_rgb_quer_scaled.png";
-        let collectionIds = this.state.collectionIds;
-        collectionIds["NFDI4CHEM"] = {"logo": NFDI4CHEMLogo, "name": "NFDI4Chem Project", "content": "The NFDI4Chem Terminology Service is a repository for chemistry and related ontologies providing a single point of access to the latest ontology versions. You can browse or search the ontologies and look into their terms and relations. The Terminology Service can be used either by humans throught the website or by machines via the TS API. The NFDI4Chem Terminology Service is developed and maintained by TIB - Leibniz Information Centre for Science and Technology. It is part of the service portfolio of the NFDI4Chem consortium within the National Research Data Infrastructure."};
-        collectionIds["NFDI4ING"] = {"logo": nfdi4ingLogoUrl, "name": "NFDI4Ing Project", "content":"NFDI4Ing Terminology Service is a repository for engineering ontologies that aims to provide a single point of access to the latest ontology versions. You can browse engineering ontologies either through this website or via the Rest API. NFDI4Ing TS is developed and maintained by TIB as an extension of the TIB Central Terminology Service ." };
-        collectionIds["CoyPu"] = {"logo": COYPULogo, "name": "CoyPu Project", "content": "The CoyPu collection by TIB Terminology Service provides a well-selected set of ontologies for representing the domain for integrating, structuring, networking, analyzing and evaluating heterogeneous data from economic value networks as well as the industry environment and social context."};
-        collectionIds["FAIR Data Spaces"] = {"logo": FAIRDSLogo, "name": "FAIR Data Spaces Project", "content": "The collection of ontologies used in FAIR Data Spaces project is developed by the demonstrators from the  biodiversity, engineering sciences, and healthcare domain to enable the implementation of a common cloud-based data space for industry and academia within the Gaia-X European data infrastructure.  The focus of this collection is to ensure interoperability and reusability in Gaia-X, especially for organizations."};
-        collectionIds["FID BAUdigital"] = {"logo": BAUDIGITALLogo , "name": "FID Baudigital Project", "content": "The FID BAUdigital collection provides a well-selected set of ontologies and controlled vocabularies related to the domains of civil engineering, architecture and urban planning with a focus on digital methods and technologies"};
-        collectionIds["FID move"] = {"logo": FIDMOVELogo, "name": "FID Move Project", "content": "The FID move collection on TIB Terminology Service provides a well-selected set of ontologies related to the domains of mobility and transportation research."};
+    createCollectionList(){                
         let result = [];
-        for (let col in collectionIds){
-            result.push(this.createCollectionCard(collectionIds[col]["name"], col, collectionIds[col]["logo"], collectionIds[col]["content"]));
+        for (let col in collectionsInfoJson){
+            result.push(this.createCollectionCard(collectionsInfoJson[col]["name"], col, collectionsInfoJson[col]["logo"], collectionsInfoJson[col]["text"]));
         }
 
         return result;
