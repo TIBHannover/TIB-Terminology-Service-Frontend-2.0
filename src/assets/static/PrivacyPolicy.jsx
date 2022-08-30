334ae5b9098222996765b50568f4ce49e636b658
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-08-30 09:34:46 +0200

    removing text-dark

--- src/assets/static/PrivacyPolicy.jsx
+++ src/assets/static/PrivacyPolicy.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import '../../components/layout/static.css'
+
 
 class PrivacyPolicy extends React.Component{
     render(){
       return(
         <section id="main-content-area" role="main">
             <div className="row">
-            <h2 className="privacy_policy text-dark">Privacy Policy</h2>
+            <h2 className="privacy_policy">Privacy Policy</h2>
              <div className="sectionbody">
-             <div className="text-dark">
+             <div>
                <p>Data protection is of high priority for the Technische Informationsbibliothek (TIB). As a general rule, the use of TIB’s services does not require the provision of any personal data. However, the processing of personal data may be required where a data subject wants to use special services via the TIB’s web pages. Where the processing of personal data is required and where there is no legal basis for such processing, we shall obtain the data subject’s consent.<br/><br/>
                   The processing of personal data, such as for example the data subject’s name, address, email address, or telephone number shall always be carried out in accordance with the General Data Protection Regulation (GDPR) and the state and institution-specific data protection rules and regulations applicable to the TIB. This privacy statement serves to inform the public about the nature, scope and purpose of the personal data we collect, use and process, as well as of the rights data subjects are entitled to.<br/><br/>
                   The terms used in this privacy statement are to be understood within the meaning of the <a href="https://eur-lex.europa.eu/legal-content/DE/TXT/?uri=CELEX%3A32016R0679">European General Data Protection Regulation (GDPR)</a>.<br/>
                 </p>
                 <br/>
-            <h3 className="text-dark">Name and address of the controller</h3>
+            <h3>Name and address of the controller</h3>
                <p>The controller under data protection law shall be:
 					 <br /><br/>
 					Technische Informationsbibliothek (TIB)
@@ -33,7 +33,7 @@ class PrivacyPolicy extends React.Component{
 					 <br />
 
                 </p><br/>
-            <h3 className="text-dark">Name and address of the data protection officer</h3>
+            <h3>Name and address of the data protection officer</h3>
                 <p>The data protection officer of the controller shall be:
                      <br /><br/>
                     Elke Brehm
@@ -44,7 +44,7 @@ class PrivacyPolicy extends React.Component{
                      <br />
                 </p>
                 <br/>
-            <h4 className="text-dark">Postal Address:</h4>
+            <h4>Postal Address:</h4>
                 <p>Technische Informationsbibliothek (TIB)
                  <br />
 				data protection officer
@@ -56,7 +56,7 @@ class PrivacyPolicy extends React.Component{
 				Germany
                  <br /><br/>
                 </p>
-            <h4 className="text-dark">Visiting Address:</h4>
+            <h4>Visiting Address:</h4>
                 <p>TIB Conti-Campus
                  <br />
 				K&ouml;nigsworther Platz 1 B
@@ -66,10 +66,10 @@ class PrivacyPolicy extends React.Component{
                 </p><br/>
                 <p>Any data subject may contact our data protection officer directly regarding any and all questions and suggestions regarding data protection at any time.<br /></p>
                 <br/>
-            <h3 className="text-dark">Cookies</h3>
+            <h3>Cookies</h3>
 	            <p>The TIB websites use cookies. Cookies are text files that are placed and stored on a computer system via an Internet browser and serve to render the offer of the TIB more user-friendly, effective and secure.<br/><br/>Most of the cookies used are so-called "session cookies", which are automatically deleted at the end of the visit. Other cookies remain stored on the userʼs terminal device until they delete them. These cookies enable the TIB to recognise the userʼs browser on their next visit.<br/><br/>Users can prevent and permanently object to the setting of cookies by TIB websites at any time by choosing the corresponding settings of the Internet browser used. Furthermore, cookies already set can be deleted at any time with the Internet browser or by other software programs. This is possible in all common Internet browsers. If the data subject deactivates the setting of cookies in the Internet browser used, the TIB web pages may not function properly.</p>
               <br/>
-            <h3 className="text-dark">Web Analytics</h3>
+            <h3>Web Analytics</h3>
 	            <p>The information generated with Matomo about the use of this website is processed and stored exclusively by TIB.</p>
             </div>
             </div>
