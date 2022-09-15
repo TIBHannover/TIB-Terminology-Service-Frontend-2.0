4332a4581ee5f610d8e7493a52d87e4bcff181c1
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-09-15 14:21:05 +0200

    nfdi4chem main logo

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import TIB_Logo from '../../../assets/img/TIB_Logo_W_W.SVG';
+import NFDI4CHEM_Logo from "../../../assets/img/NFDI4Chem_Logo_mit_Claim/Web_Word_Powerpoint/png/NFDI4Chem-Logo-Claim_mehrfarbig_schwarz.png"
 import '../../layout/Common.css';
 import SearchForm from "../../Search/SearchForm";
 
@@ -14,7 +15,12 @@ function Header () {
           </div>
           <div className='row site-header-nav-logo-holder'>
             <div className='col-lg-4 col-md-6'>
+              {process.env.REACT_APP_NFDI4CHEM_MAIN_LOGO === "false" &&
+              <img src={NFDI4CHEM_Logo} alt="nfdi4chem_logo" height={100} width={180}/>
+              }
+              {process.env.REACT_APP_REMOVE_TIB_LOGO === "true" &&
               <img src= {TIB_Logo} alt="tib logo" height={60} width={80} />
+              }
               <a className="main-title" href='/'>TERMINOLOGY SERVICE</a>
             </div>
             <Navbar />
