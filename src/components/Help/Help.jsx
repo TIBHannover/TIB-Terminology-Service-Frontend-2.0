c7b865a0fcdd10fe4103e1e753afd4cd7ce37143
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-09-16 12:35:42 +0200

    nfdi4chem help env

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -4,9 +4,20 @@ class Help extends React.Component{
     render(){
         return(
             <div>
+                {process.env.REACT_APP_SHOW_NFDI4CHEM_HELP === "false" &&
+                <div>
+                  <p>For feedback, enquiries or suggestion about NFDI4Chem Terminology Service or to request a new ontology please contact our NFDI4Chem helpdesk.</p>
+                  <br/>
+                  <p>For announcements relating to our NFDI4Chem project such as new services or events, please sign up to our <a href="https://lists.nfdi.de/postorius/lists/nfdi4chem-announce.lists.nfdi.de/">NFDI4Chem newsletter</a></p>
+                  <br/>
+                  <p>Looking for additional information or help on <a href="https://knowledgebase.nfdi4chem.de/knowledge_base/docs/intro/">research data management</a>, <a href="https://knowledgebase.nfdi4chem.de/knowledge_base/docs/topics/ontology/">ontologies</a>, <a href="https://knowledgebase.nfdi4chem.de/knowledge_base/docs/topics/repositories/">data repositories</a> or <a href="https://knowledgebase.nfdi4chem.de/knowledge_base/docs/topics/eln/">electronic lab notebooks (ELN)</a> for chemistry? The please visit <a href="https://knowledgebase.nfdi4chem.de/knowledge_base/">NFDI4Chem Knowledge Base</a> or our <a href="https://nfdi4chem.de/">NFDI4Chem Website</a></p>
+                  <h4>NFDI4Chem Helpdesk</h4>
+                  <p>Either send an email to <a href="mailto:helpdesk@nfdi4chem.de">helpdesk@nfdi4chem.de</a> or use the Helpdesk <a href="https://nfdi4chem.de/index.php/helpdesk/">contact form</a></p>
+                </div>
+                }
                 <br/>
-                <h2>Help and FAQ</h2>
-                <br/>
+                {process.env.REACT_APP_REMOVE_TIB_HELP === "true" &&
+                <div>
                 <h3>Feedback and Suggestion</h3>
                 <p>For feedback, enquiries or suggestion about TIB Terminology Service or to request a new terminology please visit <a href={"https://gitlab.com/TIBHannover/terminology/tib-terminology-service-issue-tracker"}>our issue tracker</a> (requires sign-in)</p>
                 <br/>
@@ -66,6 +77,8 @@ class Help extends React.Component{
                     <li><a href={"https://www.lod-cloud.net/"}>Linked Open Data Cloud</a></li>
                     <li><a href={"https://archivo.dbpedia.org/list"}>DBpedia Archivo</a></li>
                 </ul>
+                </div>
+                }
 
 
             </div>
