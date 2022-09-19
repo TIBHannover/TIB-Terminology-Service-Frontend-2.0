3075a850e5a405c88a43549aee48a24e69f7ff96
pooya.oa <pooya.oladazimi@tib.eu>
2022-09-19 11:52:31 +0200

    set the logo for nfdi4chem based on env

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import TIB_Logo from '../../../assets/img/TIB_Logo_W_W.SVG';
-import NFDI4CHEM_Logo from "../../../assets/img/NFDI4Chem_Logo_mit_Claim/Web_Word_Powerpoint/png/NFDI4Chem-Logo-Claim_mehrfarbig_schwarz.png"
 import '../../layout/Common.css';
 import SearchForm from "../../Search/SearchForm";
+import collectionsInfoJson from "../../../assets/collectionsText.json";
+
 
 function Header () {
 
@@ -15,13 +16,15 @@ function Header () {
           </div>
           <div className='row site-header-nav-logo-holder'>
             <div className='col-lg-4 col-md-6'>
-              {process.env.REACT_APP_NFDI4CHEM_MAIN_LOGO === "false" &&
-              <img src={NFDI4CHEM_Logo} alt="nfdi4chem_logo" height={100} width={180}/>
+              {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && 
+                <img src={collectionsInfoJson["NFDI4Chem"]["logo"]} alt="nfdi4chem_logo" height={250} width={350}/>
               }
-              {process.env.REACT_APP_REMOVE_TIB_LOGO === "true" &&
-              <img src= {TIB_Logo} alt="tib logo" height={60} width={80} />
-              }
-              <a className="main-title" href='/'>TERMINOLOGY SERVICE</a>
+              {process.env.REACT_APP_PROJECT_ID === "general" && 
+                <span>
+                  <img src= {TIB_Logo} alt="tib logo" height={60} width={80} /> 
+                  <a className="main-title" href='/'>TERMINOLOGY SERVICE</a>
+                </span>
+              }              
             </div>
             <Navbar />
           </div>
