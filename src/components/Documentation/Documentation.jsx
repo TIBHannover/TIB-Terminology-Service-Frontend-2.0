833834ddd50d4a88803873f764efc34252e8aec9
pooya.oa <pooya.oladazimi@tib.eu>
2022-09-21 11:46:11 +0200

    style the doc page

--- src/components/Documentation/Documentation.jsx
+++ src/components/Documentation/Documentation.jsx
@@ -6,10 +6,12 @@ import {nfdi4chemDoc} from './Nfdi4chem_doc';
 class Documentation extends React.Component{
     render(){
         return(
-            <div className='doc-design'>
-                {process.env.REACT_APP_PROJECT_ID === "general" && tibDoc()}
-                {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chemDoc()}
-            </div>
+            <div className='row justify-content-center'>
+                <div className='col-sm-8'>
+                    {process.env.REACT_APP_PROJECT_ID === "general" && tibDoc()}
+                    {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chemDoc()}
+                </div>                
+            </div>            
         )
     }
 }
