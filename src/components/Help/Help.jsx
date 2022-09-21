d509a9b48ed7664e5745f5d4a0a23db8dc3f3e5f
pooya.oa <pooya.oladazimi@tib.eu>
2022-09-21 11:47:05 +0200

    style the help page

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -4,11 +4,13 @@ import {nfdi4chemHelp} from './Nfdi4chem_help';
 
 class Help extends React.Component{
     render(){
-        return(
-           <span>
-                {process.env.REACT_APP_PROJECT_ID === "general" && tibHelp()}
-                {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chemHelp()}
-           </span>
+        return(           
+           <div className='row justify-content-center'>
+                <div className='col-sm-8'>
+                    {process.env.REACT_APP_PROJECT_ID === "general" && tibHelp()}
+                    {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chemHelp()}
+                </div>                
+            </div>      
         )
     }
 }
