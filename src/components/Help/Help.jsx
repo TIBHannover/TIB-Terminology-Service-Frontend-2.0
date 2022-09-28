3f4ee33841f6b95e47c0b6753c700d3f7f59e752
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-09-28 14:11:11 +0200

    flex display

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -5,7 +5,7 @@ import {nfdi4chemHelp} from './Nfdi4chem_help';
 class Help extends React.Component{
     render(){
         return(           
-           <div className='row justify-content-center'>
+           <div className='row justify-content-center doc-design'>
                 <div className='col-sm-8'>
                     {process.env.REACT_APP_PROJECT_ID === "general" && tibHelp()}
                     {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chemHelp()}
