ab695af8849a51e0c6be09204541f0225e38d2fb
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-10-06 13:49:26 +0200

    4chem doc

--- src/components/Documentation/Nfdi4chem_doc.jsx
+++ src/components/Documentation/Nfdi4chem_doc.jsx
@@ -1,10 +1,9 @@
 export function nfdi4chemDoc(){
     let content = [
         <div>
-            <h3>How to use the NFDI4Chem Terminology Service</h3>
-            <p>There are two main ways of browsing the Terminology Service. You can either browse the ontology list available via the ontology menu. You enter the ontology on top level and browse through the tree view to lower levels or you search by a specific term. For this you can use the search box to enter the term that you are looking for. The provided results show all ontologies where the term was found. By using the advanced search, you can further restrict the results to one or multiple ontologies or to a specific 'type'. These types can be class, property or individual. Futhermore, you can restrict searches to one or more ontology. No matter what way of browsing an ontology you choose, your search leads you to the view of a term.</p>
-            <h3>How to use the NFDI4Chem Terminology Service API</h3>
-            <p>Publicly available API commands can also be analyzed and executed from the <strong><a href="http://service.tib.eu/ts4tib/swagger-ui.html">Swagger Documentation</a></strong>. The underlying models can further be viewed through this documentation for a deeper understanding of the API commands. </p>
+            <p>The Terminology Service offers an open API to query and retrieve information about terms and their relations as JSON data for the use in other services such as data repositories or electronic lab notebooks (ELN). Other services can use ontological terms and their unique identifiers to annotate data with metadata, making the (meta)data more findable and interoperable. The API is part of the TIB Central Terminology Service backend. The URL of the API is  http://service.tib.eu/ts4tib/api.</p>
+            <br/>
+            <p>The methodology on how to use this interface is explained in the <a href="https://service.tib.eu/ts4tib/swagger-ui.html">Swagger Documentation</a>The underlying models can also be viewed and executed through this documentation for a deeper understanding of the API commands. </p>
         </div>
     ];
 
