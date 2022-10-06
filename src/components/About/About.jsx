fc49dc2c88acc0b053d84686ca2098053457544d
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2022-10-06 13:45:02 +0200

    4chem about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {generalAbout} from './General_About';
-import {nfdi4chmeAbout} from './NDFI4Chem_About';
+import {nfdi4chemAbout} from './NDFI4Chem_About';
 
 class About extends React.Component{
     render(){
@@ -8,7 +8,7 @@ class About extends React.Component{
             <div className='row justify-content-center doc-design'>
                 <div className='col-sm-8'>
                     {process.env.REACT_APP_PROJECT_ID === "general" && generalAbout()}
-                    {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chmeAbout()}
+                    {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chemAbout()}
                 </div>                
             </div>
         )
