e05b02e1b199d607ffee67d637391d28b9effe5a
pooya.oa <pooya.oladazimi@tib.eu>
2022-11-08 11:51:09 +0100

    style the login modal

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -7,7 +7,7 @@ class Login extends React.Component{
             <span>
                 <a type="button" data-toggle="modal" data-target="#loginModal">Login</a>
 
-                <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+                <div class="modal fade loginModal" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
