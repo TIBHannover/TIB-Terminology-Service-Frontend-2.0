02135e869ef64d97ce9af4add8f8d521bf473ba8
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2023-02-17 13:17:47 +0100

    decoration and path

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -33,8 +33,8 @@ class Home extends React.Component{
            <div className="cc_banner cc_container cc-container--open">
              <a className="cc_btn cc_btn_accept_all" href="" data-cc-event="click:dismiss" rel="nofollow">Got it</a>
              <p className="cc_message">
-               This website only uses technically necessary cookies
-               <a className="cc_more_info" data-cc-if="options.link" href="/PrivacyPolicy">More info</a>
+               This website only uses technically necessary cookies.
+               <a className="cc_more_info" data-cc-if="options.link" href={process.env.REACT_APP_PROJECT_SUB_PATH + "/PrivacyPolicy"}>More info</a>
              </p>
            </div>
         </div>
