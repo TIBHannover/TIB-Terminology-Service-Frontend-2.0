0d9aecb19aa441f6755efb2f1b8e9d784569a0df
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2023-02-20 15:44:48 +0100

    changing animation duration

--- src/App.jsx
+++ src/App.jsx
@@ -83,8 +83,8 @@ function App() {
            <div className="cc_banner cc_container cc-container--open">
              <a className="cc_btn cc_btn_accept_all" href="" data-cc-event="click:dismiss" rel="nofollow">Got it</a>
              <p className="cc_message">
-               This website only uses technically necessary cookies.
-               <a className="cc_more_info" data-cc-if="options.link" href={process.env.REACT_APP_PROJECT_SUB_PATH + "/PrivacyPolicy"}>More info</a>
+               This website only uses technically necessary cookies.&ensp;
+               <a className="cc_more_info" data-cc-if="options.link" href={process.env.REACT_APP_PROJECT_SUB_PATH + "/PrivacyPolicy"}>More info.</a>
              </p>
            </div>
         </div>
