0f8c44c07c58e167d83f30b66a17972177fde505
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2023-02-20 15:31:19 +0100

    changing the destination

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -29,15 +29,6 @@ class Home extends React.Component{
   render(){
       return(
       <div className="row">
-        <div className="cc_banner-wrapper">
-           <div className="cc_banner cc_container cc-container--open">
-             <a className="cc_btn cc_btn_accept_all" href="" data-cc-event="click:dismiss" rel="nofollow">Got it</a>
-             <p className="cc_message">
-               This website only uses technically necessary cookies.
-               <a className="cc_more_info" data-cc-if="options.link" href={process.env.REACT_APP_PROJECT_SUB_PATH + "/PrivacyPolicy"}>More info</a>
-             </p>
-           </div>
-        </div>
         <div className="col-sm-12">
           {process.env.REACT_APP_PROJECT_ID === "general" && createHomePageContent()}
           {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chemHomePage()}
