37a40d38b5dae070d9e1169fcec7ae8b3a4cae65
pooya.oa <pooya.oladazimi@tib.eu>
2023-02-22 14:26:30 +0100

    added skeleton loaidng

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Footer from "./components/common/Footer/Footer";
 import Header from "./components/common/Header/Header";
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
@@ -16,12 +16,20 @@ import About from "./components/About/About";
 import Help from "./components/Help/Help";
 import UsagePage from './components/Usage/Usage';
 import { MatomoWrapper } from './components/Matomo/MatomoWrapper';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 
 // import css file based on the target project
 process.env.REACT_APP_PROJECT_ID === "general" && import ('./components/layout/General_TIB.css');
 process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && import ('./components/layout/Nfdi4chem.css');
 process.env.REACT_APP_PROJECT_ID === "nfdi4ing" && import ('./components/layout/Nfdi4ing.css');
+
+
+
+function InlineWrapperWithMargin({ children }: PropsWithChildren<unknown>) {
+  return [<span style={{ marginRight: '0.5rem' }}>{children}</span>];
+}
 
 
 function App() {
@@ -72,33 +80,52 @@ function App() {
   });
   
 
+  const [loading, setLoading] = useState(true); 
+  useEffect(() => {    
+    setTimeout( () => {
+      setLoading(false);
+    }, 500);
+  }, []);
+
   return (
     <div className="App">   
      <BrowserRouter>
-        <MatomoWrapper>
-        <Header />
-        <div className='container-fluid application-content'>
-          <span id="backend-is-down-message-span"></span>
-          <Switch>
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/"} component={Home}/>            
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/ontologies"} component={OntologyList}/>
-            {process.env.REACT_APP_COLLECTION_TAB_SHOW === "true" &&
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/collections"} component={Collections}/>}
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/ontologies/:ontologyId/:tab?"} component={OntologyDetail}/>
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/api"} component={Documentation}/>
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/docs"} component={Documentation}/>
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/search"} component={SearchResult} />
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/imprint"} component={Imprint}/>
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/PrivacyPolicy"} component={PrivacyPolicy} />
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/TermsOfUse"} component={TermsOfUse}/>
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/AboutApi"} component={AboutApi}/>
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/about"} component={About}/>
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/help"} component={Help}/>
-            {process.env.REACT_APP_PROJECT_ID === "nfdi4ing" && 
-            <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/usage"} component={UsagePage}/>}
-          </Switch>
-        </div>        
-        <Footer />
+        <MatomoWrapper> 
+          <Header />               
+          {loading ? (
+            <Skeleton
+                count={10}
+                wrapper={InlineWrapperWithMargin}
+                inline
+                width={90}
+                marginLeft={20}
+            />
+          ):(
+            <span>
+            <div className='container-fluid application-content'>
+              <span id="backend-is-down-message-span"></span>
+              <Switch>
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/"} component={Home}/>            
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/ontologies"} component={OntologyList}/>
+                {process.env.REACT_APP_COLLECTION_TAB_SHOW === "true" &&
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/collections"} component={Collections}/>}
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/ontologies/:ontologyId/:tab?"} component={OntologyDetail}/>
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/api"} component={Documentation}/>
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/docs"} component={Documentation}/>
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/search"} component={SearchResult} />
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/imprint"} component={Imprint}/>
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/PrivacyPolicy"} component={PrivacyPolicy} />
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/TermsOfUse"} component={TermsOfUse}/>
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/AboutApi"} component={AboutApi}/>
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/about"} component={About}/>
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/help"} component={Help}/>
+                {process.env.REACT_APP_PROJECT_ID === "nfdi4ing" && 
+                <Route exact path={process.env.REACT_APP_PROJECT_SUB_PATH + "/usage"} component={UsagePage}/>}
+            </Switch>
+          </div>
+          <Footer /> 
+          </span>         
+          )}              
         </MatomoWrapper>
       </BrowserRouter>
     </div>
