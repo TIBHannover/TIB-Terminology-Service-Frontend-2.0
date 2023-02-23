b278bab4fdcb02263f20ec681f4bbf36018a7e9c
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2023-02-23 10:59:43 +0100

    adjusting the href

--- src/components/common/CookieBanner/CookieBanner.jsx
+++ src/components/common/CookieBanner/CookieBanner.jsx
@@ -21,7 +21,7 @@ class CookieBanner extends React.Component{
     return(
       <div className="cc_banner-wrapper">
         <div onClick={this.handleClick} className="cc_banner cc_container cc-container--open">
-          <a className="cc_btn cc_btn_accept_all" href="" data-cc-event="click:dismiss" rel="nofollow">Got it</a>
+          <p className="cc_btn cc_btn_accept_all" rel="nofollow">Got it</p>
           <p className="cc_message">
             This website only uses technically necessary cookies.&ensp;
           <a className="cc_more_info" data-cc-if="options.link" href={process.env.REACT_APP_PROJECT_SUB_PATH + "/PrivacyPolicy"}>More info.</a>
