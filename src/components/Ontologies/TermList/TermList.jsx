6d841107261b3f079add9ccbb91501522d4bc241
pooya.oa <pooya.oladazimi@tib.eu>
2023-03-07 13:54:33 +0100

    make the term label anchor for the tree view

--- src/components/Ontologies/TermList/TermList.jsx
+++ src/components/Ontologies/TermList/TermList.jsx
@@ -30,10 +30,16 @@ class TermList extends React.Component{
     createList(){
         let result = [];
         let listOfterms = this.state.listOfTerms;
+        let baseUrl = process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/';
         for (let term of listOfterms){
+            let termTreeUrl = baseUrl + encodeURIComponent(term['ontology_name']) + '/terms?iri=' + encodeURIComponent(term['iri']);
             result.push(
                 <tr>
-                    <td>{term['label']}</td>
+                    <td>
+                        <a className="table-list-label-anchor"  href={termTreeUrl} target="_blank">
+                            {term['label']}
+                        </a>                        
+                    </td>
                     <td>{term['short_form']}</td>
                     <td>{term['description'] ? term['description'] : ""}</td>
                 </tr>
@@ -50,8 +56,8 @@ class TermList extends React.Component{
 
     render(){
         return(
-            <div>
-                <table class="table table-striped">
+            <div className="tree-view-container">
+                <table class="table table-striped term-list-table">
                     <thead>
                         <tr>                
                             <th scope="col">Label</th>
