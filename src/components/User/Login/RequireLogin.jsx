e90b062e7eaf3f5925b3070bac7e137ee17c3b82
pooya.oa <pooya.oladazimi@tib.eu>
2023-03-14 10:38:13 +0100

    fix the issue with RequireLoginRoute function

--- src/components/User/Login/RequireLogin.jsx
+++ src/components/User/Login/RequireLogin.jsx
@@ -1,20 +1,19 @@
+import { useState } from "react";
 import { isLogin } from "./Auth"
 import {Route} from 'react-router-dom';
 import Login from "./Login";
 
+
 function RequireLoginRoute({component: Component, ...rest}){
+    const [loginStatus, setLoginStatus] = useState(null);
     isLogin().then((resp) => {
-        if(!resp){
-            return <Login isModal={false}></Login>
-        }
+        setLoginStatus(resp);
     });
 
+    if (!loginStatus) {        
+        return <Login isModal={false}></Login>;
+    }
     return (<Route component={Component} {...rest}/> )
-     
-    // if (!isLogin()) {        
-    //     return <Login isModal={false}></Login>
-    // }
-    // return (<Route component={Component} {...rest}/> )
 }
 
 export default RequireLoginRoute;
