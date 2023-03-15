e98d5aece040b8f6dd3779dddd806507f80f2220
pooya.oa <pooya.oladazimi@tib.eu>
2023-03-15 11:38:10 +0100

    added state selection dropdown

--- src/components/Ontologies/IssueList/IssueList.jsx
+++ src/components/Ontologies/IssueList/IssueList.jsx
@@ -8,20 +8,29 @@ import {createLabelTags,
 
 
 const CLOSE_ISSUE_STATE = "closed";
-const OPEN_ISSUE_STATE = "open"
-const ALL_ISSUE_STATE = "all"
+const OPEN_ISSUE_STATE = "open";
+const ALL_ISSUE_STATE = "all";
+const OPEN_ISSUE_ID = 1;
+const CLOSE_ISSUE_ID = 2;
+const ALL_ISSUE_ID = 3;
 
 
 class IssueList extends React.Component{
     constructor(props){
         super(props);
         this.state = ({
-            listOfIssues: [],
+            listOfIssuesToRender: [],
+            listOfOpenIssues: [],
+            listOfClosedIssues: [],
+            listOfAllIsses: [],
             waiting: true,
-            contentForRender: "",            
+            contentForRender: "",
+            selectedTypeId: OPEN_ISSUE_ID    
         });
         this.setComponentData = this.setComponentData.bind(this);
         this.createIssuesList = this.createIssuesList.bind(this);
+        this.createIssueStateDropDown = this.createIssueStateDropDown.bind(this);
+        this.handleIssueStateChange = this.handleIssueStateChange.bind(this);
         this.gitHubController = new GithubController();
     }
 
@@ -37,22 +46,44 @@ class IssueList extends React.Component{
         let username = "StroemPhi";
         let ontology = this.props.ontology;
         let issueTrackerUrl = typeof(ontology.config.tracker) !== "undefined" ? ontology.config.tracker : null;
-        let listOfIssues = [];        
+        let listOfIssuesToRender = [];        
         if(issueTrackerUrl){
-            listOfIssues = await this.gitHubController.getOntologyIssueListForUser(issueTrackerUrl, username, OPEN_ISSUE_STATE);
+            listOfIssuesToRender = await this.gitHubController.getOntologyIssueListForUser(issueTrackerUrl, username, OPEN_ISSUE_STATE);
         }
         this.setState({
-            listOfIssues: listOfIssues,
+            listOfIssuesToRender: listOfIssuesToRender,
+            listOfOpenIssues: listOfIssuesToRender,
             waiting: false
         }, () => {
             this.createIssuesList();
         });
     }
 
+
+    handleIssueStateChange(e){
+
+    }
+
+
+    createIssueStateDropDown(){
+        return [
+            <div className='col-sm-4 form-inline'>
+                <div class="form-group">
+                    <label for="issue-state-types" className='col-form-label'>State</label>
+                    <select className='site-dropdown-menu' id="issue-state-types" value={this.state.selectedTypeId} onChange={this.handleIssueStateChange}>
+                    <option value={OPEN_ISSUE_ID} key={OPEN_ISSUE_ID}>Open</option>
+                    <option value={CLOSE_ISSUE_ID} key={CLOSE_ISSUE_ID}>Closed</option>
+                    <option value={ALL_ISSUE_ID} key={ALL_ISSUE_ID}>All</option>                    
+                    </select>  
+                </div>                                                                                
+            </div>
+        ];
+    }
+
     
     
     createIssuesList(){
-        let listOfIssues = this.state.listOfIssues;
+        let listOfIssues = this.state.listOfIssuesToRender;
         let result = [];
         for(let issue of listOfIssues){            
             result.push(
@@ -85,11 +116,14 @@ class IssueList extends React.Component{
                         <Login isModal={false} />
                     }
                     {this.state.waiting && <div className="isLoading"></div>}
-                    {userIsLoginByLocalStorage() &&
+                    {userIsLoginByLocalStorage() && !this.state.waiting &&
                         <div className="row">
                             <div className="col-sm-8">
                                 {this.state.contentForRender}
-                            </div>                            
+                            </div>
+                            <div className="col-sm-4">
+                                {this.createIssueStateDropDown()}
+                            </div>                      
                         </div>
                     }
                 </div>
