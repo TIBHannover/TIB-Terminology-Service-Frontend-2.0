7b39841361dc1736457599c6a53a686899f6ba03
pooya.oa <pooya.oladazimi@tib.eu>
2023-03-16 16:48:17 +0100

    refactor Documentation.jsx

--- src/components/Documentation/Documentation.jsx
+++ src/components/Documentation/Documentation.jsx
@@ -2,26 +2,22 @@ import React from 'react';
 import {tibDoc} from './TIB_doc';
 import {nfdi4chemDoc} from './Nfdi4chem_doc';
 import {nfdi4IngDoc} from './Nfdi4ing_doc';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import Toolkit from '../common/Toolkit';
 
 
 class Documentation extends React.Component{
     render(){
         return(
-          <HelmetProvider>
-                <div>
-                  <Helmet>
-                    <title>Documentation</title>
-                  </Helmet>
-                </div>             
+          <span>
+            {Toolkit.createHelmet("Documentation")}
             <div className='row justify-content-center doc-design'>
                 <div className='col-sm-8'>
                     {process.env.REACT_APP_PROJECT_ID === "general" && tibDoc()}
                     {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chemDoc()}
                     {process.env.REACT_APP_PROJECT_ID === "nfdi4ing" && nfdi4IngDoc()}
                 </div>                
-            </div>  
-          </HelmetProvider>          
+            </div>
+          </span>                    
         )
     }
 }
