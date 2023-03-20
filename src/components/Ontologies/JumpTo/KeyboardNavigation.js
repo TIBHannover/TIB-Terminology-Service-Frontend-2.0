cfd0d45def33c42a2f852d814f6f874c47b0779a
pooya.oa <pooya.oladazimi@tib.eu>
2023-03-20 13:08:44 +0100

    implement the arrow down for jumpto

--- src/components/Ontologies/JumpTo/KeyboardNavigation.js
+++ src/components/Ontologies/JumpTo/KeyboardNavigation.js
@@ -1,8 +1,47 @@
 
 
 export function keyboardNavigationForJumpto(event){
-    let jumtoItems = document.getElementsByClassName('jumpto-autosuggest-item');
-    if(jumtoItems.length !== 0 && (event.code === "ArrowDown" || event.code === "ArrowUp")){
-        event.preventDefault();        
+    let jumtoItems = document.getElementsByClassName('jumpto-result-text');
+    if(jumtoItems.length === 0){
+        return false;
     }    
+    try{
+        if(event.key === "ArrowDown"){
+            event.preventDefault();
+            performArrowDown();
+        }
+    }
+    catch(e){
+        console.info(e);
+    }
+}
+
+
+function performArrowDown(){
+    let selectedElement = document.getElementsByClassName('selected-by-arrow-key');
+    if(selectedElement.length === 0){
+        selectTheFirstItem();
+    }   
+    else{                
+        selectTheNextSibling(selectedElement[0]);
+    }
+}
+
+
+
+function selectTheNextSibling(selectedElement){
+    let nextSiblng = selectedElement?.parentNode?.parentNode?.nextSibling?.firstChild.firstChild;
+    selectedElement.classList.remove('selected-by-arrow-key');
+    if(nextSiblng && nextSiblng.classList.contains('jumpto-result-text')){                    
+        nextSiblng.classList.add('selected-by-arrow-key');
+    }
+    else{
+        selectTheFirstItem();
+    }
+}
+
+
+function selectTheFirstItem(){
+    let jumtoItems = document.getElementsByClassName('jumpto-result-text');
+    jumtoItems[0].classList.add('selected-by-arrow-key');
 }
