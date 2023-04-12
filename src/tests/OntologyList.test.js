2a0302d93b6165c63f63059c62028181564c6185
pooya.oa <pooya.oladazimi@tib.eu>
2023-04-12 12:35:48 +0200

    give history as props to the component

--- src/tests/OntologyList.test.js
+++ src/tests/OntologyList.test.js
@@ -1,11 +1,13 @@
 import {render, screen} from '@testing-library/react';
+import { createMemoryHistory } from "history";
 import OntologyList from '../components/Ontologies/OntologyList/OntologyList';
 
 
 
 test("List has ontology", async ()=>{
-    const propsLocation = {pathname: "/ts/ontologies", search: "?page=1"} 
-    render(<OntologyList location={propsLocation} />);
+    const propsLocation = {pathname: "/ts/ontologies", search: "?page=1"};
+    const history = createMemoryHistory();
+    render(<OntologyList location={propsLocation} history={history} />);
          
     const testOntologyDescription = "The base ontology of the test Standard.";
     await screen.findByText(testOntologyDescription);    
