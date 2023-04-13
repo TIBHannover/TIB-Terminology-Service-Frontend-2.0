ac7b1bd37f71a2957bde2ec312c8edb55889faa3
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2023-04-13 09:53:24 +0200

    dropdown for search results

--- src/components/Search/SearchResult.jsx
+++ src/components/Search/SearchResult.jsx
@@ -404,8 +404,19 @@ createSearchResultList () {
               }              
             </div>
             <div className='col-sm-8' id="search-list-grid">
-              {this.state.searchResult.length > 0 && <h3 className="text-dark">{this.state.totalResultsCount + ' results found for "' + this.state.enteredTerm + '"'   }</h3>}              
-              {this.state.searchResult.length > 0 && this.createSearchResultList()} 
+              {this.state.searchResult.length > 0 && <h3 className="text-dark">{this.state.totalResultsCount + ' results found for "' + this.state.enteredTerm + '"'   }</h3>}  
+                 <div className='col-sm-4' id="search-list-grid">       
+                      <div class="form-group">
+                        <label for="list-result-per-page" className='col-form-label'>Results Per Page</label>
+                          <select className='site-dropdown-menu list-result-per-page-dropdown-menu' id="list-result-per-page" value={this.state.pageSize} onChange={this.handlePageSizeDropDownChange}>
+                            <option value={10} key="10">10</option>
+                            <option value={20} key="20">20</option>
+                            <option value={30} key="30">30</option>
+                            <option value={40} key="40">40</option>
+                          </select>  
+                       </div>
+                      </div> 
+              {this.state.searchResult.length > 0 && this.createSearchResultList()}              
               {this.state.searchResult.length > 0 && 
                 <Pagination 
                   clickHandler={this.handlePagination} 
