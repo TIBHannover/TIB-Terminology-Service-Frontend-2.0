f3914f6bfd6ec4a8136155b9799939f830db5347
pooya.oa <pooya.oladazimi@tib.eu>
2023-04-18 13:30:12 +0200

    fix the application content height issue when header collapse

--- src/components/Ontologies/OntologyPage/helpers.jsx
+++ src/components/Ontologies/OntologyPage/helpers.jsx
@@ -73,19 +73,22 @@ function collpaseSiteHeader(){
     let collpaseIcone = document.getElementsByClassName('header-collpase-icon')[0];
     let collpaseButton = document.getElementsByClassName('header-collapse-btn')[0];
     let ontologyBannerContainer = document.getElementsByClassName('ont-info-bar')[0];
+    let applicationContent = document.getElementsByClassName('application-content')[0];
     if(collpaseIcone.classList.contains('fa-angle-double-up')){
         siteHeader.style.display = 'none';
         ontologyBannerContainer.style.display = 'none';
         collpaseIcone.classList.remove('fa-angle-double-up');
         collpaseIcone.classList.add('fa-angle-double-down');
         collpaseButton.style.marginTop = '-30px';
+        applicationContent.style.minHeight = '800px';
     }
     else{
         siteHeader.style.display = 'block';
         ontologyBannerContainer.style.display = 'block';
         collpaseIcone.classList.add('fa-angle-double-up');
         collpaseIcone.classList.remove('fa-angle-double-down');
-        collpaseButton.style.marginTop = '';     
+        collpaseButton.style.marginTop = '';
+        applicationContent.style.minHeight = '500px';
     }
     
 }
