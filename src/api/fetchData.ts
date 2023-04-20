fd0d1289037bac66d8658154c6ffee3a431c76df
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2023-04-20 13:04:02 +0200

    fixing the parent term

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -413,6 +413,7 @@ export async function getSubClassOf(nodeIri:string, ontologyId:string){
     result += "<ul>"
     for(let i=0; i < parentRes["terms"].length; i++){
       result += '<li>'+ '<a href=' + process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/' + ontologyId + '/terms?iri=' + encodeURIComponent(parentRes["terms"][i]["iri"]) + '>' + parentRes["terms"][i]["label"] + '</a>'+ '</li>';
+    }
     if (typeof(res) !== "undefined"){
       for(let i=0; i < res["strings"].length; i++){ 
       result += '<li>'+ res["strings"][i]["content"] +'</li>';     
@@ -420,8 +421,7 @@ export async function getSubClassOf(nodeIri:string, ontologyId:string){
     result += "<ul>"
     
   }
-  return result;
- }
+  return result; 
 }
 
 /**
