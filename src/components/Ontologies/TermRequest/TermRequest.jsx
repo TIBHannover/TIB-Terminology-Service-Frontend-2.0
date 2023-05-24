6fd0abbb485c3765b0701f51b0f485ad31dc1ee9
pooya.oa <pooya.oladazimi@tib.eu>
2023-05-24 12:02:53 +0200

    added title and issue type

--- src/components/Ontologies/TermRequest/TermRequest.jsx
+++ src/components/Ontologies/TermRequest/TermRequest.jsx
@@ -2,7 +2,49 @@ import React from "react";
 
 
 
+const GENERIC_ISSUE_ID = 1;
+const TERM_REQUEST_ISSUE_ID = 2;
+
+
 class TermRequest extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = ({
+            issueType: GENERIC_ISSUE_ID
+        });
+        this.createGenericIssueFields = this.createGenericIssueFields.bind(this);
+        this.createIssueTypeDropDown = this.createIssueTypeDropDown.bind(this);
+        this.changeIssueType = this.changeIssueType.bind(this);
+
+    }
+
+
+    createGenericIssueFields(){
+
+    }
+
+    createIssueTypeDropDown(){
+        return [            
+            <div class="form-group">
+                <label for="issue-types" className='col-form-label'>Issue Type</label>
+                <select className='site-dropdown-menu list-result-per-page-dropdown-menu' id="issue-types" value={this.state.issueType} onChange={this.changeIssueType}>
+                    <option value={GENERIC_ISSUE_ID} key={GENERIC_ISSUE_ID}>General</option>
+                    <option value={TERM_REQUEST_ISSUE_ID} key={TERM_REQUEST_ISSUE_ID}>Term Request</option>                            
+                </select>  
+            </div>            
+        ];
+    }
+
+
+    changeIssueType(e){        
+        this.setState({
+            issueType: e.target.value
+        });
+    }
+
+    
+
+
 
 
 
@@ -14,7 +56,7 @@ class TermRequest extends React.Component{
             </button>
             
             <div class="modal" id="term-request-modal">
-                <div class="modal-dialog">
+                <div class="modal-dialog modal-xl">
                     <div class="modal-content">                    
                         <div class="modal-header">
                             <h4 class="modal-title">File an issue for this Ontology</h4>
@@ -23,6 +65,14 @@ class TermRequest extends React.Component{
                         
                         <div class="modal-body">
                             <p>You can file a new issue here. The issue can have a General Topic or a new Term Request</p>
+                            
+                            <div className="row">
+                                <div className="col-sm-8">
+                                    {this.createIssueTypeDropDown()}
+                                    <label className="required_input" for="issueTitle">Issue Title</label>
+                                    <input type="text" class="form-control" id="issueTitle" placeholder="Enter Issue Title"></input>
+                                </div>
+                            </div>                            
                         </div>
                         
                         <div class="modal-footer">
