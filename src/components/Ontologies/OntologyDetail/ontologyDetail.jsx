75c17fe96a8e7bf9e5e8e552d9c9838f2b384fc7
Vatsal Limbachia <vatsalkumar.limbachia@tib.eu>
2023-05-24 11:06:26 +0200

    tab change

--- src/components/Ontologies/OntologyDetail/ontologyDetail.jsx
+++ src/components/Ontologies/OntologyDetail/ontologyDetail.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 
+
+const DETAIL_TAB_ID = 0;
+
 class ontologyDetail extends React.Component{
     constructor(props){
         super(props)
         this.state= ({
 
         })
+        this.tabChange = this.tabChange.bind(this);
     }
+
+    /**
+     * Handle the tab change in the ontology detail Top menu
+     *
+     * @param {*} e
+     * @param {*} value
+     */
+  tabChange = (e, v) => {
+    try{
+      let selectedTabId = e.target.dataset.value;         
+      this.setState({
+        waiting: true
+      });
+  
+      this.setState({
+        activeTab: parseInt(selectedTabId),
+        waiting: false
+      });
+    } 
+    catch(e){
+      this.setState({
+        activeTab: DETAIL_TAB_ID,
+        waiting: false
+      });
+    }      
+  }
 }
 
 export default ontologyDetail;
