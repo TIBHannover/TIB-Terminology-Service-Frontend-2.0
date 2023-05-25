0de865a73019c0a5f3f246d4a476576e7a11ee6f
pooya.oa <pooya.oladazimi@tib.eu>
2023-05-25 13:55:01 +0200

    fetch notes list

--- src/components/Ontologies/Notes/Notes.jsx
+++ src/components/Ontologies/Notes/Notes.jsx
@@ -2,13 +2,37 @@ import React from "react";
 
 
 class OntologyNotes extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = ({
+           notesList: []
+        });
+
+        this.getNotesForOntology = this.getNotesForOntology.bind(this);
+    }
+
+
+    getNotesForOntology(){
+        let ontologyId = this.props.ontology;
+        let url = process.env.REACT_APP_TEST_BACKEND_URL + '/getNotes/' + ontologyId;
+        fetch(url).then((resp) => resp.json())
+        .then((data) => {        
+            this.setState({
+                notesList: data['result']
+            });
+        });
+    }
+
+    componentDidMount(){
+        this.getNotesForOntology();
+    }
 
 
 
     render(){
         return (
             <div className="tree-view-container term-list-container">
-                
+
             </div>
         );
     }
