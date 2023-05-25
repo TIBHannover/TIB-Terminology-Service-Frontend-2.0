5aa7a9b81dcfa6116e4f45ecef10f5d9f146aec7
Erhun Giray TUNCAY <48091473+giraygi@users.noreply.github.com>
2023-05-25 18:11:15 +0200

    experimented parameters of a carousel for the first parapraph on home page for #270

--- src/components/Home/Nfdi4ing_home.jsx
+++ src/components/Home/Nfdi4ing_home.jsx
@@ -22,45 +22,45 @@ export function nfdi4ingHomePage() {
                   </b>
                 </h4>
               </div>
-              <div style={{ display: 'block', width: 400}}>
+              <div>
               <Carousel variant="dark">
                 <Carousel.Item interval={15000}>
-                  <img
-                    className="d-block w-100"
-                    src="https://wallpapers.com/images/featured/7sn5o1woonmklx1h.jpg"
-                    alt="Image One" 
-                  />
                   <Carousel.Caption>
                     {/* <h3>Label for first slide</h3> */}
                     <p><small>In principle everyone that works with a need for the use of unambiguous language. Like organisations and institutions or in science and research. The NFDI4Ing Terminology Services is a free service offer that everyone can use via this frontend or our REST machine-to-machine communication interface.</small></p>
                   </Carousel.Caption>
+                  <img
+                    className="d-block wh-100"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/512px-Solid_white.svg.png?20220303184432"
+                    alt="Image One"
+                  />
                 </Carousel.Item>
                 <Carousel.Item interval={15000}>
-                  <img
-                    className="d-block w-100"
-                    src="https://wallpapers.com/images/featured/7sn5o1woonmklx1h.jpg"
-                    alt="Image Two"
-                  />
                   <Carousel.Caption>
                     {/* <h3>Label for second slide</h3> */}
                     <p><b>Science and Research</b> Among other things, for the development of innovative data and knowledge management systems (research data management, publishing), as a source for training new language models (natural language processing) and much more.</p>
                   </Carousel.Caption>
+                  <img
+                    className="d-block wh-100"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/512px-Solid_white.svg.png?20220303184432"
+                    alt="Image Two"
+                  />
                 </Carousel.Item>
                 <Carousel.Item interval={15000}>
-                  <img
-                    className="d-block w-100"
-                    src="https://wallpapers.com/images/featured/7sn5o1woonmklx1h.jpg"
-                    alt="Image Three"
-                  />
                   <Carousel.Caption>
                     
                     <p><b>Industry</b> I order to find and agree on a common vocabulary. E.g. in medicine for the clear designation of diseases, symptoms or therapies, but also for the billing system.</p>
                   </Carousel.Caption>
+                  <img
+                    className="d-block wh-100"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/512px-Solid_white.svg.png?20220303184432"
+                    alt="Image Three"
+                  />
                 </Carousel.Item>
                 <Carousel.Item interval={15000}>
                   <img
-                    className="d-block w-100"
-                    src="https://wallpapers.com/images/featured/7sn5o1woonmklx1h.jpg"
+                    className="d-block wh-100"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/512px-Solid_white.svg.png?20220303184432"
                     alt="Image Three"
                   />
                   <Carousel.Caption>
