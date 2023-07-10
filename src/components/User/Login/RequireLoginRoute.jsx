8aa98b6f4cbc5cbf28d4149c6e4c645e0707d247
pooya.oa <pooya.oladazimi@tib.eu>
2023-07-10 15:46:34 +0200

    fix the issue with late loading the login check

--- src/components/User/Login/RequireLoginRoute.jsx
+++ src/components/User/Login/RequireLoginRoute.jsx
@@ -6,15 +6,20 @@ import Login from "./TS/Login";
 
 function RequireLoginRoute({component: Component, ...rest}){
     const [loginStatus, setLoginStatus] = useState(null);    
+    const [loginProcessFinished, setLoginProcessFinished] = useState(false);    
     isLogin().then((resp) => {
         setLoginStatus(resp);
+        setLoginProcessFinished(true);
     });
 
-    
-    if (!loginStatus) {           
+    if(!loginProcessFinished){
+        return "";
+    }
+
+    else if (!loginStatus && loginProcessFinished) {           
         return <Login isModal={false}></Login>;
     }
-    
+        
     return (<Route component={Component} {...rest}/> )
 }
 
