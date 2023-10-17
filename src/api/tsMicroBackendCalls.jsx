9e40e11c21381c283377e8ba42f15fa7c3f10fac
pooya.oa <pooya.oladazimi@tib.eu>
2023-10-17 14:41:45 +0200

    change the issue list call to GET request

--- src/api/tsMicroBackendCalls.jsx
+++ src/api/tsMicroBackendCalls.jsx
@@ -78,17 +78,16 @@ export async function getNoteDetail({noteId}){
 
 export async function getOntologyGithubIssueList(ontologyIssueTrackerUrl, issueState, resultCountPerPage=10, pageNumber=1){
     try{ 
-        let urlPath = ontologyIssueTrackerUrl.split("https://github.com/")[1];
-        let data = new FormData();
-        data.append("issuePath", urlPath);
-        data.append("issueState", issueState);
-        data.append("size", resultCountPerPage);
-        data.append("pageNumber", pageNumber);
-        let endpoint = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/github/issuelist' 
+        let urlPath = ontologyIssueTrackerUrl.split("https://github.com/")[1];        
+        let endpoint = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/github/issuelist?';
+        endpoint += ("path=" + encodeURIComponent(urlPath));
+        endpoint += ("&state=" + issueState);
+        endpoint += ("&size=" + resultCountPerPage);
+        endpoint += ("&page=" + pageNumber);
         let header = {};
         header["X-TS-Frontend-Id"] = process.env.REACT_APP_PROJECT_ID;
         header["X-TS-Frontend-Token"] = process.env.REACT_APP_MICRO_BACKEND_TOKEN;
-        let result = await fetch(endpoint, {method: 'POST', headers:header, body: data});
+        let result = await fetch(endpoint, {headers:header});
         result = await result.json();
         result = result['_result']            
         return result.issues;
