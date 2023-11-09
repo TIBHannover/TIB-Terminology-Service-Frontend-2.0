10955f611a3e3e3df5301f7fd7827f2cb9d7654b
pooya.oa <pooya.oladazimi@tib.eu>
2023-11-09 12:04:40 +0100

    refactoring TermList component

---
+++ src/components/Ontologies/TermList/RenderTermList.jsx
@@ -0,0 +1,196 @@
+import {useEffect, useState} from "react";
+import {getSubClassOf, getEqAxiom} from '../../../api/fetchData';
+import Pagination from "../../common/Pagination/Pagination";
+import JumpTo from "../JumpTo/Jumpto";
+
+
+
+export const RenderTermList = (props) => {
+    const [tableBodyContent, setTableBodyContent] = useState("");
+
+
+    async function createList(){
+        let result = [];
+        let listOfterms = props.listOfTerms;
+        let baseUrl = process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/';
+        for (let term of listOfterms){
+            let termTreeUrl = baseUrl + encodeURIComponent(term['ontology_name']) + '/terms?iri=' + encodeURIComponent(term['iri']);
+            let subclassOfText = await getSubClassOf(term['iri'], term['ontology_name']);
+            let equivalentToText = await getEqAxiom(term['iri'], term['ontology_name']);
+            result.push(
+                <tr>
+                    <td className="label-col">
+                        <a className="table-list-label-anchor"  href={termTreeUrl} target="_blank">
+                            {term['label']}
+                        </a>                        
+                    </td>
+                    <td className="id-col">{term['short_form']}</td>
+                    <td className="des-col">{term['description'] ? term['description'] : ""}</td>
+                    <td className="alt-term-col">{term['annotation']['alternative term'] ? term['annotation']['alternative term'] : "N/A" }</td>
+                    <td className="sub-class-col"><span  dangerouslySetInnerHTML={{ __html: subclassOfText }} /></td>
+                    <td className="eqv-col"><span  dangerouslySetInnerHTML={{ __html: equivalentToText }} /></td>
+                    <td className="ex-usage-col">{term['annotation']['example of usage'] ? term['annotation']['example of usage'] : "N/A" }</td>
+                    <td className="see-also-col">{term['annotation']['seeAlso'] ? term['annotation']['seeAlso'] : "N/A" }</td>
+                    <td className="contrib-col">{setContributorField(term)}</td>
+                    <td className="comment-col">{term['annotation']['comment'] ? term['annotation']['comment'] : "N/A" }</td>
+                </tr>
+            );
+        }
+        
+        if(result.length !== 0){
+            setTableBodyContent(result);
+            props.setTableIsLoading(false);
+        }                
+    }
+
+
+    useEffect(() => {
+        createList();
+    }, [props.listOfTerms]);
+
+
+    return(
+        <div className="tree-view-container list-container">
+                <div className="row">
+                    <div className="col-sm-4">
+                        <div className="termlist-jumpto-container">
+                            <JumpTo                        
+                                ontologyId={props.ontologyId}                                
+                                isSkos={props.isSkos}
+                                componentIdentity={props.componentIdentity}
+                                containerBootstrapClass="col-sm-12"
+                            />
+                        </div>                    
+                    </div>
+                    <div className="col-sm-2">
+                        {!props.iri && 
+                            <div className='form-inline result-per-page-dropdown-container'>
+                                <div class="form-group">
+                                <label for="list-result-per-page" className='col-form-label'>Result Per Page</label>
+                                <select className='site-dropdown-menu list-result-per-page-dropdown-menu' id="list-result-per-page" value={props.pageSize} onChange={props.handlePageSizeDropDownChange}>
+                                    <option value={20} key="20">20</option>
+                                    <option value={30} key="30">30</option>
+                                    <option value={40} key="40">40</option>
+                                    <option value={50} key="50">50</option>
+                                </select>  
+                                </div>                                                                                
+                            </div>
+                        }
+                        {props.iri &&                            
+                            <button className='btn btn-secondary btn-sm tree-action-btn' onClick={props.resetList}>Show All Classes</button> 
+                        }
+                    </div>
+                    <div className="col-sm-3 text-right number-of-result-text-container">
+                        <b>{"Showing " + (props.pageNumber * props.pageSize + 1) + " - " + ((props.pageNumber + 1) * props.pageSize) + " of " + props.totalNumberOfTerms + " Classes"}</b>
+                    </div>
+                    <div className="col-sm-3">
+                        <Pagination 
+                            clickHandler={props.handlePagination} 
+                            count={props.pageCount()}
+                            initialPageNumber={props.pageNumber + 1}
+                        />
+                    </div>
+                </div>                 
+                <div className="row class-list-tablle-holder">                                      
+                    <table class="table table-striped term-list-table class-list-table" id="class-list-table">
+                        {createShowColumnsTags()}                        
+                        {createClassListTableHeader()}
+                        <tbody>
+                            {props.tableIsLoading && <div className="is-loading-term-list isLoading"></div>}
+                            {!props.tableIsLoading && tableBodyContent}               
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+    );
+}
+
+
+
+
+function createClassListTableHeader(){
+    return [
+        <thead>
+            <tr>                
+                <th scope="col" className="label-col">Label <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th>
+                <th scope="col" className="id-col">ID <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th>
+                <th scope="col" className="des-col">Description <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th>
+                <th scope="col" className="alt-term-col">Alternative Term <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th>
+                <th scope="col" className="sub-class-col">SubClass Of <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th>
+                <th scope="col" className="eqv-col">Equivalent to <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th>
+                <th scope="col" className="ex-usage-col">Example of usage <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th>
+                <th scope="col" className="see-also-col">See Also <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th>
+                <th scope="col" className="contrib-col">Contributor <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th>
+                <th scope="col" className="comment-col">Comment <a onClick={hideTableColumn}><i className="fa fa-eye-slash hidden-fa"></i></a></th> 
+            </tr>
+        </thead>
+    ];
+}
+
+
+function createShowColumnsTags(){
+    return [
+        <span>
+            <div class="show-hidden-column" data-column="label-col" id="label-col-show" onClick={showTableColumn}>Label <i className="fa fa-eye"></i></div>
+            <div class="show-hidden-column" data-column="id-col" id="id-col-show" onClick={showTableColumn}>ID <i className="fa fa-eye"></i></div>
+            <div class="show-hidden-column" data-column="des-col" id="des-col-show" onClick={showTableColumn}>Description <i className="fa fa-eye"></i></div>
+            <div class="show-hidden-column" data-column="alt-term-col" id="alt-term-col-show" onClick={showTableColumn}>Alternative Term <i className="fa fa-eye"></i></div>
+            <div class="show-hidden-column" data-column="sub-class-col" id="sub-class-col-show" onClick={showTableColumn}>SubClass Of <i className="fa fa-eye"></i></div>
+            <div class="show-hidden-column" data-column="eqv-col" id="eqv-col-show" onClick={showTableColumn}>Equivalent to <i className="fa fa-eye"></i></div>
+            <div class="show-hidden-column" data-column="ex-usage-col" id="ex-usage-col-show" onClick={showTableColumn}>Example of usage <i className="fa fa-eye"></i></div>
+            <div class="show-hidden-column" data-column="see-also-col" id="see-also-col-show" onClick={showTableColumn}>See Also <i className="fa fa-eye"></i></div>
+            <div class="show-hidden-column" data-column="contrib-col" id="contrib-col-show" onClick={showTableColumn}>Contributor <i className="fa fa-eye"></i></div>
+            <div class="show-hidden-column" data-column="comment-col" id="comment-col-show" onClick={showTableColumn}>Comment <i className="fa fa-eye"></i></div>
+        </span>        
+    ];
+}
+
+
+
+function setContributorField(term){
+    if (term['annotation']['contributor']){
+        return term['annotation']['contributor'];
+    }
+    else if(term['annotation']['term editor']){
+        return term['annotation']['term editor'];
+    }
+    else if(term['annotation']['creator']){
+        return term['annotation']['creator'];
+    }
+    else{
+        return "N/A";
+    }
+}
+
+
+
+function hideTableColumn(e){
+    let columnClassName = e.target.parentNode.parentNode.className
+    let tableCells = document.getElementsByClassName(columnClassName);
+    for(let cell of tableCells){
+        cell.style.display = "none";
+    }
+    try{document.getElementById(columnClassName + '-show').style.display = "inline-block";}
+    catch(e){return true}        
+   
+}
+
+
+
+function showTableColumn(e){
+    let columnClassName = "";
+    if(e.target.tagName === "I"){
+        columnClassName = e.target.parentNode.dataset.column;
+        e.target.parentNode.style.display = "none";
+    }
+    else{
+        columnClassName = e.target.dataset.column;
+        e.target.style.display = "none";
+    }
+    
+    let tableCells = document.getElementsByClassName(columnClassName);
+    for(let cell of tableCells){
+        cell.style.display = "";
+    }    
+   
+}
--- src/components/Ontologies/TermList/TermList.jsx
+++ src/components/Ontologies/TermList/TermList.jsx
@@ -1,9 +1,9 @@
 import {useEffect, useState} from "react";
 import { useHistory } from "react-router";
-import {getListOfTerms, getNodeByIri, getSubClassOf, getEqAxiom} from '../../../api/fetchData';
-import Pagination from "../../common/Pagination/Pagination";
-import JumpTo from "../JumpTo/Jumpto";
-import {createClassListTableHeader, setContributorField, createShowColumnsTags} from './hekpers';
+import {getListOfTerms, getNodeByIri} from '../../../api/fetchData';
+import Toolkit from "../../common/Toolkit";
+import { RenderTermList } from "./RenderTermList";
+
 
 
 const DEFAULT_PAGE_SIZE = 20;
@@ -15,19 +15,16 @@ const TermList = (props) => {
     let sizeInUrl = url.searchParams.get('size');
     let iriInUrl = url.searchParams.get('iri');        
     pageNumberInUrl = !pageNumberInUrl ? 1 : parseInt(pageNumberInUrl);
-    let internalSize = localStorage.getItem('termListPageSize') ? localStorage.getItem('termListPageSize') : DEFAULT_PAGE_SIZE;
+    let internalSize =  Toolkit.getVarInLocalSrorageIfExist('termListPageSize', DEFAULT_PAGE_SIZE);
     sizeInUrl = !sizeInUrl ? internalSize : parseInt(sizeInUrl);
 
     const [pageNumber, setPageNumber] = useState(pageNumberInUrl - 1);
     const [pageSize, setPageSize] = useState(sizeInUrl);
     const [listOfTerms, setListOfTerms] = useState([]);
-    const [totalNumberOfTerms, setTotalNumberOfTerms] = useState(0);
-    const [lastLoadedUrl, setLastLoadedUrl] = useState("");
+    const [totalNumberOfTerms, setTotalNumberOfTerms] = useState(0);    
     const [mode, setMode] = useState("terms");
-    const [iri, setIri] = useState(iriInUrl);
-    const [tableBodyContent, setTableBodyContent] = useState("");
-    const [tableIsLoading, setTableIsLoading] = useState(true);
-    const [urlProfile, setUrlProfile] = useState({iri:null, pageNumber: 0, pageSize: localStorage.getItem('termListPageSize') ? localStorage.getItem('termListPageSize') : 20});
+    const [iri, setIri] = useState(iriInUrl);    
+    const [tableIsLoading, setTableIsLoading] = useState(true);    
     const history = useHistory();
 
 
@@ -43,8 +40,7 @@ const TermList = (props) => {
         }
         
         setListOfTerms(listOfTermsAndStats['results']);
-        setTotalNumberOfTerms(listOfTermsAndStats['totalTermsCount']);        
-        setLastLoadedUrl(window.location.href);             
+        setTotalNumberOfTerms(listOfTermsAndStats['totalTermsCount']);                    
         storePageSizeInLocalStorage(pageSize);        
     }
 
@@ -101,7 +97,7 @@ const TermList = (props) => {
 
 
     function resetList(){
-        let size = localStorage.getItem('termListPageSize') ? localStorage.getItem('termListPageSize') : DEFAULT_PAGE_SIZE;
+        let size = Toolkit.getVarInLocalSrorageIfExist('termListPageSize', DEFAULT_PAGE_SIZE);
         setIri(null);
         setPageNumber(0);
         setPageSize(size);
@@ -156,109 +152,6 @@ const TermList = (props) => {
     );
 
 }
-
-
-
-const RenderTermList = (props) => {
-    const [tableBodyContent, setTableBodyContent] = useState("");
-
-
-    async function createList(){
-        let result = [];
-        let listOfterms = props.listOfTerms;
-        let baseUrl = process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/';
-        for (let term of listOfterms){
-            let termTreeUrl = baseUrl + encodeURIComponent(term['ontology_name']) + '/terms?iri=' + encodeURIComponent(term['iri']);
-            let subclassOfText = await getSubClassOf(term['iri'], term['ontology_name']);
-            let equivalentToText = await getEqAxiom(term['iri'], term['ontology_name']);
-            result.push(
-                <tr>
-                    <td className="label-col">
-                        <a className="table-list-label-anchor"  href={termTreeUrl} target="_blank">
-                            {term['label']}
-                        </a>                        
-                    </td>
-                    <td className="id-col">{term['short_form']}</td>
-                    <td className="des-col">{term['description'] ? term['description'] : ""}</td>
-                    <td className="alt-term-col">{term['annotation']['alternative term'] ? term['annotation']['alternative term'] : "N/A" }</td>
-                    <td className="sub-class-col"><span  dangerouslySetInnerHTML={{ __html: subclassOfText }} /></td>
-                    <td className="eqv-col"><span  dangerouslySetInnerHTML={{ __html: equivalentToText }} /></td>
-                    <td className="ex-usage-col">{term['annotation']['example of usage'] ? term['annotation']['example of usage'] : "N/A" }</td>
-                    <td className="see-also-col">{term['annotation']['seeAlso'] ? term['annotation']['seeAlso'] : "N/A" }</td>
-                    <td className="contrib-col">{setContributorField(term)}</td>
-                    <td className="comment-col">{term['annotation']['comment'] ? term['annotation']['comment'] : "N/A" }</td>
-                </tr>
-            );
-        }
-        
-        if(result.length !== 0){
-            setTableBodyContent(result);
-            props.setTableIsLoading(false);
-        }                
-    }
-
-
-    useEffect(() => {
-        createList();
-    }, [props.listOfTerms]);
-
-
-    return(
-        <div className="tree-view-container list-container">
-                <div className="row">
-                    <div className="col-sm-4">
-                        <div className="termlist-jumpto-container">
-                            <JumpTo                        
-                                ontologyId={props.ontologyId}                                
-                                isSkos={props.isSkos}
-                                componentIdentity={props.componentIdentity}
-                                containerBootstrapClass="col-sm-12"
-                            />
-                        </div>                    
-                    </div>
-                    <div className="col-sm-2">
-                        {!props.iri && 
-                            <div className='form-inline result-per-page-dropdown-container'>
-                                <div class="form-group">
-                                <label for="list-result-per-page" className='col-form-label'>Result Per Page</label>
-                                <select className='site-dropdown-menu list-result-per-page-dropdown-menu' id="list-result-per-page" value={props.pageSize} onChange={props.handlePageSizeDropDownChange}>
-                                    <option value={20} key="20">20</option>
-                                    <option value={30} key="30">30</option>
-                                    <option value={40} key="40">40</option>
-                                    <option value={50} key="50">50</option>
-                                </select>  
-                                </div>                                                                                
-                            </div>
-                        }
-                        {props.iri &&                            
-                            <button className='btn btn-secondary btn-sm tree-action-btn' onClick={props.resetList}>Show All Classes</button> 
-                        }
-                    </div>
-                    <div className="col-sm-3 text-right number-of-result-text-container">
-                        <b>{"Showing " + (props.pageNumber * props.pageSize + 1) + " - " + ((props.pageNumber + 1) * props.pageSize) + " of " + props.totalNumberOfTerms + " Classes"}</b>
-                    </div>
-                    <div className="col-sm-3">
-                        <Pagination 
-                            clickHandler={props.handlePagination} 
-                            count={props.pageCount()}
-                            initialPageNumber={props.pageNumber + 1}
-                        />
-                    </div>
-                </div>                 
-                <div className="row class-list-tablle-holder">                                      
-                    <table class="table table-striped term-list-table class-list-table" id="class-list-table">
-                        {createShowColumnsTags()}                        
-                        {createClassListTableHeader()}
-                        <tbody>
-                            {props.tableIsLoading && <div className="is-loading-term-list isLoading"></div>}
-                            {!props.tableIsLoading && tableBodyContent}               
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-    );
-}
-
 
 
 export default TermList;
