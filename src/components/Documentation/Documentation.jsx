260522fe84d2a8ca0eaa620155cb8965105279c5
pooya.oa <pooya.oladazimi@tib.eu>
2024-01-02 12:25:46 +0100

    make the Doc page customizable

--- src/components/Documentation/Documentation.jsx
+++ src/components/Documentation/Documentation.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import {tibDoc} from './TIB_doc';
-import {nfdi4chemDoc} from './Nfdi4chem_doc';
-import {nfdi4IngDoc} from './Nfdi4ing_doc';
 import Toolkit from '../common/Toolkit';
 import '../layout/documentationPage.css';
+import { renderDocumentation } from './DocContentPage';
 
 
 class Documentation extends React.Component{
@@ -12,11 +10,8 @@ class Documentation extends React.Component{
           <span>
             {Toolkit.createHelmet("Documentation")}
             <div className='row justify-content-center doc-design'>
-                <div className='col-sm-12'>
-                    {/* {process.env.REACT_APP_PROJECT_ID === "general" && tibDoc()}
-                    {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" && nfdi4chemDoc()}
-                    {process.env.REACT_APP_PROJECT_ID === "nfdi4ing" && nfdi4IngDoc()} */}
-                    {nfdi4chemDoc()}
+                <div className='col-sm-12'>                   
+                    {renderDocumentation()}
                 </div>                
             </div>
           </span>                    
