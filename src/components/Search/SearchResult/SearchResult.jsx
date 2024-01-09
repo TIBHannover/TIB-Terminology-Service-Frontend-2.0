eb62e4f50c8f1105626d8ca628384a7c576e6271
pooya.oa <pooya.oladazimi@tib.eu>
2024-01-09 10:00:31 +0100

    refactor search result stage 2

--- src/components/Search/SearchResult/SearchResult.jsx
+++ src/components/Search/SearchResult/SearchResult.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import queryString from 'query-string';
@@ -48,9 +47,9 @@ const SearchResult = (props) => {
 
 
   async function search(){    
-    let rangeStart = (pageNumber - 1) * this.state.pageSize
-    let baseUrl = process.env.REACT_APP_SEARCH_URL + `?q=${this.state.searchQuery}` + `&start=${rangeStart}` + `&groupField=iri` + "&rows=" + this.state.pageSize;
-    let totalResultBaseUrl = process.env.REACT_APP_SEARCH_URL + `?q=${this.state.searchQuery}&groupField=iri`;    
+    let rangeStart = (pageNumber - 1) * pageSize
+    let baseUrl = process.env.REACT_APP_SEARCH_URL + `?q=${searchQuery}` + `&start=${rangeStart}` + `&groupField=iri` + "&rows=" + pageSize;
+    let totalResultBaseUrl = process.env.REACT_APP_SEARCH_URL + `?q=${searchQuery}&groupField=iri`;    
     
 
     if(selectedOntologies.length !== 0){
@@ -93,97 +92,40 @@ const SearchResult = (props) => {
   }
 
 
-  
-
-
-
-
-}
-
-
-
-
-
-
-
-class SearchResult1 extends React.Component{
-  
-
-
-
-
-/**
- * Handle the exact search when chosen by the user (Exact match)
- */
-async handleExact(){
-  if(this.state.searchQuery.length > 0){
-    let searchUrl = process.env.REACT_APP_SEARCH_URL + `?q=${this.state.searchQuery}` + "&exact=true&rows=" + this.state.pageSize; 
-    let exactResult = await fetch(searchUrl, {mode: 'cors', headers: apiHeaders(),})
-    exactResult = (await exactResult.json())['response']['docs'];
-    this.setState({
-      searchResult: exactResult,
-      isLoaded: true 
-    })
+  function alsoInResult(iri){
+    let expanded = expandedResults;
+    let otherOntologies = [];
+    if(typeof(expanded) !== "undefined"){
+      for(let key in expanded){
+        if(key === iri){
+          let allTags = expanded[key]['docs']
+               for(let j=0; j < allTags.length; j++){              
+                 otherOntologies.push(
+                  <div className='also-in-ontologies'>
+                    {AlsoInHelpers(allTags[j])} 
+                  </div>                                             
+                 )
+               }            
+        }
+      }     
+    } 
+    return otherOntologies;
   }
-}
 
-/**
- * Handle the obsolete search term when chosen by the user
- */
-async handleObsolete(){
-  if(this.state.searchQuery.length > 0){
-    let searchUrl = process.env.REACT_APP_SEARCH_URL + `?q=${this.state.searchQuery}` + "&obsoletes=true&rows=" + this.state.pageSize;
-    let obsoletesResult = await fetch(searchUrl, {mode: 'cors', headers: apiHeaders(),})
-    obsoletesResult = (await obsoletesResult.json())['response']['docs'];
-    this.setState({
-      searchResult: obsoletesResult,
-      isLoaded: true 
-    })
+
+  function handleAlsoResult(iri){
+    if((alsoInResult(iri)).length !== 0){
+      return true;
+    }
+    else{
+      return false
+    }
   }
-}
 
 
 
-/**
- * Displaying 'Also in' in search result items
- */
-
-alsoInResult(iri){
-  let expanded = this.state.expandedResults;
-  let otherOntologies = [];
-  if(typeof(expanded) !== "undefined"){
-    for(let key in expanded){
-      if(key === iri){
-        let allTags = expanded[key]['docs']
-             for(let j=0; j < allTags.length; j++){              
-               otherOntologies.push(
-                <div className='also-in-ontologies'>
-                  {AlsoInHelpers(allTags[j])} 
-                </div>                                             
-               )
-             }            
-      }
-    }     
-  } 
-  return otherOntologies;
-}
-
-handleAlsoResult(iri){
-  if((this.alsoInResult(iri)).length !== 0){
-    return true;
-  }
-  else{
-    return false
-  }
-}
-
-/**
-  * Create the search results list view
-  *
-  * @returns
-  */
-createSearchResultList () {   
-      let searchResultItem = this.state.searchResult;
+  function createSearchResultList () {   
+      let searchResultItem = searchResult;
       const SearchResultList = [];
       for (let i = 0; i < searchResultItem.length; i++) {
         SearchResultList.push(
@@ -198,16 +140,16 @@ createSearchResultList () {
               </div>
               <div className="searchresult-ontology">
                 <span><b>Ontology: </b></span>
-                <a className='btn btn-default ontology-button' href={process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/' + this.state.searchResult[i]['ontology_name']} target="_blank">
+                <a className='btn btn-default ontology-button' href={process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/' + searchResult[i]['ontology_name']} target="_blank">
                   {searchResultItem[i].ontology_prefix}
                 </a>
               </div>
               <br/>
-              {this.handleAlsoResult(searchResultItem[i].iri) &&
+              {handleAlsoResult(searchResultItem[i].iri) &&
               <div className = "also-in-design">
                   <b>Also in:</b>
                 </div>}
-               {this.alsoInResult(searchResultItem[i].iri)}
+              {alsoInResult(searchResultItem[i].iri)}
             </div>            
           </div>   
         )
@@ -215,13 +157,11 @@ createSearchResultList () {
       return SearchResultList
   }
 
-  /**
-    * Update the url based on facet values
-    */
-   updateURL(ontologies, types, collections, obsoletes, exact){
-    let targetQueryParams = queryString.parse(this.props.location.search + this.props.location.hash);
+
+  function updateURL(ontologies, types, collections, obsoletes, exact){
+    let targetQueryParams = queryString.parse(props.location.search + props.location.hash);
     let page = targetQueryParams.page;
-    this.props.history.push(window.location.pathname);
+    props.history.push(window.location.pathname);
     let currentUrlParams = new URLSearchParams();
     for(let typ of types){
       currentUrlParams.append('type', typ);
@@ -234,7 +174,7 @@ createSearchResultList () {
     for(let col of collections){
       currentUrlParams.append('collection', col);
     }
-    currentUrlParams.append('page', this.state.pageNumber);
+    currentUrlParams.append('page', pageNumber);
 
     if(obsoletes){
       currentUrlParams.set('obsoletes', true);
@@ -243,97 +183,42 @@ createSearchResultList () {
     if(exact){
       currentUrlParams.set('exact', true);
     }
-    this.props.history.push(window.location.pathname + "?q=" + this.state.searchQuery + "&" + currentUrlParams.toString());
+    props.history.push(window.location.pathname + "?q=" + searchQuery + "&" + currentUrlParams.toString());
 
    }
 
-  /**
-    * Handles the page size values from dropdown
-    * @param {*} value
-    */
-  handlePageSizeDropDownChange(e){
+
+  function handlePageSizeDropDownChange(e){
     let size = parseInt(e.target.value);
-    let pageNumber = this.state.pageNumber + 1;
-    this.setState({
+    let pageNumber = pageNumber + 1;
+    setState({
       pageSize: size
-    });
-    this.setComponentData();
+    });    
   }
 
 
-  /**
-     * Handle the click on the pagination
-     * @param {*} value
-     */
-   handlePagination (value) {
-    this.setState({
+  function  handlePagination (value) {
+    setState({
       pageNumber: value,
       paginationReset: false
     }, () => {
-      this.updateURL(this.state.selectedOntologies,this.state.selectedTypes,this.state.selectedCollections)
-      this.paginationHandler()
+      updateURL(selectedOntologies,selectedTypes,selectedCollections)
+      paginationHandler()
     })
   }
 
 
-
-  /**
-     * Count the number of pages for the pagination
-     * @returns
-     */
-  pageCount () {    
-    if (isNaN(Math.ceil(this.state.totalResultsCount / this.state.pageSize))){
+  function pageCount () {    
+    if (isNaN(Math.ceil(totalResultsCount / pageSize))){
       return 0;
     }
-    return (Math.ceil(this.state.totalResultsCount / this.state.pageSize))
+    return (Math.ceil(totalResultsCount / pageSize))
   }
 
 
-  /**
-    * Handle the pagination change. This function has to be passed to the Pagination component
-    */
-   async paginationHandler () {
-    let ontologies = this.state.selectedOntologies;
-    let types = this.state.selectedTypes;
-    let collections = this.state.selectedCollections;
-    let rangeCount = (this.state.pageNumber - 1) * this.state.pageSize;
-    let baseUrl = process.env.REACT_APP_SEARCH_URL + `?q=${this.state.searchQuery}` + `&start=${rangeCount}` + "&rows=" + this.state.pageSize;
-    if(ontologies.length > 0 || types.length > 0 || collections.length > 0){
-      // Add other selected filters 
-        ontologies.forEach(item => {
-          baseUrl = baseUrl + `&ontology=${item.toLowerCase()}`
-        }) 
-        types.forEach(item => {
-          baseUrl = baseUrl + `&type=${item.toLowerCase()}`
-        })
-       let collectionOntologies = await getCollectionOntologies(collections, false);
-        collectionOntologies.forEach(onto => {
-          baseUrl = baseUrl + `&ontology=${onto["ontologyId"].toLowerCase()}`
-        });
-        this.updateURL(ontologies, types, collections)        
-     }
-     else{
-      // No extra filter. Get the target project ontologies
-      let collectionOntologies = await getCollectionOntologies([process.env.REACT_APP_PROJECT_NAME], false);
-      collectionOntologies.forEach(onto => {
-        baseUrl = baseUrl + `&ontology=${onto["ontologyId"].toLowerCase()}`
-      });
-     }
 
-    let targetUrl = await fetch(baseUrl,{
-      mode: 'cors',
-      headers: apiHeaders(),
-    })
-    let resultJson = (await targetUrl.json());
-    let newResults = resultJson['response']['docs']
-    this.setState({
-      searchResult: newResults
-    })
-     
-  }
-
-  handleOntoDelete(){
-    let ontologies = this.state.selectedOntologies
+  function handleOntoDelete(){
+    let ontologies = selectedOntologies
     let params = new URLSearchParams(window.location.search)
     for(let i=0; i< ontologies.length; i++){
       params.delete('ontology', ontologies[i])
@@ -342,8 +227,9 @@ createSearchResultList () {
     window.location.replace(window.location.pathname + "?" + params.toString());
   }
 
-  handleTypDelete(){
-    let types = this.state.selectedTypes;
+  
+  function handleTypDelete(){
+    let types = selectedTypes;
     let params = new URLSearchParams(window.location.search)
     for(let i=0; i< types.length; i++){
       params.delete('type', types[i])
@@ -352,8 +238,9 @@ createSearchResultList () {
     window.location.replace(window.location.pathname + "?" + params.toString());
   }
 
-  handleColDelete(){
-    let collections = this.state.selectedCollections;
+  
+  function handleColDelete(){
+    let collections = selectedCollections;
     let params = new URLSearchParams(window.location.search)
     for(let i=0; i< collections.length; i++){
       params.delete('collection', collections[i])
@@ -362,19 +249,18 @@ createSearchResultList () {
     window.location.replace(window.location.pathname + "?" + params.toString());
   }
 
-  /**
-    * facet buttons listing as per facet selections
-    */
-  facetButton(){
-    let ontologies = this.state.selectedOntologies;
-    let types = this.state.selectedTypes;
-    let collections = this.state.selectedCollections;
+
+
+  function facetButton(){
+    let ontologies = selectedOntologies;
+    let types = selectedTypes;
+    let collections = selectedCollections;
     let facetRow = [];
     for(let onto of ontologies){     
         facetRow.push(
           <div className='col-sm-2'>
             <a className='facet-btn' href>{onto}
-              <i onClick={this.handleOntoDelete} className="fa fa-remove remove-btn \n"></i>
+              <i onClick={handleOntoDelete} className="fa fa-remove remove-btn \n"></i>
             </a>
           </div>
         )     
@@ -383,7 +269,7 @@ createSearchResultList () {
         facetRow.push(
           <div className='col-sm-2'>
             <a className='facet-btn' href>{typ}
-              <i onClick={this.handleTypDelete} className="fa fa-remove remove-btn \n"></i>
+              <i onClick={handleTypDelete} className="fa fa-remove remove-btn \n"></i>
             </a>
           </div>
         )
@@ -393,7 +279,7 @@ createSearchResultList () {
           facetRow.push(
             <div className='col-sm-2'>
               <a className='facet-btn' href>{col}
-                <i onClick={this.handleColDelete} className="fa fa-remove remove-btn \n"></i>
+                <i onClick={handleColDelete} className="fa fa-remove remove-btn \n"></i>
               </a>
             </div>
           )
@@ -404,10 +290,9 @@ createSearchResultList () {
 
   }
 
-  
-  componentDidMount(){
-    if(!this.state.isLoaded && !this.state.isFiltered){      
-      this.setComponentData();
+
+  useEffect(() => {
+    if(!isLoaded && !isFiltered){            
       let cUrl = window.location.href;        
       if(cUrl.includes("q=")){
         cUrl = cUrl.split("q=")[1];
@@ -416,63 +301,62 @@ createSearchResultList () {
         cUrl = cUrl.replaceAll("+", " ");
         document.getElementById("s-field").value = cUrl;
       }       
-    }    
-  }
+    } 
+  }, []);
 
 
 
-  render(){
-    return(
-      <div className='row justify-content-center search-result-container' id="searchterm-wrapper">
-        {Toolkit.createHelmet(this.state.searchQuery)}        
-        <div className='col-sm-11'>            
-          <div className='row'>
-            <div className='col-sm-4'>          
-              {(this.state.searchResult.length > 0 || (this.state.searchResult.length === 0 && this.state.facetIsSelected)) &&
-                <Facet
-                  facetData = {this.state.facetFields}
-                  handleChange = {this.runSearch}             
-                  selectedCollections = {this.state.selectedCollections}
-                  selectedOntologies = {this.state.selectedOntologies}
-                  selectedTypes = {this.state.selectedTypes}
-                />
-              }              
+  return(
+    <div className='row justify-content-center search-result-container' id="searchterm-wrapper">
+      {Toolkit.createHelmet(searchQuery)}        
+      <div className='col-sm-11'>            
+        <div className='row'>
+          <div className='col-sm-4'>          
+            {(searchResult.length > 0 || (searchResult.length === 0 && facetIsSelected)) &&
+              <Facet
+                facetData = {facetFields}
+                handleChange = {runSearch}             
+                selectedCollections = {selectedCollections}
+                selectedOntologies = {selectedOntologies}
+                selectedTypes = {selectedTypes}
+              />
+            }              
+          </div>
+          <div className='col-sm-8' id="search-list-grid">
+            {searchResult.length > 0 && <h3 className="text-dark">{totalResultsCount + ' results found for "' + searchQuery + '"'   }</h3>}
+               <div className='row'>
+                 {facetButton()} 
+                </div>  
+               <div className='row'>                                                      
+                  <div className='col-sm-4 search-dropdown'>     
+                    <div class="form-group">
+                      <label for="list-result-per-page" className='col-form-label'>Results Per Page</label>
+                        <select className='site-dropdown-menu list-result-per-page-dropdown-menu dropdown-colour' id="list-result-per-page" value={pageSize} onChange={handlePageSizeDropDownChange}>
+                          <option value={10} key="10">10</option>
+                          <option value={20} key="20">20</option>
+                          <option value={30} key="30">30</option>
+                          <option value={40} key="40">40</option>
+                        </select>  
+                     </div>
+                  </div> 
+                </div>
+               
+            {searchResult.length > 0 && createSearchResultList()}              
+            {searchResult.length > 0 && 
+              <Pagination 
+                clickHandler={handlePagination} 
+                count={pageCount()}
+                initialPageNumber={pageNumber}          
+              />
+            } 
+
+            {searchResult.length === 0 && <h3 className="text-dark">{'No search results for "' + searchQuery + '"'   }</h3>} 
             </div>
-            <div className='col-sm-8' id="search-list-grid">
-              {this.state.searchResult.length > 0 && <h3 className="text-dark">{this.state.totalResultsCount + ' results found for "' + this.state.searchQuery + '"'   }</h3>}
-                 <div className='row'>
-                   {this.facetButton()} 
-                  </div>  
-                 <div className='row'>                                                      
-                    <div className='col-sm-4 search-dropdown'>     
-                      <div class="form-group">
-                        <label for="list-result-per-page" className='col-form-label'>Results Per Page</label>
-                          <select className='site-dropdown-menu list-result-per-page-dropdown-menu dropdown-colour' id="list-result-per-page" value={this.state.pageSize} onChange={this.handlePageSizeDropDownChange}>
-                            <option value={10} key="10">10</option>
-                            <option value={20} key="20">20</option>
-                            <option value={30} key="30">30</option>
-                            <option value={40} key="40">40</option>
-                          </select>  
-                       </div>
-                    </div> 
-                  </div>
-                 
-              {this.state.searchResult.length > 0 && this.createSearchResultList()}              
-              {this.state.searchResult.length > 0 && 
-                <Pagination 
-                  clickHandler={this.handlePagination} 
-                  count={this.pageCount()}
-                  initialPageNumber={this.state.pageNumber}          
-                />
-              } 
+          </div>
+      </div>                
+    </div>
+  );
 
-              {this.state.searchResult.length === 0 && <h3 className="text-dark">{'No search results for "' + this.state.searchQuery + '"'   }</h3>} 
-              </div>
-            </div>
-        </div>                
-      </div>
-    )
-  }
 }
 
 export default SearchResult;
