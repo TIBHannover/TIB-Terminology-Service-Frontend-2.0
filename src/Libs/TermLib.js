40f575dbf102ef0f5b9c58197bc91a60e2a04f96
pooya.oa <pooya.oladazimi@tib.eu>
2024-01-23 13:11:52 +0100

    refactor metadata builder for term rable

--- src/Libs/TermLib.js
+++ src/Libs/TermLib.js
@@ -1,3 +1,6 @@
+import Toolkit from "./Toolkit";
+
+
 
 class TermLib{
     
@@ -9,7 +12,7 @@ class TermLib{
             the term ontology_name metadata.
         */  
         if(!ontology_name){
-            return "";
+            return null;
         } 
 
         let targetHref = process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/' + encodeURIComponent(ontology_name);
@@ -29,6 +32,74 @@ class TermLib{
             </a>            
         ];                
     }
+
+
+
+    static createAlsoInTags(term, termType){
+        if(term.alsoIn && term.alsoIn.length !== 0){
+            let alsoInList = [];
+            for (let ontologyId of term.alsoIn){
+              if(term.originalOntology !== ontologyId){
+                alsoInList.push(TermLib.createOntologyTagWithTermURL(ontologyId, term.iri, termType));
+              }
+            }
+            return alsoInList;        
+        }
+        return null;
+    }
+
+
+
+    static createTermDiscription(term){
+        if(term.isIndividual && term.description){
+            // individual description structure is different
+            let result = [];     
+            for(let desc of term.description){
+              result.push(<p>{desc}</p>);
+            }
+            return result;
+        }
+        else if(term.obo_definition_citation){
+            let result = [];
+            for(let cite of term.obo_definition_citation){
+              result.push(
+                <div>
+                  {Toolkit.transformLinksInStringToAnchor(cite['definition'])}
+                  <br/>
+                   [<span className="node-metadata-label">Reference</span>:  <a href={cite['oboXrefs'][0]['url']} target="_blank">{cite['oboXrefs'][0]['url'] ? cite['oboXrefs'][0]['url'] : 'N/A'}</a>]
+                </div>
+              );
+            }
+            return result;           
+        }
+        else if(term.description ){
+            return term.description[0];
+        }
+
+        return null;
+    }
+
+
+
+    static createInstancesListForClass(term){
+        // instances are the individuals which are a type of this class.
+        if(!term.instancesList){
+            return null;
+        }        
+        let result = [];  
+        for(let instance of term.instancesList){
+          let individualUrl = process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/' + instance['ontology_name'] + "/individuals?iri=" + encodeURIComponent(instance['iri']);
+          result.push(
+            <li>        
+              <a href={individualUrl} target='_blank'>
+                {instance['label']}
+              </a>
+            </li>
+          );
+        }  
+        return result;
+    }
+
 }
 
 export default TermLib;
--- src/components/Ontologies/TermDetail/helpers.jsx
+++ src/components/Ontologies/TermDetail/helpers.jsx
@@ -4,93 +4,34 @@ import TermLib from '../../../Libs/TermLib';
 
 
 /**
- * Create the metadata for a class detail table
+ * Create the metadata for a class/individual detail table
  * The boolean in each value indicates that the metadata is a link or not.
  */
- export function classMetaData(object, termType){
+ export function classMetaData(term, termType){
     let metadata = {}
-
-    metadata['Label'] = [object.label, false];
-
-    if(object.originalOntology){
-      metadata['Imported From'] = [TermLib.createOntologyTagWithTermURL(object.originalOntology, object.iri, termType), false];
-    }
-
-    if(object.alsoIn && object.alsoIn.length !== 0){
-      let alsoInList = [];
-      for (let ontologyId of object.alsoIn){
-        if(object.originalOntology !== ontologyId){
-          alsoInList.push(TermLib.createOntologyTagWithTermURL(ontologyId, object.iri, termType));
-        }        
-      }
-      metadata['Also In'] = [alsoInList, false];
-    }
-
-    metadata['Synonyms'] = [object.synonyms ? (object.synonyms).join(',\n') : "", false];
-    metadata['CURIE'] = [object.obo_id, false];
-    metadata['Term ID'] = [object.short_form, false];    
-    metadata['Description'] = [object.description ? object.description[0] : "", false];
-    metadata['fullIRI'] = [object.iri, true];
-    metadata['SubClass Of'] = [object.subClassOf, false];
+    metadata['Label'] = [term.label, false];
+    metadata['Imported From'] = [TermLib.createOntologyTagWithTermURL(term.originalOntology, term.iri, termType), false];
+    metadata['Also In'] = [TermLib.createAlsoInTags(term, termType), false];
+    metadata['Synonyms'] = [term.synonyms ? (term.synonyms).join(',\n') : "", false];
+    metadata['CURIE'] = [term.obo_id, false];
+    metadata['Term ID'] = [term.short_form, false];    
+    metadata['Description'] = [TermLib.createTermDiscription(term), false];
+    metadata['fullIRI'] = [term.iri, true];
+    metadata['SubClass Of'] = [term.subClassOf, false];    
+    metadata['Equivalent to'] = [term.eqAxiom, false];
+    metadata['Used in axiom'] = [term.relations, false];
+    metadata['Instances'] = [TermLib.createInstancesListForClass(term), false];
     
-    if(object.eqAxiom !== "N/A"){
-      metadata['Equivalent to'] = [object.eqAxiom, false];
-    }
-    
-    if(formatText("Used in axiom", object.relations, false) !== "N/A" && formatText("Used in axiom", object.relations, false).length !== 0){
-      metadata['Used in axiom'] = [object.relations, false];
-    }
-
-    if(object.instancesList &&  object.instancesList.length !== 0){
-      metadata['Instances'] = [object.instancesList, false];
-    }
-
-    if(object.obo_definition_citation){
-      let result = [];
-      for(let cite of object.obo_definition_citation){
-        result.push(
-          <div>
-            {Toolkit.transformLinksInStringToAnchor(cite['definition'])}
-            <br/>
-             [<span className="node-metadata-label">Reference</span>:  <a href={cite['oboXrefs'][0]['url']} target="_blank">{cite['oboXrefs'][0]['url'] ? cite['oboXrefs'][0]['url'] : 'N/A'}</a>]
-          </div>
-        )
-      }
-      metadata['Description'] = [result, false];
-    }
-    
-    if(object.annotation){
-      for(let key in object.annotation){
+    if(term.annotation){
+      // add custom annotation fields. Metadata key can be anything
+      for(let key in term.annotation){
         metadata[key] = [];
         let value = [];
-        for(let annot of object.annotation[key]){
+        for(let annot of term.annotation[key]){
           value.push(annot);
         }
         metadata[key] = [value.join(',\n'), false];
       }    
-    }
-    
-    if(object.type && object.isIndividual){     
-      let typeMtadataValue = [];      
-      for(let type of object.type){
-        typeMtadataValue.push(
-          <span>
-            <a href={process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/' + type['ontology_name'] + '/terms?iri=' + type['iri']} target={"_blank"}>
-              {type['label']}
-            </a>
-            <br></br>
-          </span>        
-        );
-      }      
-      metadata['Type'] = [typeMtadataValue, false];
-    }
-
-    if(object.isIndividual && object.description){
-      // individual description structure is different
-      metadata['Description'][0] = [];      
-      for(let desc of object.description){
-        metadata['Description'][0].push(<p>{desc}</p>);
-      }
     }
     
   return metadata;
@@ -102,38 +43,24 @@ import TermLib from '../../../Libs/TermLib';
  * Create the metadata for a Property detail table
  * The boolean in each value indicates that the metadata is a link or not.
  */
-export function propertyMetaData(object){    
+export function propertyMetaData(term){    
   let metadata = {};
 
-  metadata['Label'] = [object.label, false];
+  metadata['Label'] = [term.label, false];
+  metadata['Imported From'] = [TermLib.createOntologyTagWithTermURL(term.originalOntology, term.iri, "property"), false];
+  metadata['Also In'] = [TermLib.createOntologyTagWithTermURL(term, term.iri, "property"), false];
+  metadata['Synonyms'] = [term.synonyms, false];
+  metadata['CURIE'] = [term.obo_id, false];
+  metadata['Term ID'] = [term.short_form, false];
+  metadata['Description'] = [term.description, false];
+  metadata['fullIRI'] = [term.iri, true];
+  metadata['Ontology'] = [term.ontology_name, false];
 
-  if(object.originalOntology){
-    metadata['Imported From'] = [TermLib.createOntologyTagWithTermURL(object.originalOntology, object.iri, "property"), false];
-  }
-
-  if(object.alsoIn && object.alsoIn.length !== 0){
-    let alsoInList = [];
-    for (let ontologyId of object.alsoIn){
-      if(object.originalOntology !== ontologyId){
-        alsoInList.push(TermLib.createOntologyTagWithTermURL(ontologyId, object.iri, "property"));
-      }        
-    }
-    metadata['Also In'] = [alsoInList, false];
-  }
-
-  metadata['Synonyms'] = [object.synonyms, false];
-  metadata['CURIE'] = [object.obo_id, false];
-  metadata['Term ID'] = [object.short_form, false];
-  metadata['Description'] = [object.description, false];
-  metadata['fullIRI'] = [object.iri, true];
-  metadata['Ontology'] = [object.ontology_name, false];
-
-
-  if(object.annotation){
-    for(let key in object.annotation){
+  if(term.annotation){
+    for(let key in term.annotation){
       metadata[key] = [];
       let value = [];
-      for(let annot of object.annotation[key]){
+      for(let annot of term.annotation[key]){
         value.push(annot);
       }
       metadata[key] = [value.join(',\n'), false];
@@ -153,11 +80,8 @@ export function formatText (tableLabel, text, isLink = false) {
   }
   else if (["Used in axiom", "Equivalent to", "SubClass Of"].includes(tableLabel)){        
     return (<span  dangerouslySetInnerHTML={{ __html: text }}></span>)
-  }  
-  else if (tableLabel === "Instances"){    
-    return <ul>{createInstancesList(text)}</ul>;
-  }
-  else if (["Type", "Description", "Imported From", "Also In"].includes(tableLabel)){
+  }    
+  else if (["Type", "Description", "Imported From", "Also In", "Instances"].includes(tableLabel)){
     return text;
   }
   
@@ -165,22 +89,6 @@ export function formatText (tableLabel, text, isLink = false) {
   return (<span  dangerouslySetInnerHTML={{ __html: formatedText }}></span>)
 }
 
-
-
-function createInstancesList(instancesList){
-  let result = [];  
-  for(let instance of instancesList){
-    let individualUrl = process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/' + instance['ontology_name'] + "/individuals?iri=" + encodeURIComponent(instance['iri']);
-    result.push(
-      <li>        
-        <a href={individualUrl} target='_blank'>
-          {instance['label']}
-        </a>
-      </li>
-    );
-  }  
-  return result;
-}
 
 
 
