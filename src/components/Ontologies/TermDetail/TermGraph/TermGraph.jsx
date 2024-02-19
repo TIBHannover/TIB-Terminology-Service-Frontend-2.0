86381d1c7e6ec2acc6b1c399363b8ec9a7266c0d
pooya.oa <pooya.oladazimi@tib.eu>
2024-02-19 17:20:27 +0100

    refactor graph

--- src/components/Ontologies/TermDetail/TermGraph/TermGraph.jsx
+++ src/components/Ontologies/TermDetail/TermGraph/TermGraph.jsx
@@ -4,15 +4,38 @@ import { useEffect} from 'react';
 
 export const TermGraph = (props) => {
 
+
+    function initLegacyGraphView(endpointPrefix, termiri, olsMode) {
+      let tmpnetworkOptions={ webservice : {URL: endpointPrefix, OLSschema:olsMode}}
+      let term = termiri
+      let visoptions = {
+        physics: {
+          enabled:false
+        },
+        hierarchical: {
+          enabled:false,
+          edgeMinimization: true,
+        },
+      }
+      
+    let app = require("ols-graphview");
+    let instance = new app();
+    if(document.getElementById('ontology_vis')){
+      document.getElementById('ontology_vis').innerHTML = '';
+    }
+    instance.visstart("ontology_vis", term, tmpnetworkOptions, {}, visoptions);
+  }
+
+
   function generateGraph(){
     if(props.isSkos){      
       let termFetchUrl = `${process.env.REACT_APP_API_BASE_URL}/${props.ontology}/skos/${encodeURIComponent(encodeURIComponent(props.iri))}/graph`;
-      window["initLegacyGraphView"](termFetchUrl, "", false);
+      initLegacyGraphView(termFetchUrl, "", false);
     }
     else if(props.iri){
       let termType = "terms";      
       let termFetchUrl = `${process.env.REACT_APP_API_BASE_URL}/${props.ontology}/${termType}?iri=`;
-      window["initLegacyGraphView"](termFetchUrl, props.iri, true);
+      initLegacyGraphView(termFetchUrl, props.iri, true);
     }
   }
 
