be8c7d2ee0443083a3b585e5694c27a70bfdb07e
pooya.oa <pooya.oladazimi@tib.eu>
2024-02-21 14:15:17 +0100

    implement messaging for graph

--- src/components/common/Graph/Graph.jsx
+++ src/components/common/Graph/Graph.jsx
@@ -4,6 +4,7 @@ import { DataSet } from 'vis-data';
 import TermApi from '../../../api/term';
 import GraphNode from './Node';
 import GraphEdge from './Edge';
+import AlertBox from '../Alerts/Alerts';
 
 
 
@@ -11,6 +12,7 @@ const Graph = (props) => {
     
     const [selectedNodes, setSelectedNodes] = useState([]);
     const [selectedEdges, setSelectedEdges] = useState([]);
+    const [message, setMessage] = useState(null);
     
 
     const nodes = useRef(new DataSet([]));
@@ -70,17 +72,26 @@ const Graph = (props) => {
 
     function resetGraph(){
         fetchGraphData(props.ontologyId, props.termIri, true);
+        setSelectedEdges([]);
+        setSelectedNodes([]); 
+        setMessage(null);
     }
 
 
 
     function removeFromGraph(){        
+        if(selectedEdges.length === 0 && selectedNodes.length === 0){
+            setMessage("Please select an entity to remove.");
+            return true;
+        }
         for (let id of selectedNodes){                        
             nodes.current.remove({id: id});        
         }
         for (let id of selectedEdges){                        
             edges.current.remove({id: id});           
-        }     
+        }
+        setSelectedEdges([]);
+        setSelectedNodes([]);     
     }
 
 
@@ -98,12 +109,22 @@ const Graph = (props) => {
         }
         else if(selectedEdges.length === 1){
             let edgeUri = selectedEdges[0].split('&uri=')[1]; // have a look at Edge Class Id format
+            if(edgeUri === "http://www.w3.org/2000/01/rdf-schema#subClassOf"){
+                setMessage("'Is a' property is not visible.");
+                return true;
+            }
             termLink.href = `${process.env.REACT_APP_PROJECT_SUB_PATH}/ontologies/${props.ontologyId}/props?iri=${encodeURIComponent(edgeUri)}`;
             document.body.appendChild(termLink);        
             termLink.click();
             document.body.removeChild(termLink);
             return true;  
-        }        
+        }
+        else if(selectedEdges.length === 0 && selectedNodes.length === 0){
+            setMessage("Please select an entity to visit.");
+        } 
+        else if(selectedEdges.length > 1 && selectedNodes.length > 1){
+            setMessage("Please Only select on entity to visit.");
+        }      
         return true;                        
     }
 
@@ -124,7 +145,8 @@ const Graph = (props) => {
                 }
             });
     
-            graphNetwork.current.on("click", function (params) {        
+            graphNetwork.current.on("click", function (params) {
+                setMessage(null);        
                 if(params.event.tapCount === 1){
                     // single click. Needed to differentiate it from double click
                     setSelectedNodes(params.nodes);
@@ -135,6 +157,13 @@ const Graph = (props) => {
     }, [graphNetwork]);
 
 
+    useEffect(() => {
+        if(message){
+            setTimeout(() => {setMessage(null)}, 5000);
+        }
+    }, [message])
+
+
 
     return (
         <div className='graph-section'>
@@ -143,6 +172,11 @@ const Graph = (props) => {
                 <button className='btn btn-sm btn-secondary graph-ctl-btn' onClick={resetGraph}>Reset</button>
                 <button className='btn btn-sm btn-secondary graph-ctl-btn' onClick={removeFromGraph}>Remove</button>
                 <button className='btn btn-sm btn-secondary graph-ctl-btn' onClick={visitNodeInGraph}>Visit</button>
+                {message && 
+                    <div className="graph-message-box">
+                        {message}                          
+                    </div>   
+                }
             </div>
             <div ref={container} className='graph-container' />
         </div>
