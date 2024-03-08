dd688262f00e16043e58d9864e2d69a256cf1d08
pooya.oa <pooya.oladazimi@tib.eu>
2024-03-08 13:12:47 +0100

    change toggle button class name

--- src/components/common/ToggleButton/ToggleButton.jsx
+++ src/components/common/ToggleButton/ToggleButton.jsx
@@ -12,7 +12,7 @@ export const ToggleButton = ({on, onClickCallback}) => {
     return(
         <label class="toggleButtonLabel" >
             <input type="checkbox" id="toggleBtn" onClick={onClickCallback} />
-            <div class={"slider round " + (!on ? "slider-off" : "")}>                        
+            <div class={"toggle-slider round " + (!on ? "toggle-slider-off" : "")}>                        
                 {on 
                     ? <span class="toggleButton-on">ON</span>
                     : <span class="toggleButton-off">OFF</span>
