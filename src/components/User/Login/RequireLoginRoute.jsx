e2d8979373cb0200daee3e907102ad3627bb7852
pooya.oa <pooya.oladazimi@tib.eu>
2024-06-07 11:50:37 +0200

    remove unneeded check login

--- src/components/User/Login/RequireLoginRoute.jsx
+++ src/components/User/Login/RequireLoginRoute.jsx
@@ -1,23 +1,14 @@
-import { useState } from "react";
-import AuthFactory from "./AuthFactory";
+import { useContext } from "react";
 import {Route} from 'react-router-dom';
 import UserPanel from "./UserPanel";
+import { AppContext } from "../../../context/AppContext";
 
 
 const RequireLoginRoute = ({component: Component, ...rest}) => {    
 
-    const [loginStatus, setLoginStatus] = useState(null);    
-    const [loginProcessFinished, setLoginProcessFinished] = useState(false);    
-    AuthFactory.userIsLogin().then((resp) => {
-        setLoginStatus(resp ? true : false);
-        setLoginProcessFinished(true);
-    });
+    const appContext = useContext(AppContext);
 
-    if(!loginProcessFinished){
-        return "";
-    }
-
-    else if (!loginStatus && loginProcessFinished) {           
+    if (!appContext.user) {           
         return <UserPanel isModal={false}></UserPanel>;
     }
         
