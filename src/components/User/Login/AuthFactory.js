6b0de11ddabb222264505a325cef34513850e9c1
pooya.oa <pooya.oladazimi@tib.eu>
2024-06-18 13:06:06 +0200

    refactoring

--- src/components/User/Login/AuthFactory.js
+++ src/components/User/Login/AuthFactory.js
@@ -1,6 +1,6 @@
 import AuthLib from "../../../Libs/AuthLib";
 import { runLogin, isLogin } from "../../../api/user";
-import { getTsPluginHeaders } from "../../../api/header";
+import UserModel from "../Model/user";
 
 
 
@@ -16,7 +16,7 @@ class AuthFactory{
             }        
             runLogin(code).then((resp) => {
                 if(resp){                    
-                    let userData = AuthLib.createUserDataObjectFromAuthResponse(resp);
+                    let userData = AuthFactory.createUserDataObjectFromAuthResponse(resp);
                     localStorage.setItem('user', JSON.stringify(userData));                     
                     let redirectUrl = localStorage.getItem("redirectUrl") ? localStorage.getItem("redirectUrl") : process.env.REACT_APP_PROJECT_SUB_PATH;
                     window.location.replace(redirectUrl);                    
@@ -53,20 +53,30 @@ class AuthFactory{
     }
 
 
-
-    static async userIsSysAdmin(){               
-        let headers = getTsPluginHeaders({withAccessToken: true});
-        let result = await fetch(process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/admin/is_system_admin', {method: "POST", headers:headers});    
-        if (result.status !== 200){
-            return false;
+    static createUserDataObjectFromAuthResponse(response){
+        try{
+            let authProvider = localStorage.getItem('authProvider');
+            let user = new UserModel();            
+            user.setToken(response["token"]);
+            user.setFullName(response["name"]);
+            user.setUsername(response["ts_username"]);
+            user.setUserToken(response["ts_user_token"]);
+            user.setSystemAdmin(response["system_admin"]);
+            user.setSettings(response["settings"]);            
+            user.setAuthProvider(authProvider);        
+            if(authProvider === 'github' || authProvider === 'gitlab'){            
+                user.setGitInfo({company: response["company"], homeUrl: response["github_home"]});            
+            }
+            else if(authProvider === "orcid"){
+                user.setOrcidInfo({orcidId:response["orcid_id"]});                        
+            }
+            return user;
         }
-        result = await result.json();
-        result = result["_result"];
-        if(result && result["is_system_admin"] === true){        
-            return true
-        }    
-        return false;
+        catch(e){
+            return null;
+        }        
     }
+
 
 
     static runLogout(){    
