4a3ad5d6fae5ce7ccf43c5355100e6d4cbf0d183
pooya.oa <pooya.oladazimi@tib.eu>
2024-06-21 11:24:07 +0200

    implement the contact form

--- src/components/User/ContactForm/ContactForm.jsx
+++ src/components/User/ContactForm/ContactForm.jsx
@@ -1,14 +1,70 @@
+import { useState } from "react";
 import DropDown from "../../common/DropDown/DropDown";
 import TextEditor from "../../common/TextEditor/TextEditor";
+import { getTextEditorContent } from "../../common/TextEditor/TextEditor";
 
 
 const ContactForm = () => {
 
+    const [editorState, setEditorState] = useState(null);
+    const [contactType, setContactType] = useState(0);
+    const [typeHintTextShow, setTypeHintTextShow] = useState(false);
+
 
     const CONTACT_TYPES = [
-        {label: "Question", value:0},
-        {label: "Issue", value:1}        
+        {label: "Choose ...", value:0},
+        {label: "Question", value:1},
+        {label: "Issue", value:2}        
     ];
+
+
+    function onTextAreaChange (newEditorState){
+        document.getElementsByClassName('rdw-editor-main')[0].style.border = '';
+        setEditorState(newEditorState);                
+    };
+
+
+
+    function submit(){
+        let formIsValid = true;
+        let title = document.getElementById('contact-form-title').value;
+        let safeQuestion = document.getElementById('contact-form-safe-q').value;
+        let content = "";                     
+        if(!editorState){            
+            document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
+            formIsValid = false;
+        }
+        else{
+            content = getTextEditorContent(editorState);
+        }
+        if(!title || title.trim() === ""){
+            document.getElementById('contact-form-title').style.borderColor = 'red';
+            formIsValid = false;
+        }
+        if(!safeQuestion || safeQuestion.trim() === ""){
+            document.getElementById('contact-form-safe-q').style.borderColor = 'red';
+            formIsValid = false;
+        }
+        
+        if(!content || content.trim() === ""){
+            document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
+            formIsValid = false;
+        }
+
+        if(parseInt(contactType) === 0){
+            setTypeHintTextShow(true);
+            formIsValid = false;
+        }
+
+        if(!formIsValid){
+            return;
+        }
+
+
+        return;
+    }
+
+
 
     return (
         <div className="row">
@@ -21,9 +77,14 @@ const ContactForm = () => {
                             options={CONTACT_TYPES}
                             dropDownId="contact-types"
                             dropDownTitle="I like to submit a"
-                            dropDownValue={0}
-                            // dropDownChangeHandler={props.changeArtifactType}
+                            dropDownValue={contactType}
+                            dropDownChangeHandler={(e) => {
+                                setContactType(e.target.value);
+                                setTypeHintTextShow(false);
+                            }}
+                            mandatory={true}
                         /> 
+                        {typeHintTextShow && <small className="text-danger"><i>Please choose</i></small>}
                     </div>
                 </div>
                 <br></br>
@@ -31,9 +92,8 @@ const ContactForm = () => {
                     <div className="col-sm-8">
                         <label className="required_input" for="contact-form-title">Title</label>
                         <input 
-                            type="text" 
-                            // value={props.noteTitle} 
-                            // onChange={() => {props.onTextInputChange()}}                                                 
+                            type="text"                             
+                            onChange={() => {document.getElementById('contact-form-title').style.borderColor = '';}}                                                 
                             class="form-control" 
                             id="contact-form-title"
                             placeholder="Enter Title for your query">
@@ -44,8 +104,8 @@ const ContactForm = () => {
                 <div className="row">
                     <div className="col-sm-8">
                         <TextEditor 
-                            editorState={null} 
-                            // textChangeHandlerFunction={props.onTextAreaChange}
+                            editorState={editorState} 
+                            textChangeHandlerFunction={onTextAreaChange}
                             wrapperClassName=""
                             editorClassName=""
                             placeholder="Enter your query here"
@@ -59,7 +119,7 @@ const ContactForm = () => {
                         <label className="required_input" for="contact-form-safe-q">What is 5 + 7 </label>
                         <input 
                             type="text"                             
-                            // onChange={() => {props.onTextInputChange()}}                                                 
+                            onChange={() => {document.getElementById('contact-form-safe-q').style.borderColor = '';}}                                                 
                             class="form-control" 
                             id="contact-form-safe-q">
                         </input>
@@ -68,7 +128,7 @@ const ContactForm = () => {
                 <br></br>
                 <div className="row">
                     <div className="col-sm-6">
-                        <button type="button" class="btn btn-secondary">Submit</button>
+                        <button type="button" class="btn btn-secondary" onClick={submit}>Submit</button>
                     </div>
                 </div>                        
             </div>            
