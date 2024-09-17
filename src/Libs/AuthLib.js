6103db7e92417b60603123c968a115682377ba14
pooya.oa <pooya.oladazimi@tib.eu>
2024-09-17 15:32:50 +0200

    refactor authLib

--- src/Libs/AuthLib.js
+++ src/Libs/AuthLib.js
@@ -1,7 +1,31 @@
 import UserModel from "../models/user";
+import { runLogin, isLogin } from "../api/user";
+import { getTsPluginHeaders } from "../api/header";
 
 
 class AuthLib{
+
+    static run(){
+        let cUrl = window.location.href;
+        if(cUrl.includes("code=")){
+            AuthLib.enableLoginAnimation();
+            let code = cUrl.split("code=")[1];        
+            runLogin(code).then((resp) => {
+                if(resp){                    
+                    let userData = AuthLib.createUserDataObjectFromAuthResponse(resp);
+                    localStorage.setItem('user', JSON.stringify(userData));                     
+                    let redirectUrl = localStorage.getItem("redirectUrl") ? localStorage.getItem("redirectUrl") : process.env.REACT_APP_PROJECT_SUB_PATH;
+                    window.location.replace(redirectUrl);                    
+                    return true;
+                }
+                AuthLib.disableLoginAnimation();                
+                return false;
+            });                                   
+        }
+        else{
+            return false;
+        }      
+    }
 
 
     static createUserDataObjectFromAuthResponse(response){
@@ -52,6 +76,48 @@ class AuthLib{
             return internalUserName.split("orcid_")[1];
         }
         return internalUserName;
+    }
+
+
+    static async userIsLogin(){      
+        if(process.env.REACT_APP_AUTH_FEATURE !== "true"){
+            return false;
+        } 
+        let user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;     
+        if(user && user.token){        
+            let validation = await isLogin();                        
+            if(validation){
+                return user;
+            }
+            else{
+                return null;
+            }
+        }
+        else{        
+            return null;
+        }
+    }
+
+
+    static async userIsSysAdmin(){               
+        let headers = getTsPluginHeaders({withAccessToken: true});
+        let result = await fetch(process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/admin/is_system_admin', {method: "POST", headers:headers});    
+        if (result.status !== 200){
+            return false;
+        }
+        result = await result.json();
+        result = result["_result"];
+        if(result && result["is_system_admin"] === true){        
+            return true
+        }    
+        return false;
+    }
+
+
+    static runLogout(){    
+        localStorage.setItem("user", null);
+        let redirectUrl = localStorage.getItem("redirectUrl") ? localStorage.getItem("redirectUrl") : process.env.REACT_APP_PROJECT_SUB_PATH;   
+        window.location.replace(redirectUrl);
     }
 
 }
