3f43e84fbdcba5d24de790cdfa3d3996c38475a5
pooya.oa <pooya.oladazimi@tib.eu>
2024-09-19 16:48:11 +0200

    implemented add an ontology to collections

--- src/components/Ontologies/OntologyOverview/OntologyOverview.jsx
+++ src/components/Ontologies/OntologyOverview/OntologyOverview.jsx
@@ -2,6 +2,7 @@ import OntologyInfoTable from './widgets/OntologyInfo';
 import OntologyStatsBox from './widgets/stats';
 import { useContext } from 'react';
 import { OntologyPageContext } from '../../../context/OntologyPageContext';
+import CollectionSuggestion from './widgets/CollectionSuggestion';
 
 
 
@@ -15,6 +16,7 @@ const OntologyOverview = () => {
 
 
   return(
+    <>
       <div  key={'ontolofyOverviewPage'} className="row ontology-detail-page-container">        
         <div className='col-sm-9'>
           <OntologyInfoTable />
@@ -23,19 +25,35 @@ const OntologyOverview = () => {
           <OntologyStatsBox />
           <br></br>          
           <div className='row'>
-              <div className='col-sm-12 node-metadata-value'>
+              <div className='col-sm-12'>
                 <a 
                   href={process.env.REACT_APP_API_BASE_URL + "/" + ontologyPageContext.ontology.ontologyId} 
                   target='_blank' 
                   rel="noreferrer"
-                  className='btn btn-secondary btn-dark download-ontology-btn'
+                  className='btn btn-secondary download-ontology-btn w-75'
                   >
                     Show Ontology Metdata as JSON
                 </a>
               </div>            
+          </div>
+          <br />
+          <div className='row'>
+            <div className='col-sm-12'>
+              <button 
+                  type="button" 
+                  className={"btn btn-secondary w-75 download-ontology-btn"}
+                  data-toggle="modal" 
+                  data-target={"#collectionSuggestionModal"} 
+                  data-backdrop="static"
+                  >
+                  Add to Collection
+              </button>
             </div>
+          </div>
         </div>
     </div>
+    <CollectionSuggestion />
+    </>
   );
 
  }
