792916553d8f85b29f45257b08714c979e95bd6c
pooya.oa <pooya.oladazimi@tib.eu>
2024-11-05 12:23:39 +0100

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Footer from "./components/common/Footer/Footer";
 import Header from "./components/common/Header/Header";
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { MatomoWrapper } from './components/Matomo/MatomoWrapper';
-import  CookieBanner  from './components/common/CookieBanner/CookieBanner';
+import CookieBanner from './components/common/CookieBanner/CookieBanner';
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css';
 import { BackendIsDownMessage, setSiteTitleAndFavIcon, InlineWrapperWithMargin } from './AppHelpers';
@@ -29,58 +29,58 @@ const App = () => {
   const [isBackendDown, setIsBackendDown] = useState(false);
   const [reportsListForAdmin, setReportsListForAdmin] = useState([]);
   const [userSettings, setUserSettings] = useState({
-    "activeCollection": {"title": "", "ontology_ids": []}, 
-    "userCollectionEnabled": false, 
-    "advancedSearchEnabled": false, 
+    "activeCollection": { "title": "", "ontology_ids": [] },
+    "userCollectionEnabled": false,
+    "advancedSearchEnabled": false,
     "activeSearchSetting": {},
     "activeSearchSettingIsModified": false
-  } );
-  
+  });
+
   const [showLoadingPage, setShowLoadingPage] = useState(true);
 
-  const olsIsUpQuery = useQuery({queryKey:['olsIsUpCall'], queryFn: olsIsUp, meta:{cache: false}});
-  if(olsIsUpQuery.isError){
+  const olsIsUpQuery = useQuery({ queryKey: ['olsIsUpCall'], queryFn: olsIsUp, meta: { cache: false } });
+  if (olsIsUpQuery.isError) {
     setIsBackendDown(true);
   }
 
   useEffect(() => {
     setSiteTitleAndFavIcon();
 
-    if(process.env.REACT_APP_AUTH_FEATURE === "true"){   
+    if (process.env.REACT_APP_AUTH_FEATURE === "true") {
       let cUrl = window.location.href;
-      if(cUrl.includes("code=")){
-        Auth.run();       
+      if (cUrl.includes("code=")) {
+        Auth.run();
       }
 
       Auth.userIsLogin().then((user) => {
         setUser(user);
         setIsSystemAdmin(user?.systemAdmin);
-        let settings = {...userSettings};                
+        let settings = { ...userSettings };
         settings.userCollectionEnabled = user?.settings?.userCollectionEnabled;
-        settings.advancedSearchEnabled = user?.settings?.advancedSearchEnabled; 
-        settings.activeSearchSettingIsModified = user?.settings?.activeSearchSettingIsModified;       
-        if(user?.settings?.activeCollection?.title){
-          settings.activeCollection = user?.settings?.activeCollection;          
+        settings.advancedSearchEnabled = user?.settings?.advancedSearchEnabled;
+        settings.activeSearchSettingIsModified = user?.settings?.activeSearchSettingIsModified;
+        if (user?.settings?.activeCollection?.title) {
+          settings.activeCollection = user?.settings?.activeCollection;
         }
-        if(user?.settings?.activeSearchSetting){
-          settings.activeSearchSetting = user?.settings?.activeSearchSetting;          
+        if (user?.settings?.activeSearchSetting) {
+          settings.activeSearchSetting = user?.settings?.activeSearchSetting;
         }
-        
-        if(user?.systemAdmin){
+
+        if (user?.systemAdmin) {
           getReportList().then((reports) => {
             setReportsListForAdmin(reports);
           });
-        }        
+        }
 
-        setUserSettings(settings); 
-        setShowLoadingPage(false);    
-      });      
+        setUserSettings(settings);
+        setShowLoadingPage(false);
+      });
     }
-    else{
-      setShowLoadingPage(false); 
+    else {
+      setShowLoadingPage(false);
     }
 
-    setTimeout( () => {
+    setTimeout(() => {
       setLoading(false);
     }, 500);
 
@@ -98,39 +98,39 @@ const App = () => {
   return (
     <div className="App">
       <LoginLoadingAnimation />
-     <BrowserRouter>
-        <MatomoWrapper> 
-        <div className='container-fluid'>
-            <AppContext.Provider  value={appContextData}>
-              {showLoadingPage && <LoadingPage /> }
+      <BrowserRouter>
+        <MatomoWrapper>
+          <div className='container-fluid'>
+            <AppContext.Provider value={appContextData}>
+              {showLoadingPage && <LoadingPage />}
               {!showLoadingPage &&
                 <>
-                <Header />
-                <div className='application-content'  id="application_content">
-                  {loading && 
-                    <Skeleton 
-                      count={2} 
-                      wrapper={InlineWrapperWithMargin} 
-                      inline width={600} 
-                      height={200} 
-                      marginLeft={20} 
-                      baseColor={'#f4f2f2'}/>
-                  }
-                  {!loading &&
-                    <>            
-                      {isBackendDown && <BackendIsDownMessage />}
-                      <CookieBanner />
-                      <AppRouter />        
-                    </>   
-                  }
-                </div>
-                <Footer />
+                  <Header />
+                  <div className='application-content' id="application_content">
+                    {loading &&
+                      <Skeleton
+                        count={2}
+                        wrapper={InlineWrapperWithMargin}
+                        inline width={600}
+                        height={200}
+                        marginLeft={20}
+                        baseColor={'#f4f2f2'} />
+                    }
+                    {!loading &&
+                      <>
+                        {isBackendDown && <BackendIsDownMessage />}
+                        <CookieBanner />
+                        <AppRouter />
+                      </>
+                    }
+                  </div>
+                  <Footer />
                 </>
               }
-            </AppContext.Provider>                  
-          </div>             
+            </AppContext.Provider>
+          </div>
         </MatomoWrapper>
-      </BrowserRouter>      
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/User/Login/TS/Login.jsx
+++ src/components/User/Login/TS/Login.jsx
@@ -1,133 +1,133 @@
-const Login = (props) => {    
+const Login = (props) => {
 
-    function getAuthenticationCode(e){
-        let authProvider = e.target.getAttribute("authProvider");
-        let loginUrl = "";
-        if(authProvider === "github"){
-            loginUrl = process.env.REACT_APP_GITHUB_AUTH_BASE_URL;
-            loginUrl += "&client_id=" + process.env.REACT_APP_GITHUB_CLIENT_ID;                   
-        }else if(authProvider === "orcid"){
-            loginUrl = process.env.REACT_APP_ORCID_AUTH_BASE_URL;
-            loginUrl += "&client_id=" + process.env.REACT_APP_ORCID_CLIENT_ID;            
-        }else if(authProvider === "gitlab"){
-            loginUrl = process.env.REACT_APP_GITLAB_AUTH_BASE_URL;
-            loginUrl += "&client_id=" + process.env.REACT_APP_GITLAB_CLIENT_ID;
-        }else if(authProvider === "native"){
-            loginUrl = process.env.REACT_APP_REGAPP_AUTH_BASE_URL;
-            loginUrl += "?client_id=" + process.env.REACT_APP_REGAPP_CLIENT_ID;
-        }
-
-        
-        loginUrl += "&redirect_uri=" + process.env.REACT_APP_LOGIN_REDIRECT_URL;
-        localStorage.setItem("authProvider", authProvider);
-        localStorage.setItem("redirectUrl", window.location.href);
-        window.location.replace(loginUrl);
+  function getAuthenticationCode(e) {
+    let authProvider = e.target.getAttribute("authProvider");
+    let loginUrl = "";
+    if (authProvider === "github") {
+      loginUrl = process.env.REACT_APP_GITHUB_AUTH_BASE_URL;
+      loginUrl += "&client_id=" + process.env.REACT_APP_GITHUB_CLIENT_ID;
+    } else if (authProvider === "orcid") {
+      loginUrl = process.env.REACT_APP_ORCID_AUTH_BASE_URL;
+      loginUrl += "&client_id=" + process.env.REACT_APP_ORCID_CLIENT_ID;
+    } else if (authProvider === "gitlab") {
+      loginUrl = process.env.REACT_APP_GITLAB_AUTH_BASE_URL;
+      loginUrl += "&client_id=" + process.env.REACT_APP_GITLAB_CLIENT_ID;
+    } else if (authProvider === "native") {
+      loginUrl = process.env.REACT_APP_REGAPP_AUTH_BASE_URL;
+      loginUrl += "?client_id=" + process.env.REACT_APP_REGAPP_CLIENT_ID;
     }
 
 
+    loginUrl += "&redirect_uri=" + process.env.REACT_APP_LOGIN_REDIRECT_URL;
+    localStorage.setItem("authProvider", authProvider);
+    localStorage.setItem("redirectUrl", window.location.href);
+    window.location.replace(loginUrl);
+  }
 
-    function buildAuthButtons(){        
-        return [
-            <span>
-                <div className="row justify-content-center">
-                    <a onClick={getAuthenticationCode}  authProvider="github" className="btn btn-secondary github-login-btn login-btn">
-                        <i className="fa fa-github"></i> Sign in with GitHub
-                    </a>
+
+
+  function buildAuthButtons() {
+    return [
+      <span>
+        <div className="row justify-content-center">
+          <a onClick={getAuthenticationCode} authProvider="github" className="btn btn-secondary github-login-btn login-btn">
+            <i className="fa fa-github"></i> Sign in with GitHub
+          </a>
+        </div>
+        <br></br>
+        <div className="row justify-content-center">
+          <a onClick={getAuthenticationCode} authProvider="gitlab" className="btn btn-secondary gitlab-login-btn login-btn">
+            <i className="fa fa-gitlab"></i> Sign in with GitLab
+          </a>
+        </div>
+        <br></br>
+        <div className="row justify-content-center">
+          <a onClick={getAuthenticationCode} authProvider="orcid" className="btn btn-secondary orcid-login-btn login-btn">
+            <i class="fa-brands fa-orcid"></i> Sign in with ORCID
+          </a>
+        </div>
+        <br />
+        <div className="row justify-content-center">
+          <a onClick={getAuthenticationCode} authProvider="native" className="btn btn-secondary login-btn">
+            Sign in with RegApp
+          </a>
+        </div>
+      </span>
+    ];
+  }
+
+
+
+  function createLoginButton() {
+    if (props.withoutButton) {
+      return "";
+    }
+    if (props.customLoginBtn) {
+      return props.customLoginBtn;
+    }
+    return <a className="login-btn-header" type="button" data-toggle="modal" data-target="#loginModal">Login</a>;
+  }
+
+
+
+  if (process.env.REACT_APP_AUTH_FEATURE !== "true") {
+    return null;
+  }
+
+  const modalId = props.customModalId ? props.customModalId : "loginModal";
+
+  return (
+    <>
+      {props.isModal &&
+        // render the modal. Used in the site header 
+        <span>
+          {createLoginButton()}
+          <div class="modal fade loginModal" id={modalId} tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="loginModalLabel">Login</h5>
+                  <a type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></a>
                 </div>
-                <br></br>
-                <div className="row justify-content-center">
-                    <a onClick={getAuthenticationCode}  authProvider="gitlab" className="btn btn-secondary gitlab-login-btn login-btn">
-                        <i className="fa fa-gitlab"></i> Sign in with GitLab
-                    </a>
-                </div>
-                <br></br>
-                <div className="row justify-content-center">
-                    <a onClick={getAuthenticationCode} authProvider="orcid" className="btn btn-secondary orcid-login-btn login-btn">
-                        <i class="fa-brands fa-orcid"></i> Sign in with ORCID
-                    </a>
-                </div>   
-                <br/>
-                <div className="row justify-content-center">
-                    <a onClick={getAuthenticationCode} authProvider="native" className="btn btn-secondary login-btn">
-                        Sign in with RegApp
-                    </a>
-                </div>
-            </span>
-        ];
-    }
-
-
-
-    function createLoginButton(){
-        if(props.withoutButton){
-            return "";
-        }
-        if(props.customLoginBtn){
-            return props.customLoginBtn;
-        }
-        return <a className="login-btn-header" type="button" data-toggle="modal" data-target="#loginModal">Login</a>;
-    }
-
-
-
-    if(process.env.REACT_APP_AUTH_FEATURE !== "true"){            
-        return null;
-    }
-
-    const modalId = props.customModalId ? props.customModalId : "loginModal";
-
-    return(
-        <>
-        {props.isModal &&
-            // render the modal. Used in the site header 
-            <span>
-                {createLoginButton()}
-                <div class="modal fade loginModal" id={modalId} tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="loginModalLabel">Login</h5>                                        
-                                <a type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></a>
-                            </div>
-                            <div class="modal-body">
-                                {(props.customLoginBtn || props.withoutButton) && 
-                                    <>
-                                    <div className="row">
-                                        <div className="col-sm-12">
-                                            <h5>You need to login for accessing this function.</h5>
-                                        </div>
-                                    </div>                                    
-                                    <br></br>
-                                    </>
-                                }
-                                <div className="login-modal-hint-text">  
-                                    <strong>Attention:</strong> Some of the features, such as term request, are only available if you 
-                                                authenticate with Github. 
-                                </div>
-                                <br></br> 
-                                {buildAuthButtons()}
-                            </div>                            
+                <div class="modal-body">
+                  {(props.customLoginBtn || props.withoutButton) &&
+                    <>
+                      <div className="row">
+                        <div className="col-sm-12">
+                          <h5>You need to login for accessing this function.</h5>
                         </div>
-                    </div>
+                      </div>
+                      <br></br>
+                    </>
+                  }
+                  <div className="login-modal-hint-text">
+                    <strong>Attention:</strong> Some of the features, such as term request, are only available if you
+                    authenticate with Github.
+                  </div>
+                  <br></br>
+                  {buildAuthButtons()}
                 </div>
-            </span>                    
-        }
-        {!props.isModal &&
-            // render the normal login form. Used when a user need to login before accessing a section
-            <div className="row">
-                <div className="col-sm-12 text-center">
-                    <h5>You need to login for accessing this section.</h5>
-                    <div>  
-                        <strong>Attention:</strong> Some of the features, such as term request, are only available if you 
-                                    authenticate with Github. 
-                    </div> 
-                    <br></br>
-                    {buildAuthButtons()}
-                </div>
-            </div>                   
-        }
-        </>
-    );
+              </div>
+            </div>
+          </div>
+        </span>
+      }
+      {!props.isModal &&
+        // render the normal login form. Used when a user need to login before accessing a section
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <h5>You need to login for accessing this section.</h5>
+            <div>
+              <strong>Attention:</strong> Some of the features, such as term request, are only available if you
+              authenticate with Github.
+            </div>
+            <br></br>
+            {buildAuthButtons()}
+          </div>
+        </div>
+      }
+    </>
+  );
 
 
 }
