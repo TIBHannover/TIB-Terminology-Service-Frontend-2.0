50452b9420eb8b254e55a1865eb80cb3ac9d18ac
pooya.oa <pooya.oladazimi@tib.eu>
2024-11-12 13:53:24 +0100

    disable collection related action for ontology page for non general app.

--- src/components/Ontologies/OntologyOverview/OntologyOverview.jsx
+++ src/components/Ontologies/OntologyOverview/OntologyOverview.jsx
@@ -15,47 +15,49 @@ const OntologyOverview = () => {
   const ontologyPageContext = useContext(OntologyPageContext);
 
 
-  return(
+  return (
     <>
-      <div  key={'ontolofyOverviewPage'} className="row ontology-detail-page-container">        
+      <div key={'ontolofyOverviewPage'} className="row ontology-detail-page-container">
         <div className='col-sm-9'>
           <OntologyInfoTable />
         </div>
         <div className='col-sm-3'>
           <OntologyStatsBox />
-          <br></br>          
-          <div className='row'>
-              <div className='col-sm-12'>
-                <a 
-                  href={process.env.REACT_APP_API_BASE_URL + "/" + ontologyPageContext.ontology.ontologyId} 
-                  target='_blank' 
-                  rel="noreferrer"
-                  className='btn btn-secondary download-ontology-btn w-75'
-                  >
-                    Show Ontology Metdata as JSON
-                </a>
-              </div>            
-          </div>
-          <br />
+          <br></br>
           <div className='row'>
             <div className='col-sm-12'>
-              <button 
-                  type="button" 
-                  className={"btn btn-secondary w-75 download-ontology-btn"}
-                  data-toggle="modal" 
-                  data-target={"#collectionSuggestionModal"} 
-                  data-backdrop="static"
-                  >
-                  Add to Collection
-              </button>
+              <a
+                href={process.env.REACT_APP_API_BASE_URL + "/" + ontologyPageContext.ontology.ontologyId}
+                target='_blank'
+                rel="noreferrer"
+                className='btn btn-secondary download-ontology-btn w-75'
+              >
+                Show Ontology Metdata as JSON
+              </a>
             </div>
           </div>
+          <br />
+          {process.env.REACT_APP_PROJECT_ID === "general" &&
+            <div className='row'>
+              <div className='col-sm-12'>
+                <button
+                  type="button"
+                  className={"btn btn-secondary w-75 download-ontology-btn"}
+                  data-toggle="modal"
+                  data-target={"#collectionSuggestionModal"}
+                  data-backdrop="static"
+                >
+                  Add to Collection
+                </button>
+              </div>
+            </div>
+          }
         </div>
-    </div>
-    <CollectionSuggestion />
+      </div>
+      <CollectionSuggestion />
     </>
   );
 
- }
+}
 
 export default OntologyOverview;
