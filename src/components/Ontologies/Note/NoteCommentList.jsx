05938a3ba8d698f2ffef285823818b69ec7d825c
pooya.oa <pooya.oladazimi@tib.eu>
2024-11-14 15:22:14 +0100

    cleaning

--- src/components/Ontologies/Note/NoteCommentList.jsx
+++ src/components/Ontologies/Note/NoteCommentList.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { submitNoteComment, editNoteComment } from "../../../api/note";
-import {getTextEditorContent, createTextEditorEmptyText, createTextEditorStateFromJson} from "../../common/TextEditor/TextEditor";
+import { getTextEditorContent, createTextEditorEmptyText, createTextEditorStateFromJson } from "../../common/TextEditor/TextEditor";
 import { NoteCommentListRender } from "./renders/NoteCommentListRender";
 import NoteUrlFactory from "../../../UrlFactory/NoteUrlFactory";
 import CommonUrlFactory from "../../../UrlFactory/CommonUrlFactory";
@@ -11,157 +11,157 @@ import * as SiteUrlParamNames from '../../../UrlFactory/UrlParamNames';
 
 
 const NoteCommentList = (props) => {
-    /* 
-        This component is responsible for rendering the list of comments for the note.
-        It uses the OntologyPageContext to get the ontology information.
-        It uses the submitNoteComment function to submit the note comment to the backend.
-        It uses the editNoteComment function to edit the note comment in the backend.
-    */
+  /* 
+      This component is responsible for rendering the list of comments for the note.
+      It uses the OntologyPageContext to get the ontology information.
+      It uses the submitNoteComment function to submit the note comment to the backend.
+      It uses the editNoteComment function to edit the note comment in the backend.
+  */
 
-    const ontologyPageContext = useContext(OntologyPageContext);
+  const ontologyPageContext = useContext(OntologyPageContext);
 
-    const [commentEditorState, setCommentEditorState] = useState(createTextEditorEmptyText());    
-    const [noteId, setNoteId] = useState(null);
-    const [editMode, setEditMode] = useState(false);
-    const [editCommentId, setEditCommentId] = useState(-1);
-    
-    const noteUrlFactory = new NoteUrlFactory();
-    const commonUrlFactory = new CommonUrlFactory();
+  const [commentEditorState, setCommentEditorState] = useState(createTextEditorEmptyText());
+  const [noteId, setNoteId] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [editCommentId, setEditCommentId] = useState(-1);
+
+  const noteUrlFactory = new NoteUrlFactory();
+  const commonUrlFactory = new CommonUrlFactory();
 
 
-    function onTextAreaChange (newEditorState){
-        document.getElementsByClassName('rdw-editor-main')[0].style.border = '';
-        setCommentEditorState(newEditorState);        
+  function onTextAreaChange(newEditorState) {
+    document.getElementsByClassName('rdw-editor-main')[0].style.border = '';
+    setCommentEditorState(newEditorState);
+  }
+
+
+  function handleEditButton(e) {
+    let commentId = e.target.getAttribute('data-id');
+    let commentContent = e.target.getAttribute('data-content');
+    if (commentContent) {
+      let content = createTextEditorStateFromJson(commentContent);
+      let editorBox = document.getElementsByClassName("note-comment-editor-warpper")[0];
+      editorBox.scrollIntoView();
+      setCommentEditorState(content);
+      setEditMode(true);
+      setEditCommentId(commentId);
+    }
+  }
+
+
+  function submitComment() {
+    let commentContent = "";
+    let formIsValid = true;
+    if (!commentEditorState) {
+      document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
+      formIsValid = false;
+    }
+    else {
+      commentContent = getTextEditorContent(commentEditorState);
     }
 
-
-    function  handleEditButton(e){
-        let commentId = e.target.getAttribute('data-id');
-        let commentContent = e.target.getAttribute('data-content');
-        if(commentContent){
-            let content =  createTextEditorStateFromJson(commentContent);            
-            let editorBox = document.getElementsByClassName("note-comment-editor-warpper")[0];
-            editorBox.scrollIntoView();            
-            setCommentEditorState(content);
-            setEditMode(true);
-            setEditCommentId(commentId);       
-        }
+    if (!commentContent || commentContent.trim() === "") {
+      document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
+      formIsValid = false;
     }
 
-
-    function submitComment(){
-        let commentContent = "";
-        let formIsValid = true;
-        if(!commentEditorState){            
-            document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
-            formIsValid = false;
-        }
-        else{                    
-            commentContent = getTextEditorContent(commentEditorState);
-        }
-
-        if(!commentContent || commentContent.trim() === ""){
-            document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
-            formIsValid = false;
-        }
-
-        if(!formIsValid){
-            return;
-        }
-
-        let data = {'noteId': props.note['id'], 'content': commentContent};
-        submitNoteComment(data).then((result) => {
-            if(result){                
-                setNoteId(null);
-                setCommentEditorState(null);
-                props.noteDetailReloader();            
-            }
-        });
+    if (!formIsValid) {
+      return;
     }
 
+    let data = { 'noteId': props.note['id'], 'content': commentContent };
+    submitNoteComment(data).then((result) => {
+      if (result) {
+        setNoteId(null);
+        setCommentEditorState(null);
+        props.noteDetailReloader();
+      }
+    });
+  }
 
-    function edit(){
-        let commentContent = "";
-        let formIsValid = true;
-        if(!commentEditorState){            
-            document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
-            formIsValid = false;
-        }
-        else{               
-            commentContent = getTextEditorContent(commentEditorState);
-        }
 
-        if(!commentContent || commentContent.trim() === ""){
-            document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
-            formIsValid = false;
-        }
-
-        if(!formIsValid){
-            return;
-        }
-
-        let data = {'comment_id': editCommentId, 'content': commentContent, 'ontology_id': ontologyPageContext.ontology.ontologyId};
-        editNoteComment(data).then((result) => {
-            if(result){                
-                setNoteId(null);
-                setCommentEditorState(null);
-                setEditCommentId(-1);
-                setEditMode(false);
-                props.noteDetailReloader();              
-            }
-        });
+  function edit() {
+    let commentContent = "";
+    let formIsValid = true;
+    if (!commentEditorState) {
+      document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
+      formIsValid = false;
+    }
+    else {
+      commentContent = getTextEditorContent(commentEditorState);
     }
 
+    if (!commentContent || commentContent.trim() === "") {
+      document.getElementsByClassName('rdw-editor-main')[0].style.border = '1px solid red';
+      formIsValid = false;
+    }
 
-    function cancelEdit(){        
-        commonUrlFactory.deleteParam({name: SiteUrlParamNames.CommentId});
+    if (!formIsValid) {
+      return;
+    }
+
+    let data = { 'comment_id': editCommentId, 'content': commentContent, 'ontology_id': ontologyPageContext.ontology.ontologyId };
+    editNoteComment(data).then((result) => {
+      if (result) {
+        setNoteId(null);
         setCommentEditorState(null);
         setEditCommentId(-1);
-        setEditMode(false);    
+        setEditMode(false);
+        props.noteDetailReloader();
+      }
+    });
+  }
+
+
+  function cancelEdit() {
+    commonUrlFactory.deleteParam({ name: SiteUrlParamNames.CommentId });
+    setCommentEditorState(null);
+    setEditCommentId(-1);
+    setEditMode(false);
+  }
+
+
+  function jumpToCommentIfExist() {
+    let commentId = noteUrlFactory.commentId
+    let commentBox = document.getElementById("comment-card-" + commentId);
+    if (commentBox) {
+      commentBox.style.border = '2px solid red';
+      setTimeout(() => {
+        commentBox.style.borderColor = '';
+      }, 5000);
+      commentBox.scrollIntoView();
     }
+  }
 
 
-    function jumpToCommentIfExist(){        
-        let commentId = noteUrlFactory.commentId
-        let commentBox = document.getElementById("comment-card-" + commentId);
-        if(commentBox){            
-            commentBox.style.border = '2px solid red';            
-            setTimeout(() => {
-                commentBox.style.borderColor = '';
-            },5000); 
-            commentBox.scrollIntoView();
-        }
-    }
-
-
-    useEffect(() => {
-        jumpToCommentIfExist();
-    }, [props.note]);
+  useEffect(() => {
+    jumpToCommentIfExist();
+  }, [props.note]);
 
 
 
-    if(process.env.REACT_APP_NOTE_FEATURE !== "true"){            
-        return null;
-    }
+  if (process.env.REACT_APP_NOTE_FEATURE !== "true") {
+    return null;
+  }
 
-    return (
-        <NoteCommentListRender 
-            note={props.note}
-            handleEditButton={handleEditButton}
-            commentEditorState={commentEditorState}
-            onTextAreaChange={onTextAreaChange}
-            editMode={editMode}
-            submitComment={submitComment}
-            edit={edit}
-            cancelEdit={cancelEdit}
-        />
-    );
+  return (
+    <NoteCommentListRender
+      note={props.note}
+      handleEditButton={handleEditButton}
+      commentEditorState={commentEditorState}
+      onTextAreaChange={onTextAreaChange}
+      editMode={editMode}
+      submitComment={submitComment}
+      edit={edit}
+      cancelEdit={cancelEdit}
+    />
+  );
 }
 
 
 NoteCommentList.propsTypes = {
-    note: PropTypes.object.isRequired,
-    noteDetailReloader: PropTypes.func.isRequired
+  note: PropTypes.object.isRequired,
+  noteDetailReloader: PropTypes.func.isRequired
 }
 
 
--- src/components/Ontologies/Note/helpers.jsx
+++ src/components/Ontologies/Note/helpers.jsx
@@ -3,109 +3,109 @@ import { pinnNote } from "../../../api/note";
 
 
 
-export function buildNoteAboutPart(note){
-    let url = process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/' + note['ontology_id'];
-    let label = "";
-    if (note['semantic_component_type'] === "ontology"){
-        label = note['ontology_id'];
-    }
-    else if (note['semantic_component_type'] === "class"){
-        url += ('/terms?iri=' + encodeURIComponent(note['semantic_component_iri']))
-        label = note['semantic_component_label'];
-    }
-    else if (note['semantic_component_type'] === "property"){
-        url += ('/props?iri=' + encodeURIComponent(note['semantic_component_iri']))
-        label = note['semantic_component_label'];
-    }
-    else{
-        url += ('/individuals?iri=' + encodeURIComponent(note['semantic_component_iri']))
-        label = note['semantic_component_label'];
-    }
+export function buildNoteAboutPart(note) {
+  let url = process.env.REACT_APP_PROJECT_SUB_PATH + '/ontologies/' + note['ontology_id'];
+  let label = "";
+  if (note['semantic_component_type'] === "ontology") {
+    label = note['ontology_id'];
+  }
+  else if (note['semantic_component_type'] === "class") {
+    url += ('/terms?iri=' + encodeURIComponent(note['semantic_component_iri']))
+    label = note['semantic_component_label'];
+  }
+  else if (note['semantic_component_type'] === "property") {
+    url += ('/props?iri=' + encodeURIComponent(note['semantic_component_iri']))
+    label = note['semantic_component_label'];
+  }
+  else {
+    url += ('/individuals?iri=' + encodeURIComponent(note['semantic_component_iri']))
+    label = note['semantic_component_label'];
+  }
 
-    return (
-        <a  href={url} target="_blank">
-            <b>{label}</b>
-        </a>
-    );
+  return (
+    <a href={url} target="_blank">
+      <b>{label}</b>
+    </a>
+  );
 }
 
 
 export const PinnModalBtn = (props) => {
 
-    if(!props.isAdminForOntology){
-        return "";
-    }
+  if (!props.isAdminForOntology) {
+    return "";
+  }
 
-    if(props.note['visibility'] === "me"){
-        return "";
-    }
-        
-    if(parseInt(props.numberOfpinned) === parseInt(process.env.REACT_APP_MAX_PIN_NOTES)  && !props.note['pinned']){
-        return "";
-    }
+  if (props.note['visibility'] === "me") {
+    return "";
+  }
 
-    return (
-        <button type="button" 
-                class="btn btn-sm borderless-btn note-action-menu-btn" 
-                data-toggle="modal" 
-                data-target={"#pinModal" + props.modalId}
-                data-backdrop="static"
-                >
-                {props.note['pinned'] ? "Unpin" : "Pin"}                 
-        </button>
-    );
-    
-} 
+  if (parseInt(props.numberOfpinned) === parseInt(process.env.REACT_APP_MAX_PIN_NOTES) && !props.note['pinned']) {
+    return "";
+  }
+
+  return (
+    <button type="button"
+      class="btn btn-sm borderless-btn note-action-menu-btn"
+      data-toggle="modal"
+      data-target={"#pinModal" + props.modalId}
+      data-backdrop="static"
+    >
+      {props.note['pinned'] ? "Unpin" : "Pin"}
+    </button>
+  );
+
+}
 
 
 
 export const PinnModal = (props) => {
-    const [submited, setSubmited] = useState(false);
+  const [submited, setSubmited] = useState(false);
 
-    let data = {};
-    data["ontology"] = props.note['ontology_id'];
-    data["note_id"] = props.note['id'];
-    data["pinned"] = `${!Boolean(props.note['pinned'])}`;    
-    
-    
-    async function pinNote(){
-        await pinnNote(data);            
-        setSubmited(true);            
-        redirectAfterPin();
-    }
+  let data = {};
+  data["ontology"] = props.note['ontology_id'];
+  data["note_id"] = props.note['id'];
+  data["pinned"] = `${!Boolean(props.note['pinned'])}`;
 
-    const redirectAfterPin = () => {
-        window.location.replace(window.location.href);
-    }
 
-    return (
-        <div>            
-            <div class="modal fade" id={"pinModal" + props.modalId} tabindex="-1" role="dialog" aria-labelledby={"pinModalLabel" + props.modalId} aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id={"pinModalLabel" + props.modalId}>Confirmation</h5>
-                            {!submited && 
-                                <button type="button" class="close close-btn-message-modal" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            }
-                        </div>
-                        <div class="modal-body">
-                            {!submited && 
-                                <span>
-                                    Are you sure you want to {props.note['pinned'] ? "Unpin" : "Pin"} this note?                                     
-                                </span>                                
-                            }                            
-                        </div>
-                        <div class="modal-footer justify-content-center">                            
-                            {!submited && <button type="button" class="btn btn-secondary" onClick={pinNote}>
-                                {props.note['pinned'] ? "Unpin" : "Pin"}
-                            </button>}                            
-                        </div>
-                    </div>
-                </div>
+  async function pinNote() {
+    await pinnNote(data);
+    setSubmited(true);
+    redirectAfterPin();
+  }
+
+  const redirectAfterPin = () => {
+    window.location.replace(window.location.href);
+  }
+
+  return (
+    <div>
+      <div class="modal fade" id={"pinModal" + props.modalId} tabindex="-1" role="dialog" aria-labelledby={"pinModalLabel" + props.modalId} aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id={"pinModalLabel" + props.modalId}>Confirmation</h5>
+              {!submited &&
+                <button type="button" class="close close-btn-message-modal" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              }
             </div>
+            <div class="modal-body">
+              {!submited &&
+                <span>
+                  Are you sure you want to {props.note['pinned'] ? "Unpin" : "Pin"} this note?
+                </span>
+              }
+            </div>
+            <div class="modal-footer justify-content-center">
+              {!submited && <button type="button" class="btn btn-secondary" onClick={pinNote}>
+                {props.note['pinned'] ? "Unpin" : "Pin"}
+              </button>}
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
