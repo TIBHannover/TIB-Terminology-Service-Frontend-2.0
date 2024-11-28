9b6b08b851f136978f586ca87bee8d62cc232eff
pooya.oa <pooya.oladazimi@tib.eu>
2024-11-28 14:55:07 +0100

    fix the stats issue

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import '../layout/home.css';
-import {createStatsBox} from './StatsBox';
+import { createStatsBox } from './StatsBox';
 import { renderHomePage } from "./HomePageContent";
 import { getCallSetting } from "../../api/constants";
 
-class Home extends React.Component{
-  constructor(props){
+class Home extends React.Component {
+  constructor(props) {
     super(props)
-    this.state=({
-       statsResult: []
+    this.state = ({
+      statsResult: []
     })
-    this.Stats = this.Stats.bind(this);    
+    this.Stats = this.Stats.bind(this);
   }
 
-  async Stats(){
+  async Stats() {
     let statsResult = await fetch(process.env.REACT_APP_STATS_API_URL, getCallSetting)
     statsResult = (await statsResult.json());
     this.setState({
@@ -21,22 +21,22 @@ class Home extends React.Component{
     })
   }
 
- 
-  componentDidMount(){
+
+  componentDidMount() {
     this.Stats();
   }
 
-  render(){
-      return(
+  render() {
+    return (
       <div className="row">
-        <div className="col-sm-12">          
+        <div className="col-sm-12">
           {renderHomePage()}
           <div className="row justify-content-center home-page-stats-container">
-              {createStatsBox(this.state.statsResult)}
+            {createStatsBox(this.state.statsResult)}
           </div>
-        </div>        
+        </div>
       </div>
-      );
+    );
   }
 }
 export default Home;
