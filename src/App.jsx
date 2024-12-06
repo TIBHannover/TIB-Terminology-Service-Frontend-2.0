c441b2e8d5a5fd6d1476810f30a461082e1cf83f
pooya.oa <pooya.oladazimi@tib.eu>
2024-12-06 12:36:43 +0100

    make the site tour feature configurable in env.

--- src/App.jsx
+++ src/App.jsx
@@ -122,7 +122,7 @@ const App = () => {
                       </>
                     }
                   </div>
-                  <SiteTour />
+                  {process.env.REACT_APP_SITE_TOUR === "true" && <SiteTour />}
                   <Footer />
                 </>
               }
