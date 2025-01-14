5d69a9ebd3c4f57ed30007ccbfee05c70a26365b
pooya.oa <pooya.oladazimi@tib.eu>
2025-01-14 17:25:22 +0100

    make the changes for the new ts plugin service in django.

--- src/api/collection.ts
+++ src/api/collection.ts
@@ -4,76 +4,76 @@ import { CollectionWithItsOntologyListData } from "./types/collectionTypes";
 
 
 /* react query key:  allCollectionsWithTheirStats   */
-export async function fetchCollectionsWithStats():Promise<Array<object>>{
+export async function fetchCollectionsWithStats(): Promise<Array<object>> {
   type CollectionStatsResponseType = {
-      [key: string]: {
-          numberOfOntologies: number
-      }
+    [key: string]: {
+      numberOfOntologies: number
+    }
   }
 
-  try{
-    let ontologiesBaseServiceUrl =  process.env.REACT_APP_API_BASE_URL;  
+  try {
+    let ontologiesBaseServiceUrl = process.env.REACT_APP_API_BASE_URL;
     let url = ontologiesBaseServiceUrl + '/getstatisticsbyschema?schema=collection';
     let result = await fetch(url, getCallSetting);
-    if(!result.ok){
+    if (!result.ok) {
       return Promise.reject(new Error(result.statusText));
     }
-    let colStats:Array<CollectionStatsResponseType> = await result.json();
+    let colStats: Array<CollectionStatsResponseType> = await result.json();
     let collections = [];
-    for(let colMultiKey in colStats){      
+    for (let colMultiKey in colStats) {
       let record = {
         "collection": colMultiKey.split(',')[1].split(']')[0].trim(), // format example: MultiKey[collection, NFDI4Energy]
         "ontologiesCount": colStats[colMultiKey]['numberOfOntologies']
-      };    
-      collections.push(record)    
-    }    
+      };
+      collections.push(record)
+    }
     return collections;
   }
-  catch(e){    
+  catch (e) {
     return Promise.reject(e);
   }
 }
 
 
 /* react query key: allCollectionsWithTheirOntologies  */
-export async function fetchAllCollectionWithOntologyList():Promise<Array<CollectionWithItsOntologyListData>> {
+export async function fetchAllCollectionWithOntologyList(): Promise<Array<CollectionWithItsOntologyListData>> {
   type CollectionIdsResponseType = {
-      _embedded: {
-          strings: Array<{
-              content: string
-          }>
-      }
+    _embedded: {
+      strings: Array<{
+        content: string
+      }>
+    }
   }
 
-  try{
-    let url = `${process.env.REACT_APP_COLLECTION_IDS_BASE_URL}`;          
+  try {
+    let url = `${process.env.REACT_APP_COLLECTION_IDS_BASE_URL}`;
     let resp = await fetch(url, getCallSetting);
-    if(!resp.ok){
+    if (!resp.ok) {
       return Promise.reject(new Error(resp.statusText));
     }
-    let cols:CollectionIdsResponseType = await resp.json();
+    let cols: CollectionIdsResponseType = await resp.json();
     let collections = cols['_embedded']["strings"];
     let result = [];
-    for( let col of collections ){                      
+    for (let col of collections) {
       let collectionOntologies = await fetchOntologyListForCollections([col['content']], false);
       let collectionOntologiesIds = [];
-      for(let onto of collectionOntologies){
-        let temp = {"ontologyId": onto['ontologyId'].toUpperCase(), "purl": onto?.config?.id};
+      for (let onto of collectionOntologies) {
+        let temp = { "ontologyId": onto['ontologyId'].toUpperCase(), "purl": onto?.config?.id };
         collectionOntologiesIds.push(temp)
       }
-      let record = {"collection": col['content'], "ontologies": collectionOntologiesIds};    
+      let record = { "collection": col['content'], "ontologies": collectionOntologiesIds };
       result.push(record);
-    }    
-    return result;    
+    }
+    return result;
   }
-  catch(e){
+  catch (e) {
     return Promise.reject(e);
-  }  
+  }
 }
 
 
 
-export async function fetchOntologyListForCollections (collectionsIds:Array<string>, exclusive:boolean):Promise<Array<OntologyData>>{
+export async function fetchOntologyListForCollections(collectionsIds: Array<string>, exclusive: boolean): Promise<Array<OntologyData>> {
   /*
       Fetch ontology list for a given list of collections Ids. 
       Can be used to get ontology list based on collection filter
@@ -81,27 +81,27 @@ export async function fetchOntologyListForCollections (collectionsIds:Array<stri
   */
 
   type CollectionOntologiesResponseType = {
-      _embedded: {
-          ontologies: Array<OntologyData>
-      }
+    _embedded: {
+      ontologies: Array<OntologyData>
+    }
   }
 
-  try{
-      let OntologiesBaseServiceUrl =  process.env.REACT_APP_API_BASE_URL;  
-      let ontologiesCount = 100000;
-      let targetUrl = `${OntologiesBaseServiceUrl}/filterby?schema=collection&page=0&size=${ontologiesCount}&exclusive=${exclusive}`;
-      let urlPros = "";
-      for(let col of collectionsIds){
-      if (col !== ""){
-          urlPros += ("&classification=" + encodeURIComponent(col));
+  try {
+    let OntologiesBaseServiceUrl = process.env.REACT_APP_API_BASE_URL;
+    let ontologiesCount = 100000;
+    let targetUrl = `${OntologiesBaseServiceUrl}/filterby?schema=collection&page=0&size=${ontologiesCount}&exclusive=${exclusive}`;
+    let urlPros = "";
+    for (let col of collectionsIds) {
+      if (col !== "") {
+        urlPros += ("&classification=" + encodeURIComponent(col));
       }
-      }
-      targetUrl += urlPros;
-      let result = await fetch(targetUrl, getCallSetting);      
-      let collectionOntologiesList:CollectionOntologiesResponseType = await result.json();      
-      return collectionOntologiesList['_embedded']['ontologies'];        
+    }
+    targetUrl += urlPros;
+    let result = await fetch(targetUrl, getCallSetting);
+    let collectionOntologiesList: CollectionOntologiesResponseType = await result.json();
+    return collectionOntologiesList['_embedded']['ontologies'];
   }
-  catch(e){
-      return [];
-  }        
+  catch (e) {
+    return [];
+  }
 }
--- src/api/tsMicroBackendCalls.js
+++ src/api/tsMicroBackendCalls.js
@@ -3,112 +3,112 @@ import { getTsPluginHeaders } from "./header";
 
 
 
-export async function getOntologyGithubIssueList(ontologyIssueTrackerUrl, issueState, issueType, resultCountPerPage=10, pageNumber=1){
-    try{ 
-        let urlPath = ontologyIssueTrackerUrl.split("https://github.com/")[1];        
-        let endpoint = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/github/issuelist?';
-        endpoint += ("path=" + encodeURIComponent(urlPath));
-        endpoint += ("&state=" + issueState);
-        endpoint += ("&type=" + issueType);
-        endpoint += ("&size=" + resultCountPerPage);
-        endpoint += ("&page=" + pageNumber);
-        let header = {};
-        header["X-TS-Frontend-Id"] = process.env.REACT_APP_PROJECT_ID;
-        header["X-TS-Frontend-Token"] = process.env.REACT_APP_MICRO_BACKEND_TOKEN;
-        let result = await fetch(endpoint, {headers:header});
-        result = await result.json();
-        result = result['_result']            
-        return result.issues;
-    }
-    catch(e){            
-        return [];
-    }        
+export async function getOntologyGithubIssueList(ontologyIssueTrackerUrl, issueState, issueType, resultCountPerPage = 10, pageNumber = 1) {
+  try {
+    let urlPath = ontologyIssueTrackerUrl.split("https://github.com/")[1];
+    let endpoint = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/github/issuelist?';
+    endpoint += ("path=" + encodeURIComponent(urlPath));
+    endpoint += ("&state=" + issueState);
+    endpoint += ("&type=" + issueType);
+    endpoint += ("&size=" + resultCountPerPage);
+    endpoint += ("&page=" + pageNumber);
+    let header = {};
+    header["X-TS-Frontend-Id"] = process.env.REACT_APP_PROJECT_ID;
+    header["X-TS-Frontend-Token"] = process.env.REACT_APP_MICRO_BACKEND_TOKEN;
+    let result = await fetch(endpoint, { headers: header });
+    result = await result.json();
+    result = result['_result']
+    return result.issues;
+  }
+  catch (e) {
+    return [];
+  }
 }
 
 
 
-export async function sendResolveRequest({objectType, objectId, action, creatorUsername}){
-    try{
-        let headers = getTsPluginHeaders({withAccessToken:true});  
-        let formData = new FormData();
-        formData.append('objectType', objectType);
-        formData.append('objectId', objectId);
-        formData.append('action', action);
-        formData.append('creatorUsername', creatorUsername);
-        let resolveUrl = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/report/resolve_report';
-        let result = await fetch(resolveUrl, {method:'POST', headers:headers, body:formData});
-        if (result.status !== 200){
-            return false;
-        }
-        result = await result.json();
-        return result['_result']['resolved'];
+export async function sendResolveRequest({ objectType, objectId, action, creatorUsername }) {
+  try {
+    let headers = getTsPluginHeaders({ withAccessToken: true });
+    let formData = new FormData();
+    formData.append('objectType', objectType);
+    formData.append('objectId', objectId);
+    formData.append('action', action);
+    formData.append('creatorUsername', creatorUsername);
+    let resolveUrl = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/report/resolve';
+    let result = await fetch(resolveUrl, { method: 'POST', headers: headers, body: formData });
+    if (result.status !== 200) {
+      return false;
     }
-    catch(e){
-        return false;
-    }
+    result = await result.json();
+    return result['_result']['resolved'];
+  }
+  catch (e) {
+    return false;
+  }
 }
 
 
 
-export async function getReportList(){
-    try{
-        let headers = getTsPluginHeaders({withAccessToken:true});         
-        let url = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/report/report_list';
-        let result = await fetch(url, {method:'GET', headers:headers});
-        if (result.status !== 200){
-            return [];
-        }
-        result = await result.json();
-        return result['_result']['reports'];
+export async function getReportList() {
+  try {
+    let headers = getTsPluginHeaders({ withAccessToken: true });
+    let url = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/report/list';
+    let result = await fetch(url, { method: 'GET', headers: headers });
+    if (result.status !== 200) {
+      return [];
     }
-    catch(e){
-        return [];
-    }
+    result = await result.json();
+    return result['_result']['reports'];
+  }
+  catch (e) {
+    return [];
+  }
 }
 
 
 
-export async function getGitRepoTemplates({repoUrl, gitUsername}){
-    try{
-        let headers = getTsPluginHeaders({withAccessToken:true});         
-        let url = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/github/get_issue_templates';
-        let formData = new FormData();
-        formData.append("repo_url", repoUrl);
-        formData.append("username", gitUsername);
-        let result = await fetch(url, {method:'POST', headers:headers, body:formData});
-        if (result.status !== 200){
-            return false;
-        }
-        result = await result.json();
-        return result['_result']['templates'];
+export async function getGitRepoTemplates({ repoUrl, gitUsername }) {
+  try {
+    let headers = getTsPluginHeaders({ withAccessToken: true });
+    let url = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/github/get_issue_templates';
+    let formData = new FormData();
+    formData.append("repo_url", repoUrl);
+    formData.append("username", gitUsername);
+    let result = await fetch(url, { method: 'POST', headers: headers, body: formData });
+    if (result.status !== 200) {
+      return false;
     }
-    catch(e){        
-        return false;
-    }
+    result = await result.json();
+    return result['_result']['templates'];
+  }
+  catch (e) {
+    return false;
+  }
 }
 
 
 
-export async function submitGitIssue({repoUrl, gitUsername, issueTitle, issueBody, issueType, ontologyId}){
-    try{
-        let data = new FormData();
-        let headers = getTsPluginHeaders({withAccessToken:true});       
-        data.append("ontology_id", ontologyId);
-        data.append("username", gitUsername);        
-        data.append("title", issueTitle);
-        data.append("content", issueBody);        
-        data.append("issueType", issueType);
-        data.append("repo_url", repoUrl);
-        let url = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/github/submit_issue';
-        let result = await fetch(url, {method:'POST', headers:headers, body:data});
-        if (result.status !== 200){
-            return false;
-        }
-        result = await result.json();
-        return result['_result']['new_issue_url'];
+export async function submitGitIssue({ repoUrl, gitUsername, issueTitle, issueBody, issueType, ontologyId }) {
+  try {
+    let data = new FormData();
+    let headers = getTsPluginHeaders({ withAccessToken: true });
+    data.append("ontology_id", ontologyId);
+    data.append("username", gitUsername);
+    data.append("title", issueTitle);
+    data.append("content", issueBody);
+    data.append("issueType", issueType);
+    data.append("repo_url", repoUrl);
+    let url = process.env.REACT_APP_MICRO_BACKEND_ENDPOINT + '/github/submit_issue';
+    let result = await fetch(url, { method: 'POST', headers: headers, body: data });
+    if (result.status !== 200) {
+      return false;
     }
-    catch(e){
-        return false;
-    }
+    result = await result.json();
+    return result['_result']['new_issue_url'];
+  }
+  catch (e) {
+    return false;
+  }
 }
 
