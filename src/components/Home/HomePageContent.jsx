71d08810d60e3deea1c6e55fa68a7b7d8ce89747
pooya.oa <pooya.oladazimi@tib.eu>
2025-03-04 12:29:49 +0100

    fix the issue with image path on homepage

--- src/components/Home/HomePageContent.jsx
+++ src/components/Home/HomePageContent.jsx
@@ -86,7 +86,7 @@ const CollectionCard = ({ collectionId }) => {
   return (
     <div className="collection-card">
       <a href={subPath + collectionsInfoJson[collectionId]['ontology_list_url']} className="collection-image-anchor" target='_blank'>
-        <img className="img-fluid collection-logo" alt="collection_logo" src={subPath + collectionsInfoJson[collectionId]['logo']} />
+        <img className="img-fluid collection-logo" alt="collection_logo" src={collectionsInfoJson[collectionId]['logo']} />
       </a>
       <div className="collection-card-text">
         <p className="trunc">{collectionsInfoJson[collectionId]['text']}</p>
