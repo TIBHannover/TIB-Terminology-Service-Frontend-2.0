9fb7519502ccbceab0170c3c66292a5632ed52ba
pooya.oa <pooya.oladazimi@tib.eu>
2025-05-08 15:11:27 +0200

    fix the issue with navbar not changing clicked class

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+const Navbar = () => {
 
-  let urlPath = window.location.pathname;
+  const urlPath = window.location.pathname;
   let basePath = process.env.REACT_APP_PROJECT_SUB_PATH;
+
+  function handleClick(e) {
+    let clickedElement = document.getElementsByClassName("nav-clicked");
+    if (clickedElement.length !== 0) {
+      clickedElement[0].classList.remove("nav-clicked");
+    }
+    e.target.classList.add("nav-clicked");
+  }
+
+
   return (
     <div className='col-lg-7 col-md-6'>
       <nav className="navbar navbar-expand-xl site-navbar">
@@ -14,36 +24,36 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="siteMainNavbar">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item" key={1}>
-              <Link className={"nav-link navbar-item " + ((urlPath === "/ts" || urlPath === "/ts/") && "nav-clicked")} to={basePath + "/"}>Home</Link>
+              <Link onClick={handleClick} className={"nav-link navbar-item " + ((urlPath === "/ts" || urlPath === "/ts/") ? "nav-clicked" : "")} to={basePath + "/"}>Home</Link>
             </li>
             {process.env.REACT_APP_COLLECTION_TAB_SHOW === "true" &&
               <li className="nav-item" key={2}>
-                <Link className={"nav-link navbar-item stour-collections-navbar-item  " + (urlPath.includes("/collections") && "nav-clicked")} to={basePath + "/collections"}>Collections</Link>
+                <Link onClick={handleClick} className={"nav-link navbar-item stour-collections-navbar-item  " + (urlPath.includes("/collections") ? "nav-clicked" : "")} to={basePath + "/collections"}>Collections</Link>
               </li>
             }
             <li className="nav-item second-step" key={3}>
-              <Link className={"nav-link navbar-item stour-ontologies-navbar-item  " + (urlPath.includes("/ontologies") && "nav-clicked")} to={basePath + "/ontologies"}>Ontologies</Link>
+              <Link onClick={handleClick} className={"nav-link navbar-item stour-ontologies-navbar-item  " + (urlPath.includes("/ontologies") ? "nav-clicked" : "")} to={basePath + "/ontologies"}>Ontologies</Link>
             </li>
             <li className="nav-item" key={4}>
-              <Link className={"nav-link navbar-item stour-help-navbar-item  " + (urlPath.includes("/help") && "nav-clicked")} to={basePath + "/help"}>Help</Link>
+              <Link onClick={handleClick} className={"nav-link navbar-item stour-help-navbar-item  " + (urlPath.includes("/help") ? "nav-clicked" : "")} to={basePath + "/help"}>Help</Link>
             </li>
             {process.env.REACT_APP_PROJECT_ID !== "nfdi4ing" &&
               <li className="nav-item" key={5}>
-                <Link className={"nav-link navbar-item stour-api-navbar-item " + (urlPath.includes("/api") && "nav-clicked")} to={basePath + "/api"}>API</Link>
+                <Link onClick={handleClick} className={"nav-link navbar-item stour-api-navbar-item " + (urlPath.includes("/api") ? "nav-clicked" : "")} to={basePath + "/api"}>API</Link>
               </li>
             }
             {process.env.REACT_APP_PROJECT_ID === "nfdi4ing" &&
               <li className="nav-item" key={6}>
-                <Link className={"nav-link navbar-item " + (urlPath.includes("/docs") && "nav-clicked")} to={basePath + "/docs"}>Documentation</Link>
+                <Link onClick={handleClick} className={"nav-link navbar-item " + (urlPath.includes("/docs") ? "nav-clicked" : "")} to={basePath + "/docs"}>Documentation</Link>
               </li>
             }
             {process.env.REACT_APP_PROJECT_ID === "nfdi4ing" &&
               <li className="nav-item" key={7}>
-                <Link className={"nav-link navbar-item " + (urlPath.includes("/usage") && "nav-clicked")} to={basePath + "/usage"}>Usage</Link>
+                <Link onClick={handleClick} className={"nav-link navbar-item " + (urlPath.includes("/usage") ? "nav-clicked" : "")} to={basePath + "/usage"}>Usage</Link>
               </li>
             }
             <li className="nav-item" key={8}>
-              <Link className={"nav-link navbar-item stour-about-navbar-item  " + (urlPath.includes("/about") && "nav-clicked")} to={basePath + "/about"}>About</Link>
+              <Link onClick={handleClick} className={"nav-link navbar-item stour-about-navbar-item  " + (urlPath.includes("/about") ? "nav-clicked" : "")} to={basePath + "/about"}>About</Link>
             </li>
           </ul>
         </div>
