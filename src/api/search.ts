963d1bc292ecf929e2583db5af59e0cf2ddfdedd
pooya.oa <pooya.oladazimi@tib.eu>
2025-06-17 11:23:22 +0200

    implement v2 jumpto api call

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,87 +1,90 @@
-import { getCallSetting } from "./constants";
+import {getCallSetting} from "./constants";
 import {
-  SearchApiInput,
-  SuggestAndSelectApiInput,
-  SearchApiResponse,
-  BaseSearchSingleResult,
-  AutoSuggestSingleResult
+    SearchApiInput,
+    SuggestAndSelectApiInput,
+    SearchApiResponse,
+    BaseSearchSingleResult,
+    AutoSuggestSingleResult
 } from "./types/searchApiTypes";
 import Toolkit from "../Libs/Toolkit";
 
 
-
-
 export async function olsSearch(inputData: SearchApiInput): Promise<SearchApiResponse | []> {
-  try {
-    let lang = Toolkit.getVarInLocalSrorageIfExist('language', 'en');
-    let rangeStart: number = (inputData.page - 1) * inputData.size;
-    let searchUrl: string = process.env.REACT_APP_SEARCH_URL + `?q=${encodeURIComponent(inputData.searchQuery)}&start=${rangeStart}&groupField=iri&rows=${inputData.size}&lang=${lang}&exclusive=true`;
-    searchUrl = inputData.selectedOntologies.length !== 0 ? (searchUrl + `&ontology=${inputData.selectedOntologies.join(',')}`) : searchUrl;
-    searchUrl = inputData.selectedTypes.length !== 0 ? (searchUrl + `&type=${inputData.selectedTypes.join(',')}`) : searchUrl;
-    searchUrl = inputData.searchInValues.length !== 0 ? (searchUrl + `&queryFields=${inputData.searchInValues.join(',')}`) : searchUrl;
-    searchUrl = inputData.searchUnderIris.length !== 0 ? (searchUrl + `&childrenOf=${inputData.searchUnderIris.join(',')}`) : searchUrl;
-    searchUrl = inputData.searchUnderAllIris.length !== 0 ? (searchUrl + `&allChildrenOf=${inputData.searchUnderAllIris.join(',')}`) : searchUrl;
-    searchUrl = inputData.obsoletes ? (searchUrl + "&obsoletes=true") : searchUrl;
-    searchUrl = inputData.exact ? (searchUrl + "&exact=true") : searchUrl;
-    searchUrl = inputData.isLeaf ? (searchUrl + "&isLeaf=true") : searchUrl;
-    if (process.env.REACT_APP_PROJECT_NAME === "" && inputData.selectedCollections.length !== 0) {
-      // If TIB General. Set collections if exist in filter
-      searchUrl += `&schema=collection&classification=${inputData.selectedCollections.join(',')}`;
+    try {
+        let lang = Toolkit.getVarInLocalSrorageIfExist('language', 'en');
+        let rangeStart: number = (inputData.page - 1) * inputData.size;
+        let searchUrl: string = process.env.REACT_APP_SEARCH_URL + `?q=${encodeURIComponent(inputData.searchQuery)}&start=${rangeStart}&groupField=iri&rows=${inputData.size}&lang=${lang}&exclusive=true`;
+        searchUrl = inputData.selectedOntologies.length !== 0 ? (searchUrl + `&ontology=${inputData.selectedOntologies.join(',')}`) : searchUrl;
+        searchUrl = inputData.selectedTypes.length !== 0 ? (searchUrl + `&type=${inputData.selectedTypes.join(',')}`) : searchUrl;
+        searchUrl = inputData.searchInValues.length !== 0 ? (searchUrl + `&queryFields=${inputData.searchInValues.join(',')}`) : searchUrl;
+        searchUrl = inputData.searchUnderIris.length !== 0 ? (searchUrl + `&childrenOf=${inputData.searchUnderIris.join(',')}`) : searchUrl;
+        searchUrl = inputData.searchUnderAllIris.length !== 0 ? (searchUrl + `&allChildrenOf=${inputData.searchUnderAllIris.join(',')}`) : searchUrl;
+        searchUrl = inputData.obsoletes ? (searchUrl + "&obsoletes=true") : searchUrl;
+        searchUrl = inputData.exact ? (searchUrl + "&exact=true") : searchUrl;
+        searchUrl = inputData.isLeaf ? (searchUrl + "&isLeaf=true") : searchUrl;
+        if (process.env.REACT_APP_PROJECT_NAME === "" && inputData.selectedCollections.length !== 0) {
+            // If TIB General. Set collections if exist in filter
+            searchUrl += `&schema=collection&classification=${inputData.selectedCollections.join(',')}`;
+        } else if (inputData.selectedOntologies.length === 0 && process.env.REACT_APP_PROJECT_NAME !== "") {
+            // Projects such as NFDI4CHEM. pre-set the target collection on each search
+            // This should NOT be included when ontologies are selected.
+            searchUrl += `&schema=collection&classification=${process.env.REACT_APP_PROJECT_NAME}`;
+        }
+        let result: any = await (await fetch(searchUrl, getCallSetting)).json();
+        return result;
+    } catch (e) {
+        return [];
     }
-    else if (inputData.selectedOntologies.length === 0 && process.env.REACT_APP_PROJECT_NAME !== "") {
-      // Projects such as NFDI4CHEM. pre-set the target collection on each search
-      // This should NOT be included when ontologies are selected.
-      searchUrl += `&schema=collection&classification=${process.env.REACT_APP_PROJECT_NAME}`;
-    }
-    let result: any = await (await fetch(searchUrl, getCallSetting)).json();
-    return result;
-  }
-  catch (e) {
-    return [];
-  }
 }
-
-
 
 
 export async function getJumpToResult(inputData: SuggestAndSelectApiInput, count: number = 10, lang: string = "en"): Promise<BaseSearchSingleResult[]> {
-  try {
-    inputData['searchUnderIris'] = inputData['searchUnderIris'] ? inputData['searchUnderIris'] : [];
-    inputData['searchUnderAllIris'] = inputData['searchUnderAllIris'] ? inputData['searchUnderAllIris'] : [];
-    let autocompleteApiBaseUrl: string = process.env.REACT_APP_SEARCH_URL || '';
-    autocompleteApiBaseUrl = autocompleteApiBaseUrl.split('search')[0] + "select";
-    let url = `${autocompleteApiBaseUrl}?q=${encodeURIComponent(inputData['searchQuery'])}&rows=${count}&lang=${lang}`;
-    url = inputData['ontologyIds'] ? (url + `&ontology=${inputData['ontologyIds']}`) : url;
-    url = inputData['types'] ? (url + `&type=${inputData['types']}`) : url;
-    url = inputData['obsoletes'] ? (url + "&obsoletes=true") : url;
-    url = inputData['searchUnderIris'].length !== 0 ? (url + `&childrenOf=${inputData.searchUnderIris.join(',')}`) : url;
-    url = inputData['searchUnderAllIris'].length !== 0 ? (url + `&allChildrenOf=${inputData.searchUnderAllIris.join(',')}`) : url;
-    url = inputData['collectionIds'] ? (url + `&schema=collection&classification=${inputData['collectionIds']}`) : url;
-    let result: any = await fetch(url, getCallSetting);
-    result = await result.json();
-    result = result['response']['docs'];
-    return result;
-  }
-  catch (e) {
-    return [];
-  }
+    try {
+        inputData['searchUnderIris'] = inputData['searchUnderIris'] ? inputData['searchUnderIris'] : [];
+        inputData['searchUnderAllIris'] = inputData['searchUnderAllIris'] ? inputData['searchUnderAllIris'] : [];
+        let autocompleteApiBaseUrl: string = process.env.REACT_APP_SEARCH_URL || '';
+        autocompleteApiBaseUrl = autocompleteApiBaseUrl.split('search')[0] + "select";
+        let url = `${autocompleteApiBaseUrl}?q=${encodeURIComponent(inputData['searchQuery'])}&rows=${count}&lang=${lang}`;
+        url = inputData['ontologyIds'] ? (url + `&ontology=${inputData['ontologyIds']}`) : url;
+        url = inputData['types'] ? (url + `&type=${inputData['types']}`) : url;
+        url = inputData['obsoletes'] ? (url + "&obsoletes=true") : url;
+        url = inputData['searchUnderIris'].length !== 0 ? (url + `&childrenOf=${inputData.searchUnderIris.join(',')}`) : url;
+        url = inputData['searchUnderAllIris'].length !== 0 ? (url + `&allChildrenOf=${inputData.searchUnderAllIris.join(',')}`) : url;
+        url = inputData['collectionIds'] ? (url + `&schema=collection&classification=${inputData['collectionIds']}`) : url;
+        let result: any = await fetch(url, getCallSetting);
+        result = await result.json();
+        result = result['response']['docs'];
+        return result;
+    } catch (e) {
+        return [];
+    }
 }
 
 
+export async function getJumpToResultV2(query: string, lang: string = "en") {
+    try {
+        let apiBaseUrl: string = process.env.REACT_APP_API_URL!;
+        let url = `${apiBaseUrl}/v2/entities?search=${query}&lang=${lang}&searchFields=label`;
+        let result = await (await fetch(url, getCallSetting)).json();
+        return result['elements'] ?? [];
+    } catch {
+        return [];
+    }
+}
+
 
 export async function getAutoCompleteResult(inputData: SuggestAndSelectApiInput, count: number = 5): Promise<AutoSuggestSingleResult[]> {
-  try {
-    let lang = Toolkit.getVarInLocalSrorageIfExist('language', 'en');
-    let url: string = process.env.REACT_APP_API_URL + `/suggest?q=${encodeURIComponent(inputData['searchQuery'])}&rows=${count}&lang=${lang}`;
-    url = inputData['ontologyIds'] ? (url + `&ontology=${inputData['ontologyIds']}`) : url;
-    url = inputData['types'] ? (url + `&type=${inputData['types']}`) : url;
-    url = inputData['obsoletes'] ? (url + "&obsoletes=true") : url;
-    url = inputData['collectionIds'] ? (url + `&schema=collection&classification=${inputData['collectionIds']}`) : url;
-    let searchResult: any = await fetch(url, getCallSetting);
-    searchResult = (await searchResult.json())['response']['docs'];
-    return searchResult;
-  }
-  catch (e) {
-    return [];
-  }
+    try {
+        let lang = Toolkit.getVarInLocalSrorageIfExist('language', 'en');
+        let url: string = process.env.REACT_APP_API_URL + `/suggest?q=${encodeURIComponent(inputData['searchQuery'])}&rows=${count}&lang=${lang}`;
+        url = inputData['ontologyIds'] ? (url + `&ontology=${inputData['ontologyIds']}`) : url;
+        url = inputData['types'] ? (url + `&type=${inputData['types']}`) : url;
+        url = inputData['obsoletes'] ? (url + "&obsoletes=true") : url;
+        url = inputData['collectionIds'] ? (url + `&schema=collection&classification=${inputData['collectionIds']}`) : url;
+        let searchResult: any = await fetch(url, getCallSetting);
+        searchResult = (await searchResult.json())['response']['docs'];
+        return searchResult;
+    } catch (e) {
+        return [];
+    }
 }
--- src/components/TermSet/AddTermModal.jsx
+++ src/components/TermSet/AddTermModal.jsx
@@ -5,178 +5,179 @@ import {AppContext} from "../../context/AppContext";
 import {createTermset, addTermToMultipleSets, removeTermFromSet} from "../../api/term_set";
 import FormLib from "../../Libs/FormLib";
 import Toolkit from "../../Libs/Toolkit";
-import {getJumpToResult} from "../../api/search";
+import {getJumpToResultV2} from "../../api/search";
+import TermLib from "../../Libs/TermLib";
 
 
 export const AddTermModalBtn = (props) => {
-    const {modalId} = props;
-    return (
-        <a
-            className={"btn btn-secondary btn-sm borderless-btn"}
-            data-toggle="modal"
-            data-target={"#addToTermsetModal-" + modalId}
-            data-backdrop="static"
-        >
-            <i className="bi bi-plus-square"></i>
-            {" terms"}
-        </a>
-    );
+  const {modalId} = props;
+  return (
+    <a
+      className={"btn btn-secondary btn-sm borderless-btn"}
+      data-toggle="modal"
+      data-target={"#addToTermsetModal-" + modalId}
+      data-backdrop="static"
+    >
+      <i className="bi bi-plus-square"></i>
+      {" terms"}
+    </a>
+  );
 }
 
 
 export const AddTermModal = (props) => {
-    const {modalId} = props;
-    const appContext = useContext(AppContext);
-
-    const [submited, setSubmited] = useState(false);
-    const [addedSuccess, setAddedSuccess] = useState(false);
-    const [createMode, setCreateMode] = useState(false);
-    const [selectedTermsetIds, setSelectedTermsetIds] = useState([]);
-    const [termsetNameNotValid, setTermsetNameNotValid] = useState(false);
-
-    const [termListOptions, setTermListOptions] = useState([]);
-    const [selectedTerms, setSelectedTerms] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-
-    const searchUnderRef = useRef(null);
-
-
-    function submitNewTermset() {
-        return;
+  const {modalId} = props;
+  const appContext = useContext(AppContext);
+  
+  const [submited, setSubmited] = useState(false);
+  const [addedSuccess, setAddedSuccess] = useState(false);
+  const [createMode, setCreateMode] = useState(false);
+  const [selectedTermsetIds, setSelectedTermsetIds] = useState([]);
+  const [termsetNameNotValid, setTermsetNameNotValid] = useState(false);
+  
+  const [termListOptions, setTermListOptions] = useState([]);
+  const [selectedTerms, setSelectedTerms] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  
+  const searchUnderRef = useRef(null);
+  
+  
+  function submitNewTermset() {
+    return;
+  }
+  
+  function onTermSelect(event) {
+    return;
+  }
+  
+  
+  async function onSearchTermChange(query) {
+    setLoading(true);
+    if (query === "") {
+      setTermListOptions([]);
+      return true;
     }
-
-    function onTermSelect(event) {
-        return;
+    let inputQuery = {
+      "searchQuery": query,
+      "types": "class,property,individual",
+    };
+    if (appContext.userSettings.userCollectionEnabled) {
+      inputQuery['ontologyIds'] = appContext.userSettings.activeCollection.ontology_ids.join(',');
     }
-
-
-    async function onSearchTermChange(query) {
-        setLoading(true);
-        if (query === "") {
-            setTermListOptions([]);
-            return true;
-        }
-        let inputQuery = {
-            "searchQuery": query,
-            "types": "class,property,individual",
-        };
-        if (appContext.userSettings.userCollectionEnabled) {
-            inputQuery['ontologyIds'] = appContext.userSettings.activeCollection.ontology_ids.join(',');
-        }
-        let terms = await getJumpToResult(inputQuery, 20);
-        let options = [];
-        for (let term of terms) {
-            let opt = {};
-            opt['text'] = `${term['ontology_prefix']}:${term['label']} (${term['type']})`;
-            opt['iri'] = term['iri'];
-            options.push(opt);
-        }
-        setLoading(false);
-        setTermListOptions(options);
+    let terms = await getJumpToResultV2(query);
+    let options = [];
+    for (let term of terms) {
+      let opt = {};
+      opt['text'] = `${term['ontologyId']}:${TermLib.extractLabel(term)} (${TermLib.getTermType(term)})`;
+      opt['iri'] = term['iri'];
+      options.push(opt);
     }
-
-
-    function closeModal() {
-        setAddedSuccess(false);
-        setSubmited(false);
-        setCreateMode(false);
-        setSelectedTermsetIds([]);
-    }
-
-
-    // useEffect(() => {
-    //     let options = [];
-    //     let existingSets = [];
-    //     let removeLoadingMap = new Map(removeLoading);
-    //     for (let tset of appContext.userTermsets) {
-    //         if (!tset.terms.find((tsetTerm) => tsetTerm.iri === term.iri)) {
-    //             options.push({"text": tset.name, "id": tset.id});
-    //         } else {
-    //             removeLoadingMap.set(tset.id, false);
-    //             existingSets.push({name: tset.name, id: tset.id});
-    //         }
-    //     }
-    //     setTermExistingSets(existingSets)
-    //     setRemoveLoading(removeLoadingMap);
-    //     setTermSets(options);
-    // }, [term, appContext.userTermsets]);
-
-
-    return (
-        <div>
-            <div className="modal fade" id={"addToTermsetModal-" + modalId} tabIndex={-1} role="dialog"
-                 aria-labelledby={"addToTermsetModal-" + modalId} aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title"
-                                id={"addToTermsetModal-" + modalId}>{`Add terms to this set`}</h5>
-                            {!submited &&
-                                <button onClick={closeModal} type="button" className="close" data-dismiss="modal"
-                                        aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            }
-                        </div>
-                        <div className="modal-body">
-                            {!submited &&
-                                <Multiselect
-                                    isObject={true}
-                                    options={termListOptions}
-                                    selectedValues={selectedTerms}
-                                    onSelect={onTermSelect}
-                                    onRemove={onTermSelect}
-                                    onSearch={onSearchTermChange}
-                                    displayValue={"text"}
-                                    avoidHighlightFirstOption={true}
-                                    loading={loading}
-                                    closeIcon={"cancel"}
-                                    id="search-terms"
-                                    placeholder={"class, property, individual"}
-                                    ref={searchUnderRef}
-                                />
-                            }
-                            {submited && addedSuccess &&
-                                <AlertBox
-                                    type="success"
-                                    message="Added successfully!"
-                                    alertColumnClass="col-sm-12"
-                                />
-                            }
-                            {submited && !addedSuccess &&
-                                <AlertBox
-                                    type="danger"
-                                    message="Something went wrong. Please try again!"
-                                    alertColumnClass="col-sm-12"
-                                />
-                            }
-                        </div>
-                        <div className="modal-footer justify-content-center">
-                            {!submited &&
-                                <button
-                                    type="button"
-                                    className="btn btn-secondary"
-                                    onClick={() => {
-                                        if (createMode) {
-                                            submitNewTermset();
-                                        }
-                                    }}>
-                                    {"Add"}
-                                </button>
-                            }
-                            {submited &&
-                                <button
-                                    type="button"
-                                    className="btn btn-secondary"
-                                    data-dismiss="modal"
-                                    onClick={closeModal}>Close
-                                </button>
-                            }
-                        </div>
-                    </div>
-                </div>
+    setLoading(false);
+    setTermListOptions(options);
+  }
+  
+  
+  function closeModal() {
+    setAddedSuccess(false);
+    setSubmited(false);
+    setCreateMode(false);
+    setSelectedTermsetIds([]);
+  }
+  
+  
+  // useEffect(() => {
+  //     let options = [];
+  //     let existingSets = [];
+  //     let removeLoadingMap = new Map(removeLoading);
+  //     for (let tset of appContext.userTermsets) {
+  //         if (!tset.terms.find((tsetTerm) => tsetTerm.iri === term.iri)) {
+  //             options.push({"text": tset.name, "id": tset.id});
+  //         } else {
+  //             removeLoadingMap.set(tset.id, false);
+  //             existingSets.push({name: tset.name, id: tset.id});
+  //         }
+  //     }
+  //     setTermExistingSets(existingSets)
+  //     setRemoveLoading(removeLoadingMap);
+  //     setTermSets(options);
+  // }, [term, appContext.userTermsets]);
+  
+  
+  return (
+    <div>
+      <div className="modal fade" id={"addToTermsetModal-" + modalId} tabIndex={-1} role="dialog"
+           aria-labelledby={"addToTermsetModal-" + modalId} aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title"
+                  id={"addToTermsetModal-" + modalId}>{`Add terms to this set`}</h5>
+              {!submited &&
+                <button onClick={closeModal} type="button" className="close" data-dismiss="modal"
+                        aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              }
             </div>
+            <div className="modal-body">
+              {!submited &&
+                <Multiselect
+                  isObject={true}
+                  options={termListOptions}
+                  selectedValues={selectedTerms}
+                  onSelect={onTermSelect}
+                  onRemove={onTermSelect}
+                  onSearch={onSearchTermChange}
+                  displayValue={"text"}
+                  avoidHighlightFirstOption={true}
+                  loading={loading}
+                  closeIcon={"cancel"}
+                  id="search-terms"
+                  placeholder={"class, property, individual"}
+                  ref={searchUnderRef}
+                />
+              }
+              {submited && addedSuccess &&
+                <AlertBox
+                  type="success"
+                  message="Added successfully!"
+                  alertColumnClass="col-sm-12"
+                />
+              }
+              {submited && !addedSuccess &&
+                <AlertBox
+                  type="danger"
+                  message="Something went wrong. Please try again!"
+                  alertColumnClass="col-sm-12"
+                />
+              }
+            </div>
+            <div className="modal-footer justify-content-center">
+              {!submited &&
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={() => {
+                    if (createMode) {
+                      submitNewTermset();
+                    }
+                  }}>
+                  {"Add"}
+                </button>
+              }
+              {submited &&
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                  onClick={closeModal}>Close
+                </button>
+              }
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
