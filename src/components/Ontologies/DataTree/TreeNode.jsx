1e07a732e811bf4c3ee2b42558bc26d9d311f2de
pooya.oa <pooya.oladazimi@tib.eu>
2025-08-07 12:43:20 +0200

    add number of children to tree nodes

--- src/components/Ontologies/DataTree/TreeNode.jsx
+++ src/components/Ontologies/DataTree/TreeNode.jsx
@@ -14,63 +14,67 @@ class TreeNodeController {
     this.nodeId = "";
     this.children = [];
   }
-
-
-  buildNodeWithReact({ nodeObject, nodeIsClicked = false, isExpanded = false, isSkos = false }) {
-    let nodeLabel = TermLib.extractLabel(nodeObject);
+  
+  
+  buildNodeWithReact({nodeObject, nodeIsClicked = false, isExpanded = false, isSkos = false}) {
+    let nodeLabel = `${TermLib.extractLabel(nodeObject)} (${nodeObject["numDescendants"] ?? "0"})`;
     let nodeHasChildren = !isSkos ? TermLib.termHasChildren(nodeObject) : SkosLib.skosTermHasChildren(nodeObject);
     let partOfSymbol = "";
     let individualSymbol = "";
     if (nodeObject.isObsolete) {
       nodeLabel = React.createElement("s", {}, nodeLabel);
     }
-    this.textDiv = React.createElement("div", { "className": "li-label-text stour-tree-node" }, nodeLabel);
-
+    this.textDiv = React.createElement("div", {"className": "li-label-text stour-tree-node"}, nodeLabel);
+    
     if (nodeObject.hasHierarchicalParents && false) {
       // find the part of relation metadat
-      partOfSymbol = React.createElement("div", { "className": "p-icon-style" }, "P");
+      partOfSymbol = React.createElement("div", {"className": "p-icon-style"}, "P");
     }
-
+    
     if (TermLib.getTermType(nodeObject) === "individual") {
-      individualSymbol = React.createElement("div", { "className": "i-icon-style", "title": "individual" }, <i class="fa fa-solid fa-info"></i>);
+      individualSymbol = React.createElement("div", {"className": "i-icon-style", "title": "individual"}, <i
+        class="fa fa-solid fa-info"></i>);
     }
-
+    
     if (nodeIsClicked) {
-      this.textDivContainer = React.createElement("div", { "className": "tree-text-container clicked targetNodeByIri" }, partOfSymbol, individualSymbol, this.textDiv);
-    }
-    else {
-      this.textDivContainer = React.createElement("div", { "className": "tree-text-container " }, partOfSymbol, individualSymbol, this.textDiv);
+      this.textDivContainer = React.createElement("div", {"className": "tree-text-container clicked targetNodeByIri"}, partOfSymbol, individualSymbol, this.textDiv);
+    } else {
+      this.textDivContainer = React.createElement("div", {"className": "tree-text-container "}, partOfSymbol, individualSymbol, this.textDiv);
     }
     this.nodeIri = nodeObject.iri;
     if (!nodeHasChildren) {
       this.classes += " leaf-node";
-      this.iconInTree = React.createElement("i", { "className": "" }, "");
-    }
-    else if (nodeHasChildren && !isExpanded) {
+      this.iconInTree = React.createElement("i", {"className": ""}, "");
+    } else if (nodeHasChildren && !isExpanded) {
       this.classes += " closed";
-      this.iconInTree = React.createElement("i", { "className": "fa fa-plus stour-tree-expand-node-icon", "aria-hidden": "true" }, "");
-    }
-    else {
+      this.iconInTree = React.createElement("i", {
+        "className": "fa fa-plus stour-tree-expand-node-icon",
+        "aria-hidden": "true"
+      }, "");
+    } else {
       this.classes += " opened";
-      this.iconInTree = React.createElement("i", { "className": "fa fa-minus stour-tree-expand-node-icon", "aria-hidden": "true" }, "");
+      this.iconInTree = React.createElement("i", {
+        "className": "fa fa-minus stour-tree-expand-node-icon",
+        "aria-hidden": "true"
+      }, "");
     }
-
+    
     let node = React.createElement(this.nodeRootElementName, {
-      "data-iri": this.nodeIri,
-      "data-id": nodeObject.id ?? TermLib.makeTermIdForTree(nodeObject),
-      "className": this.classes,
-      "id": nodeObject.id ?? TermLib.makeTermIdForTree(nodeObject)
-    }
+        "data-iri": this.nodeIri,
+        "data-id": nodeObject.id ?? TermLib.makeTermIdForTree(nodeObject),
+        "className": this.classes,
+        "id": nodeObject.id ?? TermLib.makeTermIdForTree(nodeObject)
+      }
       , this.iconInTree, this.textDivContainer, this.children
     );
-
+    
     return node;
-
+    
   }
-
-
-  buildNodeWithTradionalJs({ nodeObject, nodeIsClicked = false, isExpanded = false, isSkos = false }) {
-    let nodeLabel = TermLib.extractLabel(nodeObject);
+  
+  
+  buildNodeWithTradionalJs({nodeObject, nodeIsClicked = false, isExpanded = false, isSkos = false}) {
+    let nodeLabel = `${TermLib.extractLabel(nodeObject)} (${nodeObject["numDescendants"] ?? "0"})`;
     let nodeHasChildren = !isSkos ? TermLib.termHasChildren(nodeObject) : SkosLib.skosTermHasChildren(nodeObject);
     this.textDiv = document.createElement("div");
     let label = document.createTextNode(nodeLabel);
@@ -92,20 +96,18 @@ class TreeNodeController {
     this.nodeIri = nodeObject.iri;
     if (!nodeHasChildren) {
       node.classList.add("leaf-node");
-    }
-    else if (nodeHasChildren && !isExpanded) {
+    } else if (nodeHasChildren && !isExpanded) {
       node.classList.add("closed");
       this.iconInTree.classList.add('fa');
       this.iconInTree.classList.add('fa-plus');
       this.iconInTree.classList.add('stour-tree-expand-node-icon');
-    }
-    else {
+    } else {
       node.classList.add("opened");
       this.iconInTree.classList.add('fa');
       this.iconInTree.classList.add('fa-minus');
       this.iconInTree.classList.add('stour-tree-expand-node-icon');
     }
-
+    
     node.appendChild(this.iconInTree);
     if (nodeObject.hasHierarchicalParents && false) {
       let partOfSymbol = document.createElement("div");
@@ -114,7 +116,7 @@ class TreeNodeController {
       partOfSymbol.classList.add("p-icon-style");
       this.textDivContainer.appendChild(partOfSymbol);
     }
-
+    
     if (TermLib.getTermType(nodeObject) === "individual") {
       let individualSymbol = document.createElement("div");
       individualSymbol.title = "individual";
@@ -124,85 +126,85 @@ class TreeNodeController {
       individualSymbol.classList.add("i-icon-style");
       this.textDivContainer.appendChild(individualSymbol);
     }
-
+    
     this.textDivContainer.appendChild(this.textDiv);
     node.appendChild(this.textDivContainer);
-
+    
     return node;
   }
-
-
+  
+  
   unClickAllNodes() {
     let selectedElement = document.querySelectorAll(".clicked");
     for (let i = 0; i < selectedElement.length; i++) {
       selectedElement[i].classList.remove("clicked");
     }
   }
-
+  
   scrollToNode(id) {
     let position = document.getElementById(id).offsetTop;
     document.getElementsByClassName('tree-page-left-part')[0].scrollTop = position;
   }
-
+  
   scrollToNextNode(id) {
     document.getElementsByClassName('tree-page-left-part')[0].getElementById(id).nextSibling.scrollIntoView();
   }
-
+  
   scrollToPreviousNode(id) {
     let position = document.getElementById(id).previousSibling.offsetTop;
     document.getElementsByClassName('tree-page-left-part')[0].scrollTop = position;
   }
-
+  
   getClickedNodeDiv(node) {
     if (node.tagName === "DIV") {
       return node;
     }
     return null;
   }
-
+  
   getClickedNodeIri(node) {
     return node.parentNode.dataset.iri;
   }
-
+  
   getClickedNodeId(node) {
     return node.parentNode.id;
   }
-
+  
   getNodeLabelTextById(id) {
     return document.getElementById(id).getElementsByClassName('tree-text-container')[0];
   }
-
+  
   getFirstNodeInTree() {
     let treeRootUl = document.getElementById('tree-root-ul');
     return treeRootUl.querySelector('li:first-child').getElementsByClassName('tree-text-container')[0];
   }
-
+  
   getFirstChildLabelText(id) {
     return document.getElementById("children_for_" + id).getElementsByClassName('tree-text-container')[0];
   }
-
+  
   getNodeNextSiblings(id) {
     let node = document.getElementById(id);
     return node.nextSibling.getElementsByClassName('tree-text-container')[0];
   }
-
+  
   getParentNode(id) {
     let node = document.getElementById(id);
     return node.parentNode.parentNode;
   }
-
+  
   getNodeChildren(id) {
     return document.getElementById("children_for_" + id).getElementsByClassName('tree-node-li');
   }
-
+  
   isNodeExpanded(node) {
     return node.classList.contains("opened");
   }
-
+  
   isNodeClosed(node) {
     return node.classList.contains("closed")
   }
-
+  
   isNodeLeaf(node) {
     return node.classList.contains("leaf-node");
   }
