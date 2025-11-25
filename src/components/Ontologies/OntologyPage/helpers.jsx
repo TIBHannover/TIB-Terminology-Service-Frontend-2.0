16e0b37834c43515d4075c3eb36c762be62a6aae
pooya.oa <pooya.oladazimi@tib.eu>
2025-11-25 11:50:49 +0100

    remove to banner for ontology page

--- src/components/Ontologies/OntologyPage/helpers.jsx
+++ src/components/Ontologies/OntologyPage/helpers.jsx
@@ -69,33 +69,13 @@ export const OntologyPageTabs = (props) => {
 
 
 export const OntologyPageHeadSection = () => {
-  const ontologyPageContext = useContext(OntologyPageContext);
   return (
-    <div className='span'>
-      <div className='row ont-info-bar header-collapseable-section'>
-        <div className="col-sm-12">
-          <div>
-            <h2>
-              <Link
-                className={"ont-info-bar-title"}
-                to={process.env.REACT_APP_PROJECT_SUB_PATH + "/ontologies/" + ontologyPageContext.ontology.ontologyId}
-              >
-                {ontologyPageContext.ontology.title}
-              </Link>
-            </h2>
-          </div>
-          <div>
-            <a href={ontologyPageContext.ontology.iri}>{ontologyPageContext.ontology.iri}</a>
-          </div>
-        </div>
-      </div>
-      <div className='row collpase-button-container justify-content-md-center'>
-        <div className='col-sm-1'>
-          <div className='header-collapse-btn' onClick={() => {
-            collpaseSiteHeader()
-          }}>
-            <i className='fa fa-angle-double-up header-collpase-icon'></i>
-          </div>
+    <div className='row collpase-button-container justify-content-md-center'>
+      <div className='col-sm-1'>
+        <div className='header-collapse-btn' onClick={() => {
+          collpaseSiteHeader()
+        }}>
+          <i className='fa fa-angle-double-up header-collpase-icon'></i>
         </div>
       </div>
     </div>
@@ -106,24 +86,22 @@ export const OntologyPageHeadSection = () => {
 function collpaseSiteHeader() {
   let siteHeader = document.getElementsByClassName('header-wrapper')[0];
   let collapseIcone = document.getElementsByClassName('header-collpase-icon')[0];
-  let ontologyBannerContainer = document.getElementsByClassName('ont-info-bar')[0];
   let collapseIconContainer = document.getElementsByClassName('collpase-button-container')[0];
+  let collapseButton = document.getElementsByClassName('header-collapse-btn')[0];
   if (collapseIcone.classList.contains('fa-angle-double-up')) {
-    ontologyBannerContainer.classList.add('hidden-header');
     siteHeader.classList.add('hidden-header');
     siteHeader.classList.remove('visible-header');
-    ontologyBannerContainer.classList.remove('visible-header');
     collapseIcone.classList.remove('fa-angle-double-up');
     collapseIcone.classList.add('fa-angle-double-down');
     collapseIconContainer.classList.add('show-header-arrow-down');
+    collapseButton.style.marginTop = "-10px";
   } else {
     siteHeader.classList.remove('hidden-header');
-    ontologyBannerContainer.classList.remove('hidden-header');
     siteHeader.classList.add('visible-header');
-    ontologyBannerContainer.classList.add('visible-header');
     collapseIcone.classList.add('fa-angle-double-up');
     collapseIcone.classList.remove('fa-angle-double-down');
     collapseIconContainer.classList.remove('show-header-arrow-down');
+    collapseButton.style.marginTop = "-40px";
   }
 
 }
