61729228ecc5af533454de3a7e3092f08359b386
pooya <pooya.oladazimi@gmail.com>
2026-04-01 17:17:28 +0200

    initiate linked datasets table

--- src/components/Ontologies/LinkedDatasets/LinkedDatasets.tsx
+++ src/components/Ontologies/LinkedDatasets/LinkedDatasets.tsx
@@ -1,11 +1,30 @@
+import Table from 'react-bootstrap/Table';
 
 const LinkedDatasets = () => {
 
-  return (
-    <div>
-      <h1>LinkedDatasets</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>LinkedDatasets</h1>
+            <Table striped bordered>
+                <thead>
+                <tr>
+                    <th>Dataset</th>
+                    <th>Linked terms</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                </tr>
+                <tr>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                </tr>
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
 export default LinkedDatasets;
