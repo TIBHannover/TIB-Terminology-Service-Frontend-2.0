b0e3fb10e3b866f6c1a1f72efa59f2fbafb248d2
pooya.oa <pooya.oladazimi@tib.eu>
2026-05-04 11:29:43 +0200

    chage site logo and refactor its code

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -35,30 +35,17 @@ const SiteLogo = () => {
   return (
     <div className='row site-header-nav-logo-holder'>
       <div className=''>
-        {process.env.REACT_APP_PROJECT_ID === "general" &&
-          <span>
+        <span>
             <Link className="main-title" to={process.env.REACT_APP_PROJECT_SUB_PATH + "/"}>
-              <img src={"/TIB_Logo_W_W.SVG"} alt="tib logo" height={60}
-                   width={80} loading="lazy"/>
-              <h5>Terminology Service</h5>
+              <img
+                src={process.env.REACT_APP_SITE_LOGO}
+                alt="site logo"
+                loading="lazy"
+                className="site_logo_image"
+              />
+              {process.env.REACT_APP_SITE_LOGO_TEXT && <h5>{process.env.REACT_APP_SITE_LOGO_TEXT}</h5>}
             </Link>
-          </span>
-        }
-        {process.env.REACT_APP_PROJECT_ID !== "nfdi4chem" && process.env.REACT_APP_PROJECT_ID !== "general" &&
-          <span>
-            <Link className="main-title" to={process.env.REACT_APP_PROJECT_SUB_PATH + "/"}>
-              <img src="/site_logo.png" alt="site_logo" className='site-logo-image' loading="lazy"/>
-              <h5>Terminology Service</h5>
-            </Link>
-          </span>
-        }
-        {process.env.REACT_APP_PROJECT_ID === "nfdi4chem" &&
-          <span>
-            <Link className="main-title" to={process.env.REACT_APP_PROJECT_SUB_PATH + "/"}>
-              <img src="/site_logo.webp" alt="site_logo" className='site-logo-image' loading="lazy"/>
-            </Link>
-          </span>
-        }
+        </span>
       </div>
     </div>
   )
