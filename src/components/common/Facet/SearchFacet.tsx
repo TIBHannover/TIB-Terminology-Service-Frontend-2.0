1836b250be513588dd715b2892b097ae5898cd72
pooya.oa <pooya.oladazimi@tib.eu>
2026-06-11 15:30:50 +0200

    fix the issue with playwright test

--- src/components/common/Facet/SearchFacet.tsx
+++ src/components/common/Facet/SearchFacet.tsx
@@ -1,5 +1,3 @@
-
-
 type SearchFacetProps = {
   data: Record<string, number>; // string is the facet field name and number is the count
   selectedFacetFields: string[];
@@ -7,34 +5,36 @@ type SearchFacetProps = {
   className?: string;
   idPrefix: string;
   disableCount?: boolean;
-}
+};
 
 const SearchFacetBox = (props: SearchFacetProps) => {
-
   function render() {
     let result = [];
     for (let facetFieldName in props.data) {
       if (props.data[facetFieldName] === 0) {
         continue;
       }
-      result.push(<FacetCheckBox key={facetFieldName} facetFieldName={facetFieldName} {...props} />);
+      result.push(
+        <FacetCheckBox
+          key={facetFieldName}
+          facetFieldName={facetFieldName}
+          {...props}
+        />,
+      );
     }
     return result;
   }
 
-  return (
-    <>
-      {render()}
-    </>
-  );
+  return <>{render()}</>;
+};
 
-}
-
-export const FacetCheckBox = (props: SearchFacetProps & { facetFieldName: string }) => {
+export const FacetCheckBox = (
+  props: SearchFacetProps & { facetFieldName: string },
+) => {
   const facetFieldName = props.facetFieldName;
   return (
     <div className="row facet-item-row" key={facetFieldName}>
-      <div className='col-sm-9'>
+      <div className="col-sm-9">
         <div className="form-check">
           <input
             className={"form-check-input " + (props.className ?? "")}
@@ -44,20 +44,28 @@ export const FacetCheckBox = (props: SearchFacetProps & { facetFieldName: string
             key={facetFieldName}
             onClick={props.handleFacetFieldClick}
             checked={props.selectedFacetFields.includes(facetFieldName)}
-            onChange={() => { }}
+            onChange={() => {}}
           />
-          <label className="form-check-label" htmlFor={props.idPrefix + facetFieldName}>
+          <label
+            className="form-check-label"
+            htmlFor={props.idPrefix + facetFieldName}
+          >
             {facetFieldName}
           </label>
         </div>
       </div>
-      {!props.disableCount &&
+      {!props.disableCount && (
         <div className="col-sm-3">
-          <div className="facet-result-count">{props.data[facetFieldName]}</div>
+          <div
+            className="facet-result-count"
+            id={"facet-count-" + facetFieldName}
+          >
+            {props.data[facetFieldName]}
+          </div>
         </div>
-      }
+      )}
     </div>
   );
-}
+};
 
 export default SearchFacetBox;
