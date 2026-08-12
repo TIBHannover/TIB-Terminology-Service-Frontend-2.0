df919ff0636aa4f60f7ea4e964e32bf44cd85372
pooya.oa <pooya.oladazimi@tib.eu>
2026-08-12 10:11:54 +0200

    remove unused token from request

--- src/api/header.ts
+++ src/api/header.ts
@@ -10,7 +10,6 @@ export function getTsPluginHeaders(
   let user: UserModelType = userJson ? JSON.parse(userJson) : null;
   let header: TsPluginHeader = {};
   header["X-TS-Frontend-Id"] = process.env.REACT_APP_PROJECT_ID;
-  header["X-TS-Frontend-Token"] = process.env.REACT_APP_MICRO_BACKEND_TOKEN;
   header["X-TS-Auth-Provider"] = localStorage.getItem("authProvider");
 
   if (withAccessToken) {
